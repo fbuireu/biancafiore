95abef60df8034fce9b33d603bcade6981a2eacb
Ferran Buireu <fbuireu@gmail.com>
2020-03-04 22:11:51 +0100

    Added small refactor on single quotation

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,43 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
+import Header from './header';
+import './layout.css';
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+    const data = useStaticQuery(graphql`
+        query SiteTitleQuery {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
         }
-      }
-    }
-  `)
+    `);
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+    return (
+        <>
+            <Header siteTitle={`biancafiore.me (v0)`} />
+            <div
+                style={{
+                    margin: `0 auto`,
+                    maxWidth: 960,
+                    padding: `0 1.0875rem 1.45rem`,
+                }}
+            >
+                <main>{children}</main>
+                <footer>
+                    © {new Date().getFullYear()}, Built with love by
+                    {` `}
+                    <a href="https://www.gatsbyjs.org">Ferran Buireu</a>
+                </footer>
+            </div>
+        </>
+    );
+};
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+    children: PropTypes.node.isRequired,
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import Layout from '../components/layout'
-import Image from '../components/image'
-import SEO from '../components/seo'
+import React from 'react';
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
-    <Layout>
-        <SEO title="Home"/>
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}>
-            <Image/>
-        </div>
-    </Layout>
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>This will be an amazing portfolio for the best content writer ever.</p>
+    <p>Now go build something great.</p>
+    <div>
+      <Image />
+    </div>
+  </Layout>
 );
 
-export default IndexPage
+export default IndexPage;
