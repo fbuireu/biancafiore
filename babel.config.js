c915b36996f2ccf5fa0362130b03299cfa013a80
Ferran Buireu <fbuireu@gmail.com>
2020-03-05 19:37:58 +0100

    Carriage return fixed for Windows OS (CR to CRLF)

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,16 @@
-module.exports = function() {
+module.exports = function(api) {
+  api.cache(true);
+
   const presets = [
-    [`@babel/preset-env`, { 'useBuiltIns': `usage`, 'corejs': `3` }],
-    [`@babel/preset-react`, { 'development': true }],
+    [`@babel/preset-env`, { 'useBuiltIns': `usage`, 'corejs': `2` }],
+    [`@babel/preset-react`, { 'development': true, minify: true }],
   ];
 
   const plugins = [
     `@babel/plugin-syntax-dynamic-import`,
     `@babel/plugin-proposal-class-properties`,
     `@babel/plugin-transform-classes`,
+    `@babel/plugin-transform-runtime`
   ];
 
   return {
