8b08ecb45064c14b919637285a2e3d85f50b79c3
Ferran Buireu <fbuireu@gmail.com>
2020-03-17 22:18:10 +0100

    Return for arrow function added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,18 @@ import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
 
-const Index = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>This will be an amazing portfolio for the best content writer ever.</p>
-    <p>Now go build something great.</p>
-    <div>
-      <Image />
-    </div>
-  </Layout>
-);
+const Index = props => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>This will be an amazing portfolio for the best content writer ever.</p>
+      <p>Now go build something great.</p>
+      <div>
+        <Image />
+      </div>
+    </Layout>
+  );
+};
 
 export default Index;
