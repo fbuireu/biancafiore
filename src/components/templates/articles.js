de711175aa0d31f9701744971e715e8801e83377
Ferran Buireu <fbuireu@gmail.com>
2020-03-18 20:46:35 +0100

    Initial empty component for all articles added

---
+++ src/components/templates/articles.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../layout';
+import SEO from '../seo';
+
+const Articles = props => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>This will be an amazing portfolio for the best content writer ever.</p>
+      <p>Now go build something great.</p>
+    </Layout>
+  );
+};
+
+export default Articles;
