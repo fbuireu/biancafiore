6db548f2cee369387fc079dded20f955c832fda1
Ferran Buireu <fbuireu@gmail.com>
2020-03-21 12:23:49 +0100

    Removed builder for non-dynamic slug pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,11 @@
 const { createFilePath } = require(`gatsby-source-filesystem`);
 const { fmImagesToRelative } = require(`gatsby-remark-relative-images`);
-const homeBuilder = require(`./src/build/homeBuilder`);
 const articlesBuilder = require(`./src/build/articlesBuilder`);
 const tagsBuilder = require(`./src/build/tagsBuilder`);
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   await Promise.all(
     [
-      homeBuilder(graphql, actions, reporter),
       articlesBuilder(graphql, actions, reporter),
       tagsBuilder(graphql, actions, reporter),
     ],
