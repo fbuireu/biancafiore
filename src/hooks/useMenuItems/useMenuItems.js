76d8c653e5e1ade4613c8ab4f931221601bc95f6
Ferran Buireu <fbuireu@gmail.com>
2020-03-22 18:29:11 +0100

    Custom hook using useStaticQuery Gatsby hook to retrieve all menu elements from CMS

---
+++ src/hooks/useMenuItems/useMenuItems.js
@@ -0,0 +1,25 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+export const useMenuItems = () => {
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+      {
+          allMarkdownRemark(
+              filter: { frontmatter: { key: { eq: "menuItem" }}}
+              sort: { order: ASC, fields: [frontmatter___position] }) {
+              edges {
+                  node {
+                      fields {
+                          slug
+                      }
+                      frontmatter {
+                          name
+                          position
+                      }
+                  }
+              }
+          }
+      }
+  `);
+
+  return allMarkdownRemark.edges;
+};
