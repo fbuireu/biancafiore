52316daf82e66a849d7e853496d88e555f291130
Ferran Buireu <fbuireu@gmail.com>
2020-03-24 17:09:53 +0100

    Styles importation added

--- src/components/templates/Layout/Layout.js
+++ src/components/templates/Layout/Layout.js
@@ -2,36 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 import Header from '../../organisms/header/Header';
-import '../../layout.css';
+import '../../../styles/styles.scss';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
-      query SiteTitleQuery {
-          site {
-              siteMetadata {
-                  title
-              }
-          }
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
       }
+    }
   `);
 
-  return <>
-    <Header siteTitle={`biancafiore.me (v0)`} />
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `0 1.0875rem 1.45rem`,
-      }}
-    >
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with love by
-        {` `}
-        <a href="https://www.gatsbyjs.org">Ferran Buireu</a>
-      </footer>
-    </div>
-  </>;
+  return (
+    <>
+      <Header siteTitle={`biancafiore.me (v0)`} />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0 1.0875rem 1.45rem`,
+        }}
+      >
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with love by
+          {` `}
+          <a href="https://www.gatsbyjs.org">Ferran Buireu</a>
+        </footer>
+      </div>
+    </>
+  );
 };
 
 Layout.propTypes = {
