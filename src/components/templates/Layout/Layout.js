edcc93782a7d56887061d06ba2a7b8c9dcfa4670
Ferran Buireu <fbuireu@gmail.com>
2020-03-27 18:55:19 +0100

    Tabulation and indentation fixed

--- src/components/templates/Layout/Layout.js
+++ src/components/templates/Layout/Layout.js
@@ -6,13 +6,13 @@ import '../../../styles/styles.scss';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
+      query SiteTitleQuery {
+          site {
+              siteMetadata {
+                  title
+              }
+          }
       }
-    }
   `);
 
   return (
