410a8cc1139bc1502252366ce9b9773ff0928ea2
Ferran Buireu <fbuireu@gmail.com>
2020-03-28 18:56:50 +0100

    Importation of SVG added

--- src/components/atoms/LanguageSwitcher/LanguageSwitcher.js
+++ src/components/atoms/LanguageSwitcher/LanguageSwitcher.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import { changeLocale, FormattedMessage } from 'gatsby-plugin-intl';
 import { useLanguages } from '../../../hooks/useLanguages';
+import DownArrow from '../../../assets/svg/down_arrow.svg';
 import './LanguageSwitcher.scss';
 
 export const LanguageSwitcher = () => {
   const [isMenuSelectorOpen, setIsMenuSelectorOpen] = useState(false),
     languages = useLanguages();
 
-  return <li className={`language-switcher__wrapper`}>
+  return <li className={`language-switcher__wrapper ${isMenuSelectorOpen ? `--is-open` : ``}`}>
     <span onClick={() => setIsMenuSelectorOpen(!isMenuSelectorOpen)}>
       <FormattedMessage id={`changeLanguage`} />
+      <DownArrow />
     </span>
-    <ul className={`language-switcher__list ${isMenuSelectorOpen ? `--is-open` : ``}`}>
+    <ul className={`language-switcher__list`}>
       {languages.map((language, index) => isMenuSelectorOpen &&
         <li className={`language-switcher__item`} key={index} onClick={() => changeLocale(language.iso)}>{language.name}</li>)}
     </ul>
