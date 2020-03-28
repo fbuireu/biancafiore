e07492fa69583ec2fef789349a3ba90626420e8f
Ferran Buireu <fbuireu@gmail.com>
2020-03-28 17:59:42 +0100

    Custom hook to get (via StaticQuery) all available languages added

---
+++ src/hooks/useLanguages.js
@@ -0,0 +1,31 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+export const useLanguages = () => {
+  const { languages } = useStaticQuery(graphql`
+      {
+          languages: allMarkdownRemark(
+              filter: { frontmatter: { key: { eq: "tag" }}}
+          ) {
+              edges {
+                  node {
+                      frontmatter {
+                          key
+                          name
+                          iso
+                      }
+                  }
+              }
+          }
+      }
+  `);
+
+  let availableLanguages = [];
+
+  languages.edges.forEach(({ node }) => {
+    let { name, iso } = node.frontmatter;
+
+    (name || iso) && availableLanguages.push({ name: name, iso: iso });
+  });
+
+  return availableLanguages;
+};
