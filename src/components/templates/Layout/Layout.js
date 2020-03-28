f623f0a8fd06f38c7984ff1be0d491dedc34cd47
Ferran Buireu <fbuireu@gmail.com>
2020-03-28 18:01:31 +0100

    Inline default styles removed

--- src/components/templates/Layout/Layout.js
+++ src/components/templates/Layout/Layout.js
@@ -18,20 +18,12 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={`biancafiore.me (v0)`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with love by
-          {` `}
-          <a href="https://www.gatsbyjs.org">Ferran Buireu</a>
-        </footer>
-      </div>
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with love by
+        {` `}
+        <a href="https://www.gatsbyjs.org">Ferran Buireu</a>
+      </footer>
     </>
   );
 };
