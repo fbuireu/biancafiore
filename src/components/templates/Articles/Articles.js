890fc7a5b7819527ad9587203f64253e0d4d3c48
Ferran Buireu <fbuireu@gmail.com>
2020-03-31 22:19:01 +0200

    Query to retrieve all article data given a slug added

--- src/components/templates/Articles/Articles.js
+++ src/components/templates/Articles/Articles.js
@@ -1,8 +1,13 @@
+import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
 import React from 'react';
-import Layout from '../Layout/Layout';
+import Navigation from '../../molecules/Navigation/Navigation';
 import Seo from '../../organisms/Seo';
+import Layout from '../Layout/Layout';
 
-const Articles = props => {
+const Articles = ({ data }) => {
+  console.log(data);
+
   return <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
@@ -10,5 +15,34 @@ const Articles = props => {
     <p>Now go build something great.</p>
   </Layout>;
 };
+export const articleData = graphql`
+    query($slug: String!) {
+        article:markdownRemark(fields: { slug: { eq: $slug }}) {
+            html
+            frontmatter {
+                key
+                language
+                iso
+                seo {
+                    author
+                    metaDescription
+                }
+                content {
+                    publishDate
+                    readingTime
+                    title
+                    tags
+                    body
+                }
+            }
+        }
+    }
+`;
+
+Articles.propTypes = {
+  data: PropTypes.object,
+};
+
+Navigation.defaultProps = {};
 
 export default Articles;
