b9ab5f88ca02201d930ef6706bb8596fda0fb324
Ferran Buireu <fbuireu@gmail.com>
2020-04-05 20:40:02 +0200

    useRef hook to get article properties on loading component added

--- src/components/templates/Article/Article.js
+++ src/components/templates/Article/Article.js
@@ -1,18 +1,28 @@
 import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useScrollPosition } from '../../../hooks/useScrollPosition';
+import ReadingProgress from '../../atoms/ReadingProgress/ReadingProgress';
 import Navigation from '../../molecules/Navigation/Navigation';
 import Seo from '../../organisms/Seo';
 import Layout from '../Layout/Layout';
 import './Article.scss';
 
 const Article = ({ data }) => {
-  const { article } = data;
+  const [scroll, setScroll] = useState(0),
+    [articleProperties, setArticleProperties] = useState({}),
+    articleReference = useRef(null),
+    { article } = data;
+
+  useEffect(() => setArticleProperties(articleReference.current), []);
+
+  useScrollPosition(({ currentPosition }) => setScroll(currentPosition.y));
 
   return <Layout>
     <Seo title="Home" />
     <h1>{article.frontmatter.content.title}</h1>
-    <article dangerouslySetInnerHTML={{ __html: article.html }} />
+    <ReadingProgress scroll={scroll} articleProperties={articleProperties} />
+    <article ref={articleReference} dangerouslySetInnerHTML={{ __html: article.html }} />
   </Layout>;
 };
 
