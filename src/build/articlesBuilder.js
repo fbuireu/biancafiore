8ecf4a339fab8de19ebc498c0bef75a623f64d17
Ferran Buireu <fbuireu@gmail.com>
2020-04-09 16:59:26 +0200

    GraphQL map query field added

--- src/build/articlesBuilder.js
+++ src/build/articlesBuilder.js
@@ -30,6 +30,7 @@ async function articlesBuilder(graphql, { createPage }, reporter) {
                 readingTime
                 isFeaturedPost
                 featuredImage
+                map
                 tags
               }
             }
