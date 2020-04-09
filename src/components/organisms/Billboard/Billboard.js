9e1abeb0e342b9095339d876fa9023b6493eeaad
Ferran Buireu <fbuireu@gmail.com>
2020-04-09 16:36:34 +0200

    Reading time component added

--- src/components/organisms/Billboard/Billboard.js
+++ src/components/organisms/Billboard/Billboard.js
@@ -3,14 +3,16 @@ import React from 'react';
 import Subtitle from '../../atoms/Subtitle/Subtitle';
 import Summary from '../../atoms/Summary/Summary';
 import Title from '../../atoms/Title/Title';
+import ReadingTime from '../../molecules/ReadingTime/ReadingTime';
 import './Billboard.scss';
 
 const Billboard = ({ frontmatter }) => <section className={`billboard`}>
-  <div className={`wrapper`}>
-    <div className={`article-information`}>
+  <div className={`wrapper flex`}>
+    <div className={`article-information flex --column-wrap --justify-center`}>
       <Title title={frontmatter.content.title} />
       <Subtitle author={frontmatter.seo.author} lastUpdated={frontmatter.content.lastUpdated} />
       <Summary summary={frontmatter.content.summary} />
+      <ReadingTime />
     </div>
   </div>
 </section>;
