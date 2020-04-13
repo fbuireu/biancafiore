35aa2f08a5ef1dacd767049109eb970a8785231c
Ferran Buireu <fbuireu@gmail.com>
2020-04-13 20:10:40 +0200

    Progress bar with smooth scroll behaviour added

--- src/components/atoms/ReadingProgress/ReadingProgress.js
+++ src/components/atoms/ReadingProgress/ReadingProgress.js
@@ -1,16 +1,29 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import ArrowUp from '../../../assets/svg/arrow-up.svg';
 import { useWindowSize } from '../../../hooks/useWindowSize';
 import './ReadingProgress.scss';
 
-const ReadingProgress = ({ scroll, articleProperties }) => {
+export const ReadingProgress = ({ scroll, articleProperties }) => {
   let currentScroll = Math.abs(scroll),
     { height: windowHeight } = useWindowSize(),
     { offsetTop: articleOffsetTop, offsetHeight: articleHeight } = articleProperties,
     isArticleVisible = currentScroll >= articleOffsetTop,
     currentProgress = isArticleVisible ? Math.round((currentScroll - articleOffsetTop) / (articleHeight - windowHeight) * 100) : 0;
 
-  return <div className={`reading-progress ${isArticleVisible ? `--is-visible` : ``}`} style={{ width: `${currentProgress}vw` }} />;
+  const scrollToTop = () => {
+    window.scroll({
+      top: 0,
+      behavior: `smooth`,
+    });
+  };
+
+  return <div className={`reading-progress ${isArticleVisible ? `--is-visible` : ``}`} onClick={scrollToTop}>
+    <CircularProgressbarWithChildren className={`reading-progress__inner`} value={currentProgress}>
+      <ArrowUp className={`arrow-up`} />
+    </CircularProgressbarWithChildren>
+  </div>;
 };
 
 ReadingProgress.propTypes = {
