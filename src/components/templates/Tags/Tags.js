957b32d31e7ecd879728eca56e9dc8b14fd82cd9
Ferran Buireu <fbuireu@gmail.com>
2020-04-14 18:01:35 +0200

    Single-lane return added

--- src/components/templates/Tags/Tags.js
+++ src/components/templates/Tags/Tags.js
@@ -2,13 +2,11 @@ import React from 'react';
 import SEO from '../../organisms/SEO/SEO';
 import Layout from '../Layout/Layout';
 
-const Tags = props => {
-  return <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>This will be an amazing portfolio for the best content writer ever.</p>
-    <p>Now go build something great.</p>
-  </Layout>;
-};
+const Tags = props => <Layout>
+  <SEO title="Home" />
+  <h1>Hi people</h1>
+  <p>This will be an amazing portfolio for the best content writer ever.</p>
+  <p>Now go build something great.</p>
+</Layout>;
 
 export default Tags;
