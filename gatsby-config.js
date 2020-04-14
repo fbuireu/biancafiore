5d09f940375a7e7ccd5428e63dbddd63187c15c2
Ferran Buireu <fbuireu@gmail.com>
2020-04-14 19:03:04 +0200

    Author in siteMetadata added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Bianca Fiore`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@biancamariola`,
     url: `https://biancafiore.me`,
-    twitterUser: `biancamariola`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
