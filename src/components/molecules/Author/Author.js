7fd353754b1ad420fb64627bb223ddc6863d2826
Ferran Buireu <fbuireu@gmail.com>
2020-04-16 19:39:21 +0200

    Linking internal pages with reach-router fix

--- src/components/molecules/Author/Author.js
+++ src/components/molecules/Author/Author.js
@@ -1,14 +1,24 @@
+import { Link } from 'gatsby';
 import Img from 'gatsby-image';
 import PropTypes from 'prop-types';
 import React from 'react';
+import slugify from 'slugify';
+import './Author.scss';
 
-export const Author = ({ author, tags }) => {
-  return <div>
-    <Img className={``}
-         fluid={author.frontmatter.image.childImageSharp.fluid}
-         alt={author.frontmatter.name} />
-  </div>;
-};
+export const Author = ({ author, tags }) => <section className={`author__wrapper`}>
+  <div className={`author`}>
+    <Link to={`/tag/${slugify(author.frontmatter.name, { lower: true })}`}>
+      <Img className={`author__image`}
+           fluid={author.frontmatter.image.childImageSharp.fluid}
+           alt={author.frontmatter.name} />
+    </Link>
+
+    {author.frontmatter.description && <p className={`author__description`}>{author.frontmatter.description}</p>}
+  </div>
+  <ul className={`tags__list`}>
+    {tags.map((tag, index) => <li className={`tag__item`} key={index}><Link to={`/tag/${slugify(tag, { lower: true })}`}>#{tag}</Link></li>)}
+  </ul>
+</section>;
 
 Author.propTypes = {
   author: PropTypes.objectOf(PropTypes.object),
