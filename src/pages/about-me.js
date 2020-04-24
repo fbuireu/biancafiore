1724c319f78e12e703616cc03e2605ad99cfec4f
Ferran Buireu <fbuireu@gmail.com>
2020-04-24 16:40:12 +0200

    GraphQL query to retrieve data added

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,25 +1,43 @@
+import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
+
 import Map from '../components/organisms/Map/Map';
 import SEO from '../components/organisms/SEO/SEO';
 import Layout from '../components/templates/Layout/Layout';
 
-const AboutMe = props => {
+const AboutMe = ({ data }) => {
+  const { aboutMe } = data,
+    cities = aboutMe.edges[0].node.frontmatter.cities;
 
   return <Layout>
     <SEO title="Home" />
     <h1>Hi About page</h1>
     <p>This will be an amazing portfolio for the best content writer ever.</p>
     <p>Now go build something great.</p>
-    <Map />
+    <Map cities={cities} />
   </Layout>;
 };
 
-// export const AboutMeData = graphql`
-//     query getAboutMeData($slug: String!, $author: String) {
-// #        about-me: markDownRemark
-//     }
-// `;
+export const AboutMeData = graphql`
+    query getAboutMeData {
+        aboutMe: allMarkdownRemark(filter: { frontmatter: { key: { eq: "about-me" }}}) {
+            edges {
+                node {
+                    html
+                    frontmatter {
+                        cities {
+                            city
+                            coordinates
+                            countryIsoCode
+                            description
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
 
 AboutMe.propTypes = {
   data: PropTypes.objectOf(PropTypes.object).isRequired,
