87577bd50ff1a67aa48951bd7b875ab10ffa2824
Ferran Buireu <fbuireu@gmail.com>
2020-05-07 15:40:12 +0200

    Context consumer to get current language added

--- src/components/molecules/Navigation/Navigation.js
+++ src/components/molecules/Navigation/Navigation.js
@@ -1,4 +1,5 @@
 import { Link } from 'gatsby';
+import { IntlContextConsumer } from 'gatsby-plugin-intl';
 import React from 'react';
 import { useMenuItems } from '../../../hooks/useMenuItems';
 import { LanguageSwitcher } from '../../atoms/LanguageSwitcher/LanguageSwitcher';
@@ -8,8 +9,13 @@ const Navigation = () => {
   const menuItems = useMenuItems();
 
   return <nav>
-    <ul className={`flex --row-wrap --justify-between`}>
-      {menuItems.map(({ node }) => <li key={node.frontmatter.position}><Link to={node.fields.slug}>{node.frontmatter.name}</Link></li>)}
+    <ul>
+      <IntlContextConsumer>
+        {({ language: currentLocale }) =>
+          menuItems.map(
+            ({ node }) => <li key={node.frontmatter.position}><Link to={`/${currentLocale}/${node.fields.slug}`}>{node.frontmatter.name}</Link></li>)
+        }
+      </IntlContextConsumer>
       <LanguageSwitcher />
     </ul>
   </nav>;
