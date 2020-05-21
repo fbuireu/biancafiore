641964ac4b778d31285f789e620de41ad9a3734a
Ferran Buireu <fbuireu@gmail.com>
2020-05-21 20:21:05 +0200

    Refactored initial call to set and build map

--- src/components/molecules/Map/Map.js
+++ src/components/molecules/Map/Map.js
@@ -3,94 +3,141 @@ import * as am4core from '@amcharts/amcharts4/core';
 import * as am4maps from '@amcharts/amcharts4/maps';
 import am4themes_animated from '@amcharts/amcharts4/themes/animated';
 import PropTypes from 'prop-types';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Map.scss';
 
 am4core.useTheme(am4themes_animated);
-//Todo:
+// Todo:
 // JSON config (mixed),
-// selected point(from to),
-// refactor in promises (await)
 // Change plane image
-// Add series for countries (to remove on create event)
 // Custom ZoomIn/Out Buttons (add home button)
-// Loader
 // Treure pin al punt on esta l'avio
 // Const in mayus
+// Improve scale
+// zIndex linies
 // https://css-tricks.com/making-movies-with-amcharts/#article-header-id-6
 
 const Map = ({ cities }) => {
-  const [origin, setOrigin] = useState(null);
-  const [destination, setDestination] = useState(null);
   const mapReference = useRef(null);
+  const previousDestinationRef = useRef();
+  const previousLineRef = useRef();
   const mapConfiguration = {
     exclude: `AQ`,
     initialCity: cities.find(city => city.isInitialCity),
     countriesIsoCode: cities.map(city => city.countryIsoCode.toUpperCase()),
     mapCities: [],
+    flight: {
+      from: 0,
+      to: 1,
+    },
   };
+  let origin, destination;
 
-  useEffect(() => {
-    let mapChart = am4core.create(mapReference.current, am4maps.MapChart);
-
-    //CreateMapChart()
+  const createMapChart = mapChart => {
     mapChart.geodata = am4geodata_worldHigh;
+    mapChart.preloader.disabled = false;
     mapChart.projection = new am4maps.projections.Miller();
     mapChart.zoomControl = new am4maps.ZoomControl();
     mapChart.homeZoomLevel = 0;
     mapChart.maxZoomLevel = 4;
+  };
 
-    //SetPolygonSeries()
-    let polygonSeries = mapChart.series.push(new am4maps.MapPolygonSeries());
+  const setLoader = mapChart => {
+    const loader = mapChart.tooltipContainer.createChild(am4core.Container);
+    loader.background.fill = am4core.color(`#fff`);
+    loader.background.fillOpacity = 0.8;
+    loader.width = am4core.percent(100);
+    loader.height = am4core.percent(100);
+
+    const loaderLabel = loader.createChild(am4core.Label);
+    loaderLabel.text = `Loading stuff...`;
+    loaderLabel.align = `center`;
+    loaderLabel.valign = `middle`;
+    loaderLabel.fontSize = `1rem`;
+    loaderLabel.dy = 50;
+
+    const hourGlass = loader.createChild(am4core.Image);
+    hourGlass.href = `/assets/svg/loader.svg`;
+    hourGlass.align = `center`;
+    hourGlass.valign = `middle`;
+    hourGlass.horizontalCenter = `middle`;
+    hourGlass.verticalCenter = `middle`;
+    hourGlass.scale = 0.7;
+
+    return loader;
+  };
+
+  const setPolygonSeries = mapChart => {
+    const polygonSeries = mapChart.series.push(new am4maps.MapPolygonSeries());
     polygonSeries.useGeodata = true;
     polygonSeries.mapPolygons.template.fill = am4core.color(`#fff9f1`);
     polygonSeries.mapPolygons.template.stroke = am4core.color(`#b37e33`);
     polygonSeries.tooltip.background.strokeWidth = 0;
     polygonSeries.exclude = mapConfiguration.exclude;
 
-    //SetPolygonTemplate()
-    let polygonTemplate = polygonSeries.mapPolygons.template,
-      activeState = polygonTemplate.states.create(`active`);
+    return polygonSeries;
+  };
+
+  const setPolygonTemplate = polygonSeries => {
+    const polygonTemplate = polygonSeries.mapPolygons.template;
+    const activeState = polygonTemplate.states.create(`active`);
     activeState.properties.fill = am4core.color(`#fbcf90`);
 
-    //AddLines
-    let lineSeries = mapChart.series.push(new am4maps.MapArcSeries());
+    return polygonTemplate;
+  };
+
+  const setLineSeries = mapChart => {
+    const lineSeries = mapChart.series.push(new am4maps.MapArcSeries());
     lineSeries.mapLines.template.line.strokeWidth = 2;
     lineSeries.mapLines.template.line.stroke = am4core.color(`#d4a259`);
     lineSeries.mapLines.template.line.nonScalingStroke = true;
     lineSeries.mapLines.template.line.strokeDasharray = `.25rem`;
     lineSeries.zIndex = 10;
 
-    //AddDropShadowFilter
-    let lineSeriesShadow = lineSeries.filters.push(new am4core.DropShadowFilter);
-    lineSeriesShadow.blur = 5;
+    return lineSeries;
+  };
 
-    //AddShadowLines
-    let shadowLineSeries = mapChart.series.push(new am4maps.MapLineSeries());
+  const setDropShadowFilter = lineSeries => {
+    const lineSeriesShadow = lineSeries.filters.push(new am4core.DropShadowFilter());
+    lineSeriesShadow.blur = 5;
+  };
+
+  const setShadowLineSeries = mapChart => {
+    const shadowLineSeries = mapChart.series.push(new am4maps.MapLineSeries());
     shadowLineSeries.mapLines.template.line.strokeOpacity = 0;
     shadowLineSeries.mapLines.template.line.nonScalingStroke = true;
     shadowLineSeries.mapLines.template.shortestDistance = false;
     shadowLineSeries.zIndex = 5;
 
-    //AddCities
-    let mapCities = mapChart.series.push(new am4maps.MapImageSeries());
+    return shadowLineSeries;
+  };
+
+  const setCities = mapChart => {
+    const mapCities = mapChart.series.push(new am4maps.MapImageSeries());
     mapCities.mapImages.template.nonScaling = true;
     mapCities.tooltip.background.strokeWidth = 0;
     mapCities.cursorOverStyle = am4core.MouseCursorStyle.pointer;
 
-    //SetCity
-    let mapCity = mapCities.mapImages.template.createChild(am4core.Sprite);
+    return mapCities;
+  };
+
+  const setCity = mapCities => {
+    const mapCity = mapCities.mapImages.template.createChild(am4core.Sprite);
     mapCity.horizontalCenter = `middle`;
     mapCity.verticalCenter = `bottom`;
-    mapCity.scale = .04;
+    mapCity.scale = 0.04;
     mapCity.path = `M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
                     \tc6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
                     \tc-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z`;
     mapCity.fill = am4core.color(`#8b5a14`);
-    mapCity.filters.push(new am4core.DropShadowFilter);
+    mapCity.filters.push(new am4core.DropShadowFilter());
 
+    return mapCity;
+  };
+
+  const setInitialCities = mapCities => {
     cities.forEach(city => {
-      let mapCity = mapCities.mapImages.create();
+      const mapCity = mapCities.mapImages.create();
 
       mapCity.latitude = city.coordinates.coordinates[1];
       mapCity.longitude = city.coordinates.coordinates[0];
@@ -99,99 +146,156 @@ const Map = ({ cities }) => {
 
       mapConfiguration.mapCities.push(mapCity);
     });
+  };
 
-
-    const addLine = (from, to) => {
-      let line = lineSeries.mapLines.create(),
-        shadowLine = shadowLineSeries.mapLines.create();
-      line.className = `path`;
-      line.setClassName();
-      line.imagesToConnect = [from, to];
-      line.line.controlPointDistance = -.3;
-      shadowLine.imagesToConnect = [from, to];
-
-      return line;
-    };
-
-    //InitialConf
-    let initialCity = mapConfiguration.mapCities.find(initialCity => initialCity.name === mapConfiguration.initialCity.name);
-    addLine(initialCity, initialCity);
-
-    //CreatePlaneContainer
-    let planeContainer = lineSeries.mapLines.getIndex(0).lineObjects.create();
+  const setPlaneContainer = (lineSeries, mapChart) => {
+    const planeContainer = lineSeries.mapLines.getIndex(0).lineObjects.create();
     planeContainer.position = 0;
     planeContainer.nonScaling = false;
-    planeContainer.adapter.add(`scale`, (scale, target) => .5 * (1 - (Math.abs(.5 - target.position))) / mapChart.zoomLevel);
+    planeContainer.adapter.add(`scale`, (scale, target) => 0.5 * (1 - (Math.abs(0.5 - target.position))) / mapChart.zoomLevel);
 
-    //CreatePlaneShadow
-    let planeShadowContainer = shadowLineSeries.mapLines.getIndex(0).lineObjects.create();
+    return planeContainer;
+  };
+
+  const setPlaneShadowContainer = (shadowLineSeries, mapChart) => {
+    const planeShadowContainer = shadowLineSeries.mapLines.getIndex(0).lineObjects.create();
     planeShadowContainer.position = 0;
     planeShadowContainer.nonScaling = false;
     planeShadowContainer.adapter.add(`scale`, (scale, target) => {
-      target.opacity = .6 - (Math.abs(.5 - target.position));
+      target.opacity = 0.6 - (Math.abs(0.5 - target.position));
 
-      return .5 * (1 - (Math.abs(.5 - target.position))) / mapChart.zoomLevel;
+      return 0.5 * (1 - (Math.abs(0.5 - target.position))) / mapChart.zoomLevel;
     });
 
-    //SetPlane
-    let plane = planeContainer.createChild(am4core.Sprite);
-    plane.scale = .25;
+    return planeShadowContainer;
+  };
+
+  const setPlane = planeContainer => {
+    const plane = planeContainer.createChild(am4core.Sprite);
+    plane.scale = 0.25;
     plane.horizontalCenter = `middle`;
     plane.verticalCenter = `middle`;
     plane.path = `m2,106h28l24,30h72l-44,-133h35l80,132h98c21,0 21,34 0,34l-98,0 -80,134h-35l43,-133h-71l-24,30h-28l15,-47`;
     plane.fill = am4core.color(`#633a00`);
     plane.strokeOpacity = 0;
 
-    //SetPlaneImage
-    let planeShadow = planeShadowContainer.createChild(am4core.Sprite);
-    planeShadow.scale = .15;
+    return plane;
+  };
+
+  const setPlaneShadow = planeShadowContainer => {
+    const planeShadow = planeShadowContainer.createChild(am4core.Sprite);
+    planeShadow.scale = 0.15;
     planeShadow.horizontalCenter = `middle`;
     planeShadow.verticalCenter = `middle`;
     planeShadow.path = `m2,106h28l24,30h72l-44,-133h35l80,132h98c21,0 21,34 0,34l-98,0 -80,134h-35l43,-133h-71l-24,30h-28l15,-47`;
     planeShadow.fill = am4core.color(`#000`);
     planeShadow.strokeOpacity = 0;
 
-    // Plane animation
-    let currentLine = 0;
-    let direction = 1;
+    return planeShadow;
+  };
 
-    //FlyPlane
-    const flyPlane = () => {
-      let from = 0,
-        to = 1;
-      planeContainer.mapLine = lineSeries.mapLines.getIndex(currentLine);
-      planeContainer.parent = lineSeries;
-      planeShadowContainer.mapLine = shadowLineSeries.mapLines.getIndex(currentLine);
-      planeShadowContainer.parent = shadowLineSeries;
+  const addLines = (lineSeries, shadowLineSeries) => {
+    return (from, to) => {
+      const line = lineSeries.mapLines.create();
+      const shadowLine = shadowLineSeries.mapLines.create();
+      line.className = `--is-created`;
+      line.setClassName();
+      line.imagesToConnect = [from, to];
+      line.line.controlPointDistance = -0.3;
+      shadowLine.imagesToConnect = [from, to];
 
-      // Set up flyAnimation
-      if (plane.rotation !== 0) plane.animate({ to: 0, property: `rotation` }, 1000).events.on(`animationended`, flyPlane);
-
-      // Start the flyAnimation
-      let flyAnimation = planeContainer.animate({
-        property: `position`,
-        from: from,
-        to: to,
-      }, 5000, am4core.ease.sinInOut);
-      flyAnimation.events.on(`animationended`, flyPlane);
-
-      planeShadowContainer.animate({
-        property: `position`,
-        from: from,
-        to: to,
-      }, 5000, am4core.ease.sinInOut);
-
-      // Increment line, or reverse the direction
-      currentLine += direction;
-      if (currentLine < 0) {
-        currentLine = 0;
-        direction = 1;
-      }
+      return line;
     };
+  };
+
+  const eraseLine = line => {
+    line.className = `--is-erased`;
+    line.setClassName();
+  };
+
+  const flyPlane = (planeContainer, lineSeries, planeShadowContainer, shadowLineSeries, currentLine) => {
+    const { from, to } = mapConfiguration.flight;
+
+    planeContainer.mapLine = lineSeries.mapLines.getIndex(currentLine);
+    planeContainer.parent = lineSeries;
+    planeShadowContainer.mapLine = shadowLineSeries.mapLines.getIndex(currentLine);
+    planeShadowContainer.parent = shadowLineSeries;
+
+    planeContainer.animate({
+      property: `position`,
+      from: from,
+      to: to,
+    }, 5000, am4core.ease.sinInOut).delay(1000);
+
+    planeShadowContainer.animate({
+      property: `position`,
+      from: from,
+      to: to,
+    }, 5000, am4core.ease.sinInOut).delay(1000);
+  };
+
+  const buildMap = mapChart => {
+    const currentLine = 1;
+
+    const loader = setLoader(mapChart);
+    loader.show();
+    createMapChart(mapChart);
+
+    const polygonSeries = setPolygonSeries(mapChart);
+    const polygonTemplate = setPolygonTemplate(polygonSeries);
+    const lineSeries = setLineSeries(mapChart);
+    setDropShadowFilter(lineSeries);
+
+    const shadowLineSeries = setShadowLineSeries(mapChart);
+    const mapCities = setCities(mapChart);
+
+    setCity(mapCities);
+    setInitialCities(mapCities);
+
+    const initialCity = mapConfiguration.mapCities.find(initialCity => initialCity.name === mapConfiguration.initialCity.name);
+    const addLine = addLines(lineSeries, shadowLineSeries);
+
+    addLine(initialCity, initialCity);
+
+    const planeContainer = setPlaneContainer(lineSeries, mapChart);
+    const planeShadowContainer = setPlaneShadowContainer(shadowLineSeries, mapChart);
+
+    setPlane(planeContainer);
+    setPlaneShadow(planeShadowContainer);
+    loader.hide();
+
+    return {
+      polygonSeries,
+      polygonTemplate,
+      lineSeries,
+      shadowLineSeries,
+      initialCity,
+      addLine,
+      eraseLine,
+      planeContainer,
+      planeShadowContainer,
+      currentLine,
+    };
+  };
+
+  useEffect(() => {
+    const mapChart = am4core.create(mapReference.current, am4maps.MapChart);
+    let {
+      polygonSeries,
+      polygonTemplate,
+      lineSeries,
+      shadowLineSeries,
+      initialCity,
+      addLine,
+      eraseLine,
+      planeContainer,
+      planeShadowContainer,
+      currentLine,
+    } = buildMap(mapChart);
 
     polygonTemplate.events.on(`over`, element => {
       mapConfiguration.countriesIsoCode.map(iso => {
-        let country = polygonSeries.getPolygonById(iso);
+        const country = polygonSeries.getPolygonById(iso);
 
         if (element.target.dataItem.dataContext.id === iso) {
           country.isHover = true;
@@ -203,18 +307,19 @@ const Map = ({ cities }) => {
 
     mapConfiguration.mapCities.forEach(city => {
       city.events.on(`hit`, element => {
-        // console.log(element.target.name)
-        // let clickedCity = mapConfiguration.mapCities.find(city => city.name === element.target.name);
-        // console.log(clickedCity)
-        //         setDestination(element.target);
-        // origin ? setOrigin(initialCity) : setOrigin(destination);
+        destination = element.target;
+        origin = previousDestinationRef.current ? previousDestinationRef.current : initialCity;
+        if (currentLine > 1) {
+          console.log(`in`);
+          eraseLine(previousLineRef.current);
+        }
 
-        // console.log('origin', origin);
-        // console.log('destination', destination);
-        // // console.log('destination', clickedCity);
-        //
-        addLine(initialCity, element.target);
-        flyPlane();
+        const line = addLine(origin, destination);
+        flyPlane(planeContainer, lineSeries, planeShadowContainer, shadowLineSeries, currentLine);
+
+        previousDestinationRef.current = element.target;
+        previousLineRef.current = line;
+        currentLine++;
       });
     });
 
@@ -226,7 +331,7 @@ const Map = ({ cities }) => {
       let north, south, west, east;
 
       mapConfiguration.countriesIsoCode.forEach(isoCode => {
-        let country = polygonSeries.getPolygonById(isoCode);
+        const country = polygonSeries.getPolygonById(isoCode);
 
         if (!north || (country.north > north)) north = country.north;
         if (!south || (country.south < south)) south = country.south;
@@ -240,12 +345,11 @@ const Map = ({ cities }) => {
     });
 
     return () => mapChart && mapChart.dispose();
-  }, []);
+  });
 
   return <section>
     <div ref={mapReference} style={{ width: `100%`, height: `500px` }} />
   </section>;
-
 };
 
 Map.propTypes = {
