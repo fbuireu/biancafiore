379d112a9f3fb21a9e66f05d1ffa6e88f4fa07b6
Ferran Buireu <fbuireu@gmail.com>
2020-05-25 18:23:25 +0200

    Context consumer for internationalisation added

--- src/components/atoms/Subtitle/Subtitle.js
+++ src/components/atoms/Subtitle/Subtitle.js
@@ -1,11 +1,16 @@
+import { Link } from 'gatsby';
+import { IntlContextConsumer } from 'gatsby-plugin-intl';
 import PropTypes from 'prop-types';
 import React from 'react';
+import slugify from 'slugify';
 import './Subtitle.scss';
 
 const Subtitle = ({ lastUpdated, author }) => <p className={`subtitle`}>
   <time dateTime={lastUpdated}>{lastUpdated}</time>
   &nbsp;|&nbsp;
-  <span className={`author`}>{author}</span>
+  <IntlContextConsumer>
+    {({ language: currentLanguage }) => <Link to={`${currentLanguage}/tag/${slugify(author, { lower: true })}`} className={`author`}>{author}</Link>}
+  </IntlContextConsumer>
 </p>;
 
 Subtitle.propTypes = {
