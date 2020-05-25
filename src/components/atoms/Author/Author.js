d929ea5423442bdff479bb289a31de6ff93ad47a
Ferran Buireu <fbuireu@gmail.com>
2020-05-25 18:23:25 +0200

    Context consumer for internationalisation added

--- src/components/atoms/Author/Author.js
+++ src/components/atoms/Author/Author.js
@@ -1,5 +1,6 @@
 import { Link } from 'gatsby';
 import Img from 'gatsby-image';
+import { IntlContextConsumer } from 'gatsby-plugin-intl';
 import PropTypes from 'prop-types';
 import React from 'react';
 import slugify from 'slugify';
@@ -7,12 +8,15 @@ import './Author.scss';
 
 export const Author = ({ author, tags }) => <section className={`author__wrapper`}>
   <div className={`author`}>
-    <Link to={`/tag/${slugify(author.frontmatter.name, { lower: true })}`}>
-      <Img className={`author__image`}
-           fluid={author.frontmatter.image.childImageSharp.fluid}
-           alt={author.frontmatter.name} />
-    </Link>
-
+    <IntlContextConsumer>
+      {({ language: currentLanguage }) =>
+        <Link to={`${currentLanguage}/tag/${slugify(author.frontmatter.name, { lower: true })}`}>
+          <Img className={`author__image`}
+               fluid={author.frontmatter.image.childImageSharp.fluid}
+               alt={author.frontmatter.name} />
+        </Link>
+      }
+    </IntlContextConsumer>
     {author.frontmatter.description && <p className={`author__description`}>{author.frontmatter.description}</p>}
   </div>
   <ul className={`tags__list`}>
