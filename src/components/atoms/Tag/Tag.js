351418ea8f8a0782358f51ab4ae20ed7e8f35a31
Ferran Buireu <fbuireu@gmail.com>
2020-05-26 15:18:42 +0200

    Consumer to get current language and display proper URL path added

---
+++ src/components/atoms/Tag/Tag.js
@@ -0,0 +1,24 @@
+import { Link } from 'gatsby';
+import { IntlContextConsumer } from 'gatsby-plugin-intl';
+import PropTypes from 'prop-types';
+import React from 'react';
+import slugify from 'slugify';
+import Label from '../../../assets/svg/label.svg';
+import './Tag.scss';
+
+const Tag = ({ tags }) => <ul className={`tags__list`}>
+  <IntlContextConsumer>
+    {({ language: currentLanguage }) =>
+      tags.map((tag, index) =>
+        <li className={`tag__item`} key={index}><Link to={`${currentLanguage}/${slugify(tag, { lower: true })}`}><Label className={`tag__item__label`} />{tag}</Link></li>)
+    }
+  </IntlContextConsumer>
+</ul>;
+
+Tag.propTypes = {
+  tags: PropTypes.arrayOf(PropTypes.string),
+};
+
+Tag.defaultProps = {};
+
+export default Tag;
