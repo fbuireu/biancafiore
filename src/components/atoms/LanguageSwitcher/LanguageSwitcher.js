aac65b49051da66f7a920fdbf94803e4e58bfe3d
Ferran Buireu <fbuireu@gmail.com>
2020-05-27 19:04:42 +0200

    Blind binding in JSX onClick fixed

--- src/components/atoms/LanguageSwitcher/LanguageSwitcher.js
+++ src/components/atoms/LanguageSwitcher/LanguageSwitcher.js
@@ -1,28 +1,33 @@
-import { changeLocale, FormattedMessage } from 'gatsby-plugin-intl';
-import React, { useEffect, useState } from 'react';
+import { changeLocale, FormattedMessage, IntlContextConsumer } from 'gatsby-plugin-intl';
+import React, { useState } from 'react';
 import ArrowDown from '../../../assets/svg/arrow-down.svg';
 import { useLanguages } from '../../../hooks/useLanguages';
 import './LanguageSwitcher.scss';
 
 export const LanguageSwitcher = () => {
   const [isMenuSelectorOpen, setIsMenuSelectorOpen] = useState(false),
-    [currentLanguage, setCurrentLanguage] = useState(undefined),
     languages = useLanguages();
 
-  useEffect(() => setCurrentLanguage(localStorage.getItem(`gatsby-intl-language`)), []);
+  const handleMenuSelector = () => setIsMenuSelectorOpen(!isMenuSelectorOpen),
+    handleLanguage = language => changeLocale(language);
 
   return <li className={`language-switcher__wrapper ${isMenuSelectorOpen ? `--is-open` : ``}`}>
-    <span onClick={() => setIsMenuSelectorOpen(!isMenuSelectorOpen)}>
+    <span onClick={handleMenuSelector}>
       <FormattedMessage id={`global.changeLanguage`} />
       <ArrowDown className={`arrow-down`} />
     </span>
     <ul className={`language-switcher__list`}>
       {languages.map((language, index) => isMenuSelectorOpen &&
-        <li className={`language-switcher__item ${currentLanguage === language.isoCode ? `--is-current-language` : ``}`}
-            onClick={() => changeLocale(language.isoCode)}
-            key={index}>
-          <FormattedMessage id={`global.${language.isoCode}`} />
-        </li>)}
+        <IntlContextConsumer>
+          {({ language: currentLanguage }) =>
+            <li className={`language-switcher__item ${currentLanguage === language.isoCode ? `--is-current-language` : ``}`}
+                onClick={() => handleLanguage(language.isoCode)}
+                key={index}>
+              <FormattedMessage id={`global.${language.isoCode}`} />
+            </li>
+          }
+        </IntlContextConsumer>,
+      )}
     </ul>
   </li>;
 };
