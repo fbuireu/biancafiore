614bf3600284659bdc709cf4ec857f53985ccf0a
Ferran Buireu <fbuireu@gmail.com>
2020-06-02 18:50:43 +0200

    Indentation fixed

--- __mocks__/gatsby.js
+++ __mocks__/gatsby.js
@@ -6,16 +6,16 @@ module.exports = {
   graphql: jest.fn(),
   Link: jest.fn().mockImplementation(
     ({
-       activeClassName,
-       activeStyle,
-       getProps,
-       innerRef,
-       partiallyActive,
-       ref,
-       replace,
-       to,
-       ...rest
-     }) =>
+      activeClassName,
+      activeStyle,
+      getProps,
+      innerRef,
+      partiallyActive,
+      ref,
+      replace,
+      to,
+      ...rest
+    }) =>
       React.createElement(`a`, {
         ...rest,
         href: to,
