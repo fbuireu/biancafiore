1eeffc48694bd05c82ef8035737f163282451e21
Ferran Buireu <fbuireu@gmail.com>
2020-06-02 22:14:04 +0200

    Loop through related articles added

--- src/components/molecules/RelatedArticles/RelatedArticles.js
+++ src/components/molecules/RelatedArticles/RelatedArticles.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import ArticleCard from '../../atoms/ArticleCard/ArticleCard';
 import './RelatedArticles.scss';
 
 export const RelatedArticles = ({ relatedArticles }) => <section className={`related-articles__wrapper`}>
   <h3>Related Articles</h3>
   <ul className={`related-articles__list`}>
-    {relatedArticles.map(({ node: relatedArticle }, index) => <li key={index}>{relatedArticle.frontmatter.content.title}</li>)}
+    {relatedArticles.map(({ node: relatedArticle }, index) => <li key={index}><ArticleCard article={relatedArticle} /></li>)}
   </ul>
 </section>;
 
