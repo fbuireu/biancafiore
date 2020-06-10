d001fe1bf884c60227022754995a3f116051e43e
Ferran Buireu <fbuireu@gmail.com>
2020-06-10 20:49:33 +0200

    Algolia GraphQL query to feed indices with articles added

--- src/utils/search/algolia.js
+++ src/utils/search/algolia.js
@@ -6,49 +6,36 @@ const articlesQuery = `{
       order: DESC }) {
     edges {
       node {
+        html
+        excerpt (pruneLength: 350)
         fields {
           slug
         }
+        objectID: id
         frontmatter {
+          language
           author
           content {
+            title
+            summary
+            publishDate
+            lastUpdated
+            readingTime
             tags
           }
         }
       }
     }
   }
-}`,
-  tagsQuery = `{
-      tags: allMarkdownRemark (
-      filter: { frontmatter: { key: { eq: "tag" }}}) {
-      edges {
-        node {
-          frontmatter {
-            name
-            slug
-            content {
-              tags
-            }
-          }
-        }
-      }
-    }
-  }`;
+}`;
 
-const flatten = arr => arr.map(({ node: { frontmatter, ...articleBody } }) => ({ ...frontmatter, ...articleBody })),
+const flatten = data => data.map(({ node: { frontmatter, ...rest } }) => ({ ...frontmatter, ...rest })),
   settings = { attributesToSnippet: [`excerpt:20`] },
   algoliaQueries = [
     {
       query: articlesQuery,
       transformer: ({ data }) => flatten(data.articles.edges),
       indexName: `Articles`,
-      settings,
-    },
-    {
-      query: tagsQuery,
-      transformer: ({ data }) => flatten(data.tags.edges),
-      indexName: `tags`,
       settings,
     },
   ];
