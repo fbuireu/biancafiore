fab827f9b837297a154491f00d1a32ddefca3cb2
Ferran Buireu <fbuireu@gmail.com>
2020-06-15 19:38:46 +0200

    Initial FilterStats component added

---
+++ src/components/molecules/FilterStats/FilterStats.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Stats } from 'react-instantsearch-dom';
+
+const FilterStats = () => <div className={`filter__stats`}><Stats /></div>;
+
+FilterStats.propTypes = {};
+
+FilterStats.defaultProps = {};
+
+export default FilterStats;
