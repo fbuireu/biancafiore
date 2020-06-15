06e2206729378dc10358d6874079290105718147
Ferran Buireu <fbuireu@gmail.com>
2020-06-15 19:39:01 +0200

    Initial FilterSearch component added

---
+++ src/components/molecules/FilterSearch/FilterSearch.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SearchBox } from 'react-instantsearch-dom';
+
+const FilterSearch = () => <div className={`filter__search-box`}><SearchBox /></div>;
+
+FilterSearch.propTypes = {};
+
+FilterSearch.defaultProps = {};
+
+export default FilterSearch;
