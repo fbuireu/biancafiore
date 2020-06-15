8f6d1fb000062cb2c4e206d27aa6d60c1a2e58e1
Ferran Buireu <fbuireu@gmail.com>
2020-06-15 19:34:46 +0200

    Algolia's ClearRefinements component added

---
+++ src/components/molecules/FilterClearRefinements/FilterClearRefinements.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ClearRefinements } from 'react-instantsearch-dom';
+
+const FilterClearRefinements = () => <div className={`filter__stats`}><ClearRefinements /></div>;
+
+FilterClearRefinements.propTypes = {};
+
+FilterClearRefinements.defaultProps = {};
+
+export default FilterClearRefinements;
