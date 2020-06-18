a6d164698ed0ae1bf0b49621f8a9156e3ffb5d1b
Ferran Buireu <fbuireu@gmail.com>
2020-06-18 21:00:10 +0200

    HOC to get custom Algolia's hit structure added

--- src/components/molecules/Hit/Hit.js
+++ src/components/molecules/Hit/Hit.js
@@ -1,15 +1,24 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { connectHits } from 'react-instantsearch-dom';
 import FeaturedImageHit from '../../organisms/FeaturedImageHit/FeaturedImageHit';
 import SimpleHit from '../../organisms/SimpleHit/SimpleHit';
 import './Hit.scss';
 
-const Hit = ({ hit: article }) => article.content.featuredImage ? <FeaturedImageHit {...article} /> : <SimpleHit {...article} />;
+export const Hit = ({ hits: articles }) => {
+  return <ul className={`article-card__list`}>
+    {articles.map(article => article.content.featuredImage
+      ? <FeaturedImageHit key={article.content.title} {...article} />
+      : <SimpleHit key={article.content.title} {...article} />,
+    )}
+  </ul>;
+};
 
 Hit.propTypes = {
-  hit: PropTypes.objectOf(PropTypes.object),
+  hits: PropTypes.objectOf(PropTypes.object),
 };
 
 Hit.defaultProps = {};
 
-export default Hit;
+export const ArticleHit = connectHits(Hit);
+
