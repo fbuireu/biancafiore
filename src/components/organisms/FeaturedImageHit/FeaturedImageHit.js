74065ada4449f3afafb69927df8ee3de7b36306a
Ferran Buireu <fbuireu@gmail.com>
2020-06-18 18:13:42 +0200

    Complex article card (hit) component added

---
+++ src/components/organisms/FeaturedImageHit/FeaturedImageHit.js
@@ -0,0 +1,31 @@
+import { Link } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Highlight } from 'react-instantsearch-dom';
+
+export const FeaturedImageHit = article => <article className={`article-card`}>
+  <BackgroundImage className={`article-card__image`}
+                   fluid={[
+                     `linear-gradient(to bottom, transparent, #000)`,
+                     article.content.featuredImage.childImageSharp.fluid]}>
+    <Link to={`/${article.language.toLowerCase()}/blog${article.fields.slug}`}>
+      <h2>
+        <Highlight attribute={`content.title`} hit={article} tagName={`mark`} />
+      </h2>
+      <p>
+        <Highlight attribute={`${article.content.summary ? `content.summary` : `excerpt`}`}
+                   hit={article}
+                   tagName={`mark`} />
+      </p>
+    </Link>
+  </BackgroundImage>
+</article>;
+
+FeaturedImageHit.propTypes = {
+  article: PropTypes.objectOf(PropTypes.object),
+};
+
+FeaturedImageHit.defaultProps = {};
+
+export default FeaturedImageHit;
