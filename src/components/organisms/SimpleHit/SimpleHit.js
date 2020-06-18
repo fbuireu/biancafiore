903220f651b07932fe83078a7f557af6b6795d87
Ferran Buireu <fbuireu@gmail.com>
2020-06-18 20:59:47 +0200

    Component wrapped in unordered list

--- src/components/organisms/SimpleHit/SimpleHit.js
+++ src/components/organisms/SimpleHit/SimpleHit.js
@@ -3,16 +3,18 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Highlight } from 'react-instantsearch-dom';
 
-export const SimpleHit = article => <article className={`article-card`}>
-  <Link to={`/${article.language.toLowerCase()}/blog${article.fields.slug}`}>
-    <h2>
-      <Highlight attribute={`content.title`} hit={article} tagName={`mark`} />
-    </h2>
-    <p>
-      <Highlight attribute={`${article.content.summary ? `content.summary` : `excerpt`}`} hit={article} tagName={`mark`} />
-    </p>
-  </Link>
-</article>;
+export const SimpleHit = article => <li className={`article-card__item`}>
+  <article>
+    <Link to={`/${article.language.toLowerCase()}/blog${article.fields.slug}`}>
+      <h2>
+        <Highlight attribute={`content.title`} hit={article} tagName={`mark`} />
+      </h2>
+      <p>
+        <Highlight attribute={`${article.content.summary ? `content.summary` : `excerpt`}`} hit={article} tagName={`mark`} />
+      </p>
+    </Link>
+  </article>
+</li>;
 
 SimpleHit.propTypes = {
   article: PropTypes.objectOf(PropTypes.object),
