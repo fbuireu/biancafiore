e31cbc977669f40aeb57e79428fecf56b386cc4a
Ferran Buireu <fbuireu@gmail.com>
2020-06-18 18:16:12 +0200

    Refactor in Algolia's hits components added

--- src/components/organisms/FilterHits/FilterHits.js
+++ src/components/organisms/FilterHits/FilterHits.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import { connectStateResults, Hits } from 'react-instantsearch-dom';
-import { Hit } from '../../molecules/Hit/Hit';
+import { SortBy } from 'react-instantsearch-dom';
+import FilterClearRefinements from '../../molecules/FilterClearRefinements/FilterClearRefinements';
+import FilterRange from '../../molecules/FilterRange/FilterRange';
+import FilterRefinementList from '../../molecules/FilterRefinementList/FilterRefinementList';
+import FilterSearch from '../../molecules/FilterSearch/FilterSearch';
+import FilterStats from '../../molecules/FilterStats/FilterStats';
 
 const FilterHits = () => {
-  const FilterResults = connectStateResults(
-    ({ searchState, searchResults, children }) =>
-      searchResults && searchResults.nbHits !== 0
-        ? children
-        : <div>No results have been found for {searchState.query}.</div>,
-  );
+  const SEARCH_PARAMETERS = [
+    {
+      label: `Tags`,
+      attribute: `content.tags`,
+      operator: `and`,
+    },
+    {
+      label: `Author`,
+      attribute: `author`,
+      operator: `or`,
+    },
+  ];
 
-  return <div className={`filter__results`}>
-    <FilterResults>
-      <Hits hitComponent={Hit} />
-    </FilterResults>
-  </div>;
+  return <aside className={`filter__wrapper`}>
+    <p className={`filter__title`}>Filters</p>
+    <FilterSearch />
+    <FilterStats />
+    <SortBy defaultRefinement={`Articles`}
+            items={[
+              { value: `content.readingTime_asc`, label: `Reading Time Asc` },
+              { value: `content.readingTime_desc`, label: `Reading Time Desc` },
+              { value: `content.isFeaturedArticle`, label: `Featured` },
+            ]} />
+    <FilterClearRefinements />
+    {SEARCH_PARAMETERS.map(searchParameter => <FilterRefinementList key={searchParameter.label} {...searchParameter} />)}
+    <FilterRange />
+  </aside>;
 };
 
 FilterHits.propTypes = {};
