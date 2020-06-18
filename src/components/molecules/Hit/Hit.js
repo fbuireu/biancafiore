c663daa89dfdca05c723755683b433daf7255e9e
Ferran Buireu <fbuireu@gmail.com>
2020-06-18 18:11:37 +0200

    Refactor in Algolia's filter components added

--- src/components/molecules/Hit/Hit.js
+++ src/components/molecules/Hit/Hit.js
@@ -1,20 +1,10 @@
-import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Highlight } from 'react-instantsearch-dom';
+import FeaturedImageHit from '../../organisms/FeaturedImageHit/FeaturedImageHit';
+import SimpleHit from '../../organisms/SimpleHit/SimpleHit';
+import './Hit.scss';
 
-export const Hit = ({ hit: article }) => <article>
-  <Link to={`/${article.language.toLowerCase()}/blog${article.fields.slug}`}>
-    <h2>
-      <Highlight attribute={`content.title`} hit={article} tagName={`mark`} />
-    </h2>
-    <p>
-      <Highlight attribute={`${article.content.summary ? `content.summary` : `excerpt`}`}
-                 hit={article}
-                 tagName={`mark`} />
-    </p>
-  </Link>
-</article>;
+const Hit = ({ hit: article }) => article.content.featuredImage ? <FeaturedImageHit {...article} /> : <SimpleHit {...article} />;
 
 Hit.propTypes = {
   hit: PropTypes.objectOf(PropTypes.object),
