10ea5d0dc236c675426a2560715b68a6b2e31b5b
Ferran Buireu <fbuireu@gmail.com>
2020-06-18 20:59:47 +0200

    Component wrapped in unordered list

--- src/components/organisms/FeaturedImageHit/FeaturedImageHit.js
+++ src/components/organisms/FeaturedImageHit/FeaturedImageHit.js
@@ -4,23 +4,25 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Highlight } from 'react-instantsearch-dom';
 
-export const FeaturedImageHit = article => <article className={`article-card`}>
-  <BackgroundImage className={`article-card__image`}
-                   fluid={[
-                     `linear-gradient(to bottom, transparent, #000)`,
-                     article.content.featuredImage.childImageSharp.fluid]}>
-    <Link to={`/${article.language.toLowerCase()}/blog${article.fields.slug}`}>
-      <h2>
-        <Highlight attribute={`content.title`} hit={article} tagName={`mark`} />
-      </h2>
-      <p>
-        <Highlight attribute={`${article.content.summary ? `content.summary` : `excerpt`}`}
-                   hit={article}
-                   tagName={`mark`} />
-      </p>
-    </Link>
-  </BackgroundImage>
-</article>;
+export const FeaturedImageHit = article => <li className={`article-card__item`}>
+  <article>
+    <BackgroundImage className={`article-card__image`}
+                     fluid={[
+                       `linear-gradient(to bottom, transparent, #000)`,
+                       article.content.featuredImage.childImageSharp.fluid]}>
+      <Link to={`/${article.language.toLowerCase()}/blog${article.fields.slug}`}>
+        <h2>
+          <Highlight attribute={`content.title`} hit={article} tagName={`mark`} />
+        </h2>
+        <p>
+          <Highlight attribute={`${article.content.summary ? `content.summary` : `excerpt`}`}
+                     hit={article}
+                     tagName={`mark`} />
+        </p>
+      </Link>
+    </BackgroundImage>
+  </article>
+</li>;
 
 FeaturedImageHit.propTypes = {
   article: PropTypes.objectOf(PropTypes.object),
