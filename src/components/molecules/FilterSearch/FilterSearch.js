664ed10c88c85e6e5e8897da1a8a6ae5ce906e9a
Ferran Buireu <fbuireu@gmail.com>
2020-06-22 22:10:34 +0200

    Logic for translated placeholder for search states added

--- src/components/molecules/FilterSearch/FilterSearch.js
+++ src/components/molecules/FilterSearch/FilterSearch.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { SearchBox } from 'react-instantsearch-dom';
 
-const FilterSearch = () => <div className={`filter__search-box`}><SearchBox /></div>;
+const FilterSearch = () => <div className={`filter__search-box`}>
+  <SearchBox translations={{
+    submitTitle: `Submit your search query.`,
+    resetTitle: `Clear your search query.`,
+    placeholder: `Search here...`,
+  }} />
+</div>;
 
 FilterSearch.propTypes = {};
 
