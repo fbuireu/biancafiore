ff493bb3bb662bec523fbc73eefda4caba2d52de
Ferran Buireu <fbuireu@gmail.com>
2020-06-22 22:12:09 +0200

    HTML structure for refined filter categories added

--- src/components/molecules/FilterRefinementList/FilterRefinementList.js
+++ src/components/molecules/FilterRefinementList/FilterRefinementList.js
@@ -2,13 +2,20 @@ import orderBy from 'lodash.orderby';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { RefinementList } from 'react-instantsearch-dom';
+import ArrowDown from '../../../assets/svg/arrow-down.svg';
+import './FilterRefinementList.scss';
 
 const FilterRefinementList = ({ label, attribute, operator }) => {
   const defineRefinementListOrder = items => orderBy(items, [`label`, `count`], [`asc`, `desc`]);
 
   return <div className={`filter__refinement-list`}>
-    <p>{label}</p>
-    <RefinementList attribute={attribute} transformItems={items => defineRefinementListOrder(items)} operator={operator} />
+    <details className={`filter__refinement-details`}>
+      <summary className={`filter__refinement-summary`}>
+        <span>{label}</span>
+        <ArrowDown className={`arrow-down`} />
+      </summary>
+      <RefinementList attribute={attribute} transformItems={items => defineRefinementListOrder(items)} operator={operator} />
+    </details>
   </div>;
 };
 
