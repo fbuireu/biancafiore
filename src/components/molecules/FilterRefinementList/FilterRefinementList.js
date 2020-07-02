de4da382d080fc305f756286f2392d06334dbc30
Ferran Buireu <fbuireu@gmail.com>
2020-07-02 20:49:11 +0200

    Logic to detect when refinement is selected added

--- src/components/molecules/FilterRefinementList/FilterRefinementList.js
+++ src/components/molecules/FilterRefinementList/FilterRefinementList.js
@@ -1,20 +1,27 @@
 import orderBy from 'lodash.orderby';
 import PropTypes from 'prop-types';
-import React from 'react';
-import { RefinementList } from 'react-instantsearch-dom';
+import React, { useState } from 'react';
 import ArrowDown from '../../../assets/svg/arrow-down.svg';
+import { RefinementList } from '../../atoms/RefinementList/RefinementList';
 import './FilterRefinementList.scss';
 
 const FilterRefinementList = ({ label, attribute, operator }) => {
+  const [refinementSelected, setRefinementSelected] = useState(false);
+
   const defineRefinementListOrder = items => orderBy(items, [`label`, `count`], [`asc`, `desc`]);
+
+  const handleRefinementSelection = value => setRefinementSelected(value);
 
   return <div className={`filter__refinement-list`}>
     <details className={`filter__refinement-details`}>
       <summary className={`filter__refinement-summary`}>
-        <span>{label}</span>
+        <span className={`filter__refinement-title ${refinementSelected ? `--is-selected` : ``}`}>{label}</span>
         <ArrowDown className={`arrow-down`} />
       </summary>
-      <RefinementList attribute={attribute} transformItems={items => defineRefinementListOrder(items)} operator={operator} />
+      <RefinementList attribute={attribute}
+                      operator={operator}
+                      transformItems={items => defineRefinementListOrder(items)}
+                      selectRefinement={handleRefinementSelection} />
     </details>
   </div>;
 };
