4302db6692e790e90f4496cc0fea886b119637ac
Ferran Buireu <fbuireu@gmail.com>
2020-07-02 20:46:57 +0200

    Class name for Algolia empty query component added

--- src/components/organisms/ArticleHits/ArticleHits.js
+++ src/components/organisms/ArticleHits/ArticleHits.js
@@ -7,7 +7,7 @@ const ArticleHits = () => {
   const FilterResults = connectStateResults(
     ({ searchResults, children }) => searchResults && searchResults.nbHits !== 0
       ? children
-      : <p>{`Blimey! You're so picky that not even a genie could find what you're looking for. Try to expand your search.`}</p>,
+      : <p className={`filter__results --no-results-found`}>{`Blimey! You're so picky that not even a genie could find what you're looking for. Try to expand your search.`}</p>,
   );
 
   return <div className={`filter__results`}>
