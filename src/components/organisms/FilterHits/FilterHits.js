266f0c711078e66b2daf95d2f5c1f50391b91c07
Ferran Buireu <fbuireu@gmail.com>
2020-07-02 20:45:15 +0200

    Removed legacy Algolia's component

--- src/components/organisms/FilterHits/FilterHits.js
+++ src/components/organisms/FilterHits/FilterHits.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { SortBy } from 'react-instantsearch-dom';
-import FilterClearRefinements from '../../molecules/FilterClearRefinements/FilterClearRefinements';
 import FilterRange from '../../molecules/FilterRange/FilterRange';
 import FilterRefinementList from '../../molecules/FilterRefinementList/FilterRefinementList';
 import FilterSearch from '../../molecules/FilterSearch/FilterSearch';
@@ -31,7 +30,6 @@ const FilterHits = () => {
               { value: `content.readingTime_desc`, label: `Reading Time Desc` },
               { value: `content.isFeaturedArticle`, label: `Featured` },
             ]} />
-    <FilterClearRefinements />
     {SEARCH_PARAMETERS.map(searchParameter => <FilterRefinementList key={searchParameter.label} {...searchParameter} />)}
     <FilterRange />
   </aside>;
