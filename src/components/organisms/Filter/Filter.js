182325082b867063c202553b7bfa1b6cb0bc91cd
Ferran Buireu <fbuireu@gmail.com>
2020-07-05 22:46:50 +0200

    Custom react-select component added to Algolia's refinements

--- src/components/organisms/Filter/Filter.js
+++ src/components/organisms/Filter/Filter.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import Range from '../../molecules/Range/Range';
 import RefinementsList from '../../molecules/RefinementsList/RefinementsList';
-import SortHitsBy from '../../molecules/SortHitsBy/SortHitsBy';
-import Search from '../Search/Search';
+import { SortHitsBy } from '../../molecules/SortHitsBy/SortHitsBy';
+import { Search } from '../Search/Search';
 import './Filter.scss';
 
 const Filter = () => {
   const SEARCH_PARAMETERS = [
-    {
-      label: `Tags`,
-      attribute: `content.tags`,
-      operator: `and`,
-    },
-    {
-      label: `Authors`,
-      attribute: `author`,
-      operator: `or`,
-    },
-  ];
+      {
+        label: `Tags`,
+        attribute: `content.tags`,
+        operator: `and`,
+      },
+      {
+        label: `Authors`,
+        attribute: `author`,
+        operator: `or`,
+      },
+    ],
+    SORT_BY_PARAMETERS = [
+      { value: `content.readingTime_asc`, label: `Reading Time (asc)` },
+      { value: `content.readingTime_desc`, label: `Reading Time (desc)` },
+      { value: `content.isFeaturedArticle`, label: `Featured` },
+    ];
 
   return <aside className={`filters__wrapper`}>
     <p className={`filters__title`}>Filters</p>
     <Search />
-    <SortHitsBy />
+    <SortHitsBy defaultRefinement={process.env.GATSBY_ALGOLIA_INDEX_NAME} items={SORT_BY_PARAMETERS} />
     {SEARCH_PARAMETERS.map(searchParameter => <RefinementsList key={searchParameter.label} {...searchParameter} />)}
     <Range />
   </aside>;
