1f859be2c249e21bd7f35181be9da26a5a68c4fd
Ferran Buireu <fbuireu@gmail.com>
2020-07-06 22:23:22 +0200

    Subtitle for article hit component using <Highlight>'s Algolia HOC

---
+++ src/components/atoms/ArticleHitSubtitle/ArticleHitSubtitle.js
@@ -0,0 +1,25 @@
+import { Link } from 'gatsby';
+import { IntlContextConsumer } from 'gatsby-plugin-intl';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Highlight } from 'react-instantsearch-dom';
+import slugify from 'slugify';
+import './ArticleHitSubtitle.scss';
+
+const ArticleHitSubtitle = ({ hit }) => <p className={`article__subtitle`}>
+  <time dateTime={hit.content.lastUpdated}><Highlight attribute={`content.lastUpdated`} hit={hit} tagName={`mark`} /></time>
+  &nbsp;|&nbsp;
+  <IntlContextConsumer>
+    {({ language: currentLanguage }) => <Link to={`/${currentLanguage}/tag/${slugify(hit.author, { lower: true })}`} className={`article__author`}>
+      <Highlight attribute={`author`} hit={hit} tagName={`mark`} />
+    </Link>}
+  </IntlContextConsumer>
+</p>;
+
+ArticleHitSubtitle.propTypes = {
+  hit: PropTypes.objectOf(PropTypes.object),
+};
+
+ArticleHitSubtitle.defaultProps = {};
+
+export default ArticleHitSubtitle;
