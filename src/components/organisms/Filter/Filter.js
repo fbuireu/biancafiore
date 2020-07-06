aa1338aa162e6730326bdf5683e14fe52414108d
Ferran Buireu <fbuireu@gmail.com>
2020-07-06 22:16:23 +0200

    Unicode arrows in label added

--- src/components/organisms/Filter/Filter.js
+++ src/components/organisms/Filter/Filter.js
@@ -19,11 +19,11 @@ const Filter = () => {
       },
     ],
     SORT_BY_PARAMETERS = [
-      { value: `content.readingTime_asc`, label: `Reading Time (asc)` },
-      { value: `content.readingTime_desc`, label: `Reading Time (desc)` },
-      { value: `content.lastUpdated_asc`, label: `Last Updated Date (asc)` },
-      { value: `content.lastUpdated_desc`, label: `Last Updated Date (desc)` },
       { value: `content.isFeaturedArticle`, label: `Featured` },
+      { value: `content.readingTime_asc`, label: `Reading Time (ascending ↑)` },
+      { value: `content.readingTime_desc`, label: `Reading Time (descending ↓)` },
+      { value: `content.lastUpdated_asc`, label: `Updated Date (ascending ↑)` },
+      { value: `content.lastUpdated_desc`, label: `Updated Date (descending ↓)` },
     ];
 
   return <aside className={`filters__wrapper`}>
