88ad71718b70cd6062900c7c06227ffb06ba0189
Ferran Buireu <fbuireu@gmail.com>
2020-07-06 22:17:00 +0200

    <ArticleHitSummary> component added

--- src/components/organisms/FeaturedImageArticleCard/FeaturedImageArticleCard.js
+++ src/components/organisms/FeaturedImageArticleCard/FeaturedImageArticleCard.js
@@ -2,26 +2,25 @@ import { Link } from 'gatsby';
 import BackgroundImage from 'gatsby-background-image';
 import PropTypes from 'prop-types';
 import React from 'react';
-import ArticleHitSummary from '../../atoms/ArticleHitSummary/ArticleHitSummary';
+import ArticleHitSubtitle from '../../atoms/ArticleHitSubtitle/ArticleHitSubtitle';
 import ArticleHitTitle from '../../atoms/ArticleHitTitle/ArticleHitTitle';
 import ReadingTime from '../../atoms/ReadingTime/ReadingTime';
-import Subtitle from '../../atoms/Subtitle/Subtitle';
+import Summary from '../../atoms/Summary/Summary';
 import Tag from '../../atoms/Tag/Tag';
 
 export const FeaturedImageArticleCard = article => <li className={`article-card__item ${article.content.isFeaturedArticle ? `--is-featured` : ``}`}>
   <article className={`article-card__item__inner`}>
     <BackgroundImage className={`article-card__image`}
                      fluid={[
-                       `linear-gradient(rgba(0,0,0, .4), rgba(0, 0, 0, .8))`,
+                       `linear-gradient(rgba(0,0,0, .5), rgba(0, 0, 0, .8))`,
                        article.content.featuredImage.childImageSharp.fluid]}>
       <Link to={`/${article.language.toLowerCase()}/blog${article.fields.slug}`}
             className={`article-card__link`}>
         <ArticleHitTitle hit={article} />
-        {console.log(article)}
-        <Subtitle author={article.author} lastUpdated={article.content.lastUpdated} />
+        <ArticleHitSubtitle hit={article} />
         <ReadingTime readingTime={article.content.readingTime} />
         <Tag tags={article.content.tags} />
-        <ArticleHitSummary hit={article} article={article} />
+        <Summary summary={article.content.summary || article.excerpt} />
       </Link>
     </BackgroundImage>
   </article>
