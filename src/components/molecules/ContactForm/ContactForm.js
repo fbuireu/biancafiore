77987f0fc25c1e4d69b5e05ba1eee68752a30018
Ferran Buireu <fbuireu@gmail.com>
2020-07-08 21:31:07 +0200

    Initial usage for Google's recaptcha added

--- src/components/molecules/ContactForm/ContactForm.js
+++ src/components/molecules/ContactForm/ContactForm.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import Recaptcha from 'react-google-recaptcha';
 import TextareaInput from '../../atoms/TextareaInput/TextareaInput';
 import TextInput from '../../atoms/TextInput/TextInput';
 
 const ContactForm = () => {
-  return <form name={`contact`} method={`POST`} data-netlify={true} data-netlify-recaptcha={true}>
+  const recaptchaReference = useRef(null);
+
+  return <form action={`/contact/success`} name={`contact`} method={`POST`} data-netlify={true} data-netlify-recaptcha={true}>
     <TextInput />
     <TextareaInput />
-    <div data-netlify-recaptcha={true} />
+    {/*<div data-netlify-recaptcha={true} />*/}
+    <Recaptcha ref={recaptchaReference} sitekey={process.env.SITE_RECAPTCHA_KEY} />
     <button type={`submit`}>Send</button>
   </form>;
 };
