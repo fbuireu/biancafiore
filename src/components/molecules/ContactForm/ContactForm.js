75a6f175907a3bb77c1c91fad89d88f4e964887e
Ferran Buireu <fbuireu@gmail.com>
2020-07-09 19:18:51 +0200

    Initial verification field added

--- src/components/molecules/ContactForm/ContactForm.js
+++ src/components/molecules/ContactForm/ContactForm.js
@@ -6,11 +6,21 @@ import TextInput from '../../atoms/TextInput/TextInput';
 const ContactForm = () => {
   const recaptchaReference = useRef(null);
 
-  return <form action={`/contact/success`} name={`contact`} method={`POST`} data-netlify={true} data-netlify-recaptcha={true}>
+  const handleChange = value => {
+    console.log(value);
+  };
+
+  const handleSubmit = () => {
+    const recaptchaValue = recaptchaReference.current.getValue();
+    console.log(`submit value`, recaptchaValue);
+    // props.onSubmit(recaptchaValue);
+  };
+
+  return <form action={`/contact/success`} method={`POST`} name={`contact`} data-netlify={true} data-netlify-recaptcha={true}>
     <TextInput />
     <TextareaInput />
-    <Recaptcha ref={recaptchaReference} sitekey={process.env.GATSBY_SITE_RECAPTCHA_KEY} />
-    <button type={`submit`}>Send</button>
+    <Recaptcha ref={recaptchaReference} sitekey={process.env.GATSBY_SITE_RECAPTCHA_KEY} onChange={handleChange} />
+    <button type={`submit`} onSubmit={handleSubmit}>Send</button>
   </form>;
 };
 
