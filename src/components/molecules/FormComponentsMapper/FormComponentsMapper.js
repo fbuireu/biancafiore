8a5d4c0a35694600809bf7b512e90134f8942d8d
Ferran Buireu <fbuireu@gmail.com>
2020-07-09 23:07:28 +0200

    Mapping component to render a custom form field added

---
+++ src/components/molecules/FormComponentsMapper/FormComponentsMapper.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TextareaInput from '../../atoms/TextareaInput/TextareaInput';
+import TextInput from '../../atoms/TextInput/TextInput';
+
+const FormComponentsMapper = {
+  text: TextInput,
+  email: TextInput,
+  textarea: TextareaInput,
+};
+
+export default FormComponentsMapper;
+
+
+
+
+
