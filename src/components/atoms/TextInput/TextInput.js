3cdec4e7da5ee5c679cbb828f5afcdc8351c01a3
Ferran Buireu <fbuireu@gmail.com>
2020-07-09 23:04:48 +0200

    Custom props destructuring added

--- src/components/atoms/TextInput/TextInput.js
+++ src/components/atoms/TextInput/TextInput.js
@@ -1,12 +1,21 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-const TextInput = () => <div>
-  <label>Your Email:
-    <input type={`email`} name={`email`} />
+const TextInput = ({ name, type, isRequired, label, isValid, errorMessage }) => <div>
+  <label>{label}:
+    <input type={type} name={name.toLowerCase()} required={isRequired} />
   </label>
+  {!isValid && <div>{errorMessage}</div>}
 </div>;
 
-TextInput.propTypes = {};
+TextInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  isRequired: PropTypes.bool.isRequired,
+  label: PropTypes.string.isRequired,
+  isValid: PropTypes.bool.isRequired,
+  errorMessage: PropTypes.string.isRequired,
+};
 
 TextInput.defaultProps = {};
 
--- src/components/atoms/TextareaInput/TextareaInput.js
+++ src/components/atoms/TextareaInput/TextareaInput.js
@@ -1,14 +1,21 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-const TextareaInput = () => <div>
-  <label>Message:
-    <textarea name={`message`} />
+const TextareaInput = ({ name, isRequired, label, isValid, errorMessage }) => <div>
+  <label>{label}:
+    <textarea name={name.toLowerCase()} required={isRequired} />
   </label>
+  {!isValid && <div>{errorMessage}</div>}
 </div>;
 
-TextareaInput.propTypes = {};
+TextareaInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  isRequired: PropTypes.bool.isRequired,
+  label: PropTypes.string.isRequired,
+  isValid: PropTypes.bool.isRequired,
+  errorMessage: PropTypes.string.isRequired,
+};
 
 TextareaInput.defaultProps = {};
 
 export default TextareaInput;
-
