db6ff412681f2e2cc8d0d7ba274622969aa5a3b5
Ferran Buireu <fbuireu@gmail.com>
2020-07-20 20:48:29 +0200

    SEO component renamed

--- src/components/templates/Tags/Tags.js
+++ src/components/templates/Tags/Tags.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Seo from '../../atoms/Seo/Seo';
+import SEO from '../../atoms/SEO/SEO';
 import Layout from '../Layout/Layout';
 
 const Tags = props => <Layout>
-  <Seo title="Home" />
+  <SEO title="Home" />
   <h1>Hi people</h1>
   <p>This will be an amazing portfolio for the best content writer ever.</p>
   <p>Now go build something great.</p>
