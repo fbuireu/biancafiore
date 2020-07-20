f8a8f9d0efe616a90a8765943d7f3a6a092e0e3c
Ferran Buireu <fbuireu@gmail.com>
2020-07-20 20:51:35 +0200

    Initial validation onChange and onBlur added

--- src/components/molecules/ContactForm/ContactForm.js
+++ src/components/molecules/ContactForm/ContactForm.js
@@ -1,13 +1,37 @@
 import PropTypes from 'prop-types';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Recaptcha from 'react-google-recaptcha';
 import FormComponentsMapper from '../FormComponentsMapper/FormComponentsMapper';
 
 const ContactForm = ({ formInputs }) => {
+  const [formState, setFormState] = useState(formInputs);
   const recaptchaReference = useRef(null);
+  const IS_VALID_NAME = /^[a-zA-Z]+$/;
+  const IS_VALID_EMAIL = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  const handleChange = value => {
-    console.log(value);
+  const updateField = ({ target }) => {
+    let { value, name } = target;
+    const scopedForm = [...formState];
+    const field = scopedForm.find(field => field.name.toLowerCase() === name);
+
+    field.value = value;
+    field.isValid = true;
+    setFormState([...scopedForm]);
+
+    switch (name) {
+      case`name`:
+        return field.isValid = !!field.value.match(IS_VALID_NAME);
+      case`email`:
+        return field.isValid = !!field.value.match(IS_VALID_EMAIL);
+      case`message`:
+        return field.isValid = !!field.value.length;
+      default:
+        return field.isValid = false, field.errorMessage = `Something went wrong`;
+    }
+  };
+
+  const validateField = ({ target }) => {
+    console.log(`a`,target);
   };
 
   const handleSubmit = () => {
@@ -17,12 +41,12 @@ const ContactForm = ({ formInputs }) => {
   };
 
   return <form action={`/contact/success`} method={`POST`} name={`contact`} data-netlify={true} data-netlify-recaptcha={true}>
-    {formInputs.map(input => {
+    {formState.map(input => {
       let FormComponent = FormComponentsMapper[input.type];
 
-      return <FormComponent key={input.label} {...input} />;
+      return <FormComponent key={input.label} {...input} onChange={updateField} onBlur={validateField} />;
     })}
-    <Recaptcha ref={recaptchaReference} sitekey={process.env.GATSBY_SITE_RECAPTCHA_KEY} onChange={handleChange} />
+    <Recaptcha ref={recaptchaReference} sitekey={process.env.GATSBY_SITE_RECAPTCHA_KEY} onChange={updateField} />
     <button type={`submit`} onSubmit={handleSubmit}>Send</button>
   </form>;
 };
