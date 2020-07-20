24e2626f5137f096f11c50af7d31beaa6a121543
Ferran Buireu <fbuireu@gmail.com>
2020-07-20 20:50:12 +0200

    Default props destructured

--- src/components/atoms/TextInput/TextInput.js
+++ src/components/atoms/TextInput/TextInput.js
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const TextInput = ({ name, type, isRequired, label, isValid, errorMessage }) => {
+const TextInput = ({ name, type, isRequired, label, value, isValid, errorMessage, onChange, onBlur }) => {
   name = name.toLowerCase();
-    
+
   return <div>
     <label htmlFor={name}>{label}:
-      <input type={type} name={name} required={isRequired} />
+      <input type={type} name={name} value={value} required={isRequired} onChange={onChange} onBlur={onBlur} />
     </label>
     {!isValid && <div>{errorMessage}</div>}
   </div>;
@@ -18,11 +18,13 @@ TextInput.propTypes = {
   type: PropTypes.string.isRequired,
   isRequired: PropTypes.bool.isRequired,
   label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
   isValid: PropTypes.bool.isRequired,
   errorMessage: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onBlur: PropTypes.func.isRequired,
 };
 
 TextInput.defaultProps = {};
 
 export default TextInput;
-
--- src/components/atoms/TextareaInput/TextareaInput.js
+++ src/components/atoms/TextareaInput/TextareaInput.js
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const TextareaInput = ({ name, isRequired, label, isValid, errorMessage }) => {
+const TextareaInput = ({ name, isRequired, label, value, isValid, errorMessage, onChange, onBlur }) => {
   name = name.toLowerCase();
 
   return <div>
     <label htmlFor={name}>{label}:
-      <textarea name={name} required={isRequired} />
+      <textarea name={name} value={value} required={isRequired} onChange={onChange} onBlur={onBlur} />
     </label>
     {!isValid && <div>{errorMessage}</div>}
   </div>;
@@ -15,8 +15,11 @@ TextareaInput.propTypes = {
   name: PropTypes.string.isRequired,
   isRequired: PropTypes.bool.isRequired,
   label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
   isValid: PropTypes.bool.isRequired,
   errorMessage: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onBlur: PropTypes.func.isRequired,
 };
 
 TextareaInput.defaultProps = {};
