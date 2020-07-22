42ecb74a390c1f8c91530d8b9b138d9993b9f270
Ferran Buireu <fbuireu@gmail.com>
2020-07-22 22:33:06 +0200

    Isolated use-case field validation added

---
+++ src/utils/form/validateField.js
@@ -0,0 +1,19 @@
+const IS_VALID_NAME = /^[a-z ,.'-]+$/i;
+const IS_VALID_EMAIL = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+
+const validateField = (name, field) => {
+  switch (name) {
+    case`name`:
+      return field.isValid = !!field.value.match(IS_VALID_NAME);
+    case`email`:
+      return field.isValid = !!field.value.match(IS_VALID_EMAIL);
+    case`message`:
+      return field.isValid = !!field.value.length;
+    default:
+      field.errorMessage = `Something went wrong`;
+
+      return field.isValid = false;
+  }
+};
+
+export default validateField;
