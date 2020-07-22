606fea9f4fae63f878c33e898df0b17d8ee6a24f
Ferran Buireu <fbuireu@gmail.com>
2020-07-22 22:30:10 +0200

    Blur validation fixed

--- src/components/molecules/ContactForm/ContactForm.js
+++ src/components/molecules/ContactForm/ContactForm.js
@@ -1,58 +1,87 @@
+import axios from 'axios';
 import PropTypes from 'prop-types';
+import * as qs from 'query-string';
 import React, { useRef, useState } from 'react';
 import Recaptcha from 'react-google-recaptcha';
+import validateField from '../../../utils/form/validateField';
+import validateForm from '../../../utils/form/validateForm';
 import FormComponentsMapper from '../FormComponentsMapper/FormComponentsMapper';
 
-const ContactForm = ({ formInputs }) => {
+const ContactForm = ({ formInputs, location }) => {
   const [formState, setFormState] = useState(formInputs);
+  const formReference = useRef(null);
   const recaptchaReference = useRef(null);
-  const IS_VALID_NAME = /^[a-zA-Z]+$/;
-  const IS_VALID_EMAIL = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  const updateField = ({ target }) => {
-    let { value, name } = target;
+  const handleChange = ({ target }) => {
+    const { name, field } = updateField(target);
+    if (!field.isValid) validateField(name, field);
+  };
+
+  const handleBlur = ({ target }) => {
+    const { name, field } = updateField(target);
+    validateField(name, field);
+  };
+
+  const updateField = ({ value, name }) => {
     const scopedForm = [...formState];
-    const field = scopedForm.find(field => field.name.toLowerCase() === name);
+    const field = scopedForm.find(field => field.name === name);
 
     field.value = value;
     field.isValid = true;
     setFormState([...scopedForm]);
 
-    switch (name) {
-      case`name`:
-        return field.isValid = !!field.value.match(IS_VALID_NAME);
-      case`email`:
-        return field.isValid = !!field.value.match(IS_VALID_EMAIL);
-      case`message`:
-        return field.isValid = !!field.value.length;
-      default:
-        return field.isValid = false, field.errorMessage = `Something went wrong`;
-    }
+    return { name, field };
   };
 
-  const validateField = ({ target }) => {
-    console.log(`a`,target);
-  };
-
-  const handleSubmit = () => {
+  const handleSubmit = event => {
+    event.preventDefault();
+    const data = {};
     const recaptchaValue = recaptchaReference.current.getValue();
-    console.log(`submit value`, recaptchaValue);
-    // props.onSubmit(recaptchaValue);
+    const scopedForm = [...formState];
+
+    let isValidForm = validateForm(scopedForm);
+    setFormState([...scopedForm]);
+
+    if (!isValidForm || !recaptchaValue) return false;
+
+    formInputs.forEach(input => data[input.name] = input.value);
+
+    const AXIOS_PARAMETERS = {
+      url: location.pathname,
+      method: `POST`,
+      headers: { 'Content-Type': `application/x-www-form-urlencoded` },
+      data: qs.stringify(data),
+    };
+
+    axios(AXIOS_PARAMETERS)
+      .then(response => {
+        console.log(`OK`, response);
+      })
+      .catch(error =>
+        console.log(`ERROR`, error),
+      );
   };
 
-  return <form action={`/contact/success`} method={`POST`} name={`contact`} data-netlify={true} data-netlify-recaptcha={true}>
+  return <form ref={formReference}
+               action={`/contact/success`}
+               method={`POST`}
+               name={`contact`}
+               data-netlify={true}
+               data-netlify-recaptcha={true}
+               onSubmit={event => handleSubmit(event)}>
     {formState.map(input => {
       let FormComponent = FormComponentsMapper[input.type];
 
-      return <FormComponent key={input.label} {...input} onChange={updateField} onBlur={validateField} />;
+      return <FormComponent key={input.label} {...input} onChange={handleChange} onBlur={handleBlur} />;
     })}
-    <Recaptcha ref={recaptchaReference} sitekey={process.env.GATSBY_SITE_RECAPTCHA_KEY} onChange={updateField} />
-    <button type={`submit`} onSubmit={handleSubmit}>Send</button>
+    <Recaptcha ref={recaptchaReference} sitekey={process.env.GATSBY_SITE_RECAPTCHA_KEY} />
+    <button type={`submit`} onSubmit={event => handleSubmit(event)}>Send</button>
   </form>;
 };
 
 ContactForm.propTypes = {
   formInputs: PropTypes.arrayOf(PropTypes.object).isRequired,
+  location: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 ContactForm.defaultProps = {};
