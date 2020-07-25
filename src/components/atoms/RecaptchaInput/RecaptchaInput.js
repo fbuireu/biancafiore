00dba423a08dac86b1c6b3bd473c34ba8ab051fe
Ferran Buireu <fbuireu@gmail.com>
2020-07-25 17:56:01 +0200

    Missing gathered prop fixed

--- src/components/atoms/RecaptchaInput/RecaptchaInput.js
+++ src/components/atoms/RecaptchaInput/RecaptchaInput.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Recaptcha from 'react-google-recaptcha';
 
-const RecaptchaInput = ({ isValid, errorMessage, onChange }) => <div>
+const RecaptchaInput = ({ name, isValid, errorMessage, onChange }) => <div>
   <label htmlFor={name}>
     <Recaptcha sitekey={process.env.GATSBY_SITE_RECAPTCHA_KEY} onChange={onChange} />
   </label>
@@ -10,6 +10,7 @@ const RecaptchaInput = ({ isValid, errorMessage, onChange }) => <div>
 </div>;
 
 RecaptchaInput.propTypes = {
+  name: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   isValid: PropTypes.bool.isRequired,
   errorMessage: PropTypes.string.isRequired,
