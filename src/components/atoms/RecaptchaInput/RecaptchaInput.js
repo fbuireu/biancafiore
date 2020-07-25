c408a62001a99b8f7cbaf7397d0c69106739dbe0
Ferran Buireu <fbuireu@gmail.com>
2020-07-25 18:14:48 +0200

    Custom validation inside component added

--- src/components/atoms/RecaptchaInput/RecaptchaInput.js
+++ src/components/atoms/RecaptchaInput/RecaptchaInput.js
@@ -2,12 +2,18 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Recaptcha from 'react-google-recaptcha';
 
-const RecaptchaInput = ({ name, isValid, errorMessage, onChange }) => <div>
-  <label htmlFor={name}>
-    <Recaptcha sitekey={process.env.GATSBY_SITE_RECAPTCHA_KEY} onChange={onChange} />
-  </label>
-  {!isValid && <small>{errorMessage}</small>}
-</div>;
+const RecaptchaInput = ({ name, isValid, errorMessage, onChange: updateField }) => {
+  const handleChange = value => {
+    if (value) updateField(value, `recaptcha`);
+  };
+
+  return <div>
+    <label htmlFor={name}>
+      <Recaptcha sitekey={process.env.GATSBY_SITE_RECAPTCHA_KEY} onChange={handleChange} />
+    </label>
+    {!isValid && <small>{errorMessage}</small>}
+  </div>;
+};
 
 RecaptchaInput.propTypes = {
   name: PropTypes.string.isRequired,
