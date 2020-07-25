c78849cc5d77d805e43545475146efe26ea83090
Ferran Buireu <fbuireu@gmail.com>
2020-07-25 17:49:07 +0200

    Custom recaptcha component added

---
+++ src/components/atoms/RecaptchaInput/RecaptchaInput.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import Recaptcha from 'react-google-recaptcha';
+
+const RecaptchaInput = ({ isValid, errorMessage, onChange }) => <div>
+  <label htmlFor={name}>
+    <Recaptcha sitekey={process.env.GATSBY_SITE_RECAPTCHA_KEY} onChange={onChange} />
+  </label>
+  {!isValid && <small>{errorMessage}</small>}
+</div>;
+
+RecaptchaInput.propTypes = {
+  value: PropTypes.string.isRequired,
+  isValid: PropTypes.bool.isRequired,
+  errorMessage: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+RecaptchaInput.defaultProps = {};
+
+export default RecaptchaInput;
