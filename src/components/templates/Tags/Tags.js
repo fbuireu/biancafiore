1af089b6c370f2f1c573bf552e856f3ac16eff89
Ferran Buireu <fbuireu@gmail.com>
2020-11-19 20:01:03 +0100

    Initial Algolia refinement added

--- src/components/templates/Tags/Tags.js
+++ src/components/templates/Tags/Tags.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { TAGS_SEARCH_PARAMETERS } from '../../../utils/algolia/tagsSearchParameters';
+import { TAGS_SORT_BY } from '../../../utils/algolia/tagsSortBy';
 import SEO from '../../atoms/SEO/SEO';
+import AlgoliaWrapper from '../../organisms/AlgoliaWrapper/AlgoliaWrapper';
+import ArticleHitCards from '../../organisms/ArticleHitCards/ArticleHitCards';
 import Layout from '../Layout/Layout';
 
 const Tags = props => {
+  const FILTER_PARAMETERS = {
+    SEARCH_PARAMETERS: TAGS_SEARCH_PARAMETERS,
+    SORT_BY: TAGS_SORT_BY
+  };
+
   return <Layout>
-    <SEO title="Home"/>
-    <h1>Hi people</h1>
-    <p>This will be an amazing portfolio for the best content writer ever.</p>
-    <p>Now go build something great.</p>
+    <SEO title="Blog" />
+    <section className={`wrapper`}>
+      <AlgoliaWrapper hitsComponent={ArticleHitCards}
+                      indexName={process.env.GATSBY_ALGOLIA_ARTICLES_INDEX_NAME}
+                      filterParameters={FILTER_PARAMETERS}
+                      hasRange={true} />
+    </section>
   </Layout>;
 };
-
 Tags.propTypes = {};
 
 Tags.defaultProps = {};
