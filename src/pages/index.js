738c93a4449c11b570173059f6e4f3fae6f1a39c
Ferran Buireu <fbuireu@gmail.com>
2021-01-20 17:55:41 +0100

    "Under construction" temporary statement added

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,17 @@ import SEO from '../components/atoms/SEO/SEO';
 import Layout from '../components/templates/Layout/Layout';
 
 const Index = props => {
-  return <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>This will be an amazing portfolio for the best content writer ever.</p>
-    <p>Now go build something great.</p>
-  </Layout>;
+  return <h1 style={{
+    textAlign: `center`,
+    marginTop: `50vh`,
+    fontSize: `52px`
+  }}>Site under construction<br />We&apos;ll back soon</h1>;
+  // return <Layout>
+  //   <SEO title="Home" />
+  //   <h1>Hi people</h1>
+  //   <p>This will be an amazing portfolio for the best content writer ever.</p>
+  //   <p>Now go build something great.</p>
+  // </Layout>;
 };
 
 Index.propTypes = {
