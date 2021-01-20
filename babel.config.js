d0629363eb3f23bcdc5822d171457d0ca0764c56
Ferran Buireu <fbuireu@gmail.com>
2021-01-20 17:57:22 +0100

    Major upgrade to core-js v3 added

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,8 @@ module.exports = function (api) {
   api.cache(true);
 
   const presets = [
-    [`@babel/preset-env`, { 'useBuiltIns': `usage`, 'corejs': `2` }],
-    [`@babel/preset-react`, { 'development': true, minify: true }],
+    [`@babel/preset-env`, { 'useBuiltIns': `usage`, 'corejs': `3` }],
+    [`@babel/preset-react`, { 'development': true, minify: true }]
   ];
 
   const plugins = [
