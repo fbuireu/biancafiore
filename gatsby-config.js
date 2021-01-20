7552cb24c607a09e4bf7190dfce1357e2a63ad30
Ferran Buireu <fbuireu@gmail.com>
2021-01-20 18:01:45 +0100

    Unresolved inferred GraphQL node when dealing images fixed (umbrella https://github.com/gatsbyjs/gatsby/issues/5990)

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require(`dotenv`).config({
 
 const { GATSBY_ALGOLIA_APP_ID, GATSBY_ALGOLIA_API_KEY, GATSBY_GOOGLE_ANALYTICS_ID } = process.env;
 
-const ALGOLIA_QUERIES = require(`./src/utils/algolia/queries`);
+const ALGOLIA_QUERIES = require(`./src/utils/algolia/queries/queries`);
 
 module.exports = {
   siteMetadata: {
@@ -14,22 +14,23 @@ module.exports = {
     url: `https://biancafiore.me`
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-catch-links`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-catch-links`,
     `gatsby-plugin-eslint`,
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-sass`,
+    `gatsby-plugin-netlify-cms-paths`,
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
           `Zilla Slab: 400, 400i, 700`,
-          `Montserrat: 300, 400, 700`,
+          `Montserrat: 300, 400, 700`
         ],
-        display: `swap`,
-      },
+        display: `swap`
+      }
     },
     {
       resolve: `gatsby-plugin-react-svg`,
@@ -42,23 +43,23 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`,
-      },
+        name: `uploads`,
+        path: `${__dirname}/static/assets/images`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `uploads`,
-        path: `${__dirname}/static/assets/images`,
-      },
+        name: `images`,
+        path: `${__dirname}/src/assets/images`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `languages`,
-        path: `${__dirname}/content/languages`,
-      },
+        path: `${__dirname}/content/languages`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
@@ -114,7 +115,20 @@ module.exports = {
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-relative-images`,
+            resolve: `gatsby-remark-relative-images-v2`,
+            options: {
+              name: `uploads`
+            }
+          },
+          {
+            resolve: `gatsby-plugin-netlify-cms-paths`
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+              withWebp: true
+            }
           },
           {
             resolve: `gatsby-remark-autolink-headers`,
@@ -123,15 +137,8 @@ module.exports = {
               icon: `<svg aria-hidden="true" height="20" version="1.1" viewBox="0 0 16 16" width="20"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>`,
               className: `--is-heading`,
               maintainCase: true,
-              removeAccents: true,
-            },
-          },
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 1200,
-              withWebp: true,
-            },
+              removeAccents: true
+            }
           },
           {
             resolve: `gatsby-remark-highlight-code`
