cd547fcbb157a4208416bf67012df80891d70a7e
Ferran Buireu <fbuireu@gmail.com>
2021-01-27 11:47:56 +0100

    Hamburger menu component isolation added

---
+++ src/components/atoms/HamburgerMenu/HamburgerMenu.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import Hamburger from '../../../assets/svg/menu.svg';
+import './HamburgerMenu.scss';
+
+const HamburgerMenu = ({ toggleMenuVisibility, isMenuActive }) => {
+  return <Hamburger className={`hamburger-menu ${isMenuActive ? `--is-active` : ``}`} onClick={toggleMenuVisibility} />;
+};
+
+HamburgerMenu.propTypes = {
+  toggleMenuVisibility: PropTypes.func.isRequired,
+  isMenuActive: PropTypes.bool.isRequired
+};
+
+HamburgerMenu.defaultProps = {};
+
+export default HamburgerMenu;
