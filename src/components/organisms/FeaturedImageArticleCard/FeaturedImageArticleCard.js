360c6316220cb6b7801dfe13e459cbc2c25ffdca
Ferran Buireu <fbuireu@gmail.com>
2021-02-03 18:02:32 +0100

    Navigation component what detects the current available links to display a navigation added

--- src/components/organisms/FeaturedImageArticleCard/FeaturedImageArticleCard.js
+++ src/components/organisms/FeaturedImageArticleCard/FeaturedImageArticleCard.js
@@ -20,11 +20,13 @@ const FeaturedImageArticleCard = article => {
                          article?.content?.featuredImage?.childImageSharp?.fluid]}>
         <Link to={`/${currentLanguage}/blog${article.fields.slug}`}
               className={`hit-card__link`}>
-          <HitTitle hit={article} attribute={`content.title`} />
-          <HitSubtitle hit={article} attribute={`content.lastUpdated`} hasAuthor={true} />
-          <ReadingTime readingTime={article.content.readingTime} classNames={`hit-card__reading-time`} />
-          <HitTags hit={article} attribute={`content.tags`} />
-          <Summary summary={article.content.summary || article.excerpt} classNames={`hit-card__summary`} />
+          <header>
+            <HitTitle hit={article} attribute={`content.title`} />
+            <HitSubtitle hit={article} attribute={`content.lastUpdated`} hasAuthor={true} />
+            <ReadingTime readingTime={article.content.readingTime} classNames={`hit-card__reading-time`} />
+            <HitTags hit={article} attribute={`content.tags`} />
+            <Summary summary={article.content.summary || article.excerpt} classNames={`hit-card__summary`} />
+          </header>
         </Link>
       </BackgroundImage>
     </article>
