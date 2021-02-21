f9c92697de73941eaf0e516ea3b7f8f6a4c513b7
Ferran Buireu <fbuireu@gmail.com>
2021-02-21 12:21:07 +0100

    Initial layout for isolated testimonial component added

--- src/components/molecules/Testimonials/Testimonials.js
+++ src/components/molecules/Testimonials/Testimonials.js
@@ -1,22 +1,29 @@
+import Markdown from 'markdown-to-jsx';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper.scss';
 import Testimonial from '../../atoms/Testimonial/Testimonial';
+import './Testimonials.scss';
 
-const Testimonials = ({ testimonials }) => {
-  return <Swiper>
-    {testimonials.map(testimonial => {
-      return <SwiperSlide key={testimonial.name}>
-        <Testimonial {...testimonial} />
-      </SwiperSlide>;
-    })
-    }
-  </Swiper>;
+const Testimonials = ({ title, subtitle, testimonials }) => {
+  return <section className={`testimonials__wrapper`}>
+    <h2>{title}</h2>
+    <Markdown>{subtitle}</Markdown>
+    <Swiper>
+      {testimonials.map(testimonial => {
+        return <SwiperSlide key={testimonial.name}>
+          <Testimonial {...testimonial} />
+        </SwiperSlide>;
+      })}
+    </Swiper>
+  </section>;
 };
 
 Testimonials.propTypes = {
-  testimonials: PropTypes.arrayOf(PropTypes.object).isRequired
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  testimonials: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 Testimonials.defaultProps = {};
