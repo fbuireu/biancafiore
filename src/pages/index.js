056d4d7e836f47771a80fcc2aa4321fbf8678df5
Ferran Buireu <fbuireu@gmail.com>
2021-02-21 12:18:17 +0100

    GraphQL queries adaptation to new CMS structure added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,16 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const Index = ({ data }) => {
-  const { home: { edges: [{ node: { html, frontmatter: { image: bianca, testimonials } } }] } } = data;
+  const {
+    home: {
+      edges: [{
+        node: {
+          html,
+          frontmatter: { jumbotron, testimonials: { title, subtitle, testimonials } }
+        }
+      }]
+    }
+  } = data;
 
   return <h1 style={{
     textAlign: `center`,
@@ -11,52 +20,55 @@ const Index = ({ data }) => {
     fontSize: `52px`
   }}>Site under construction<br />We&apos;ll be back soon</h1>;
 
-
   // return <Layout>
-  //   <SEO title="Home" />
+  //   <SEO title="Home"/>
   //   <h1>Hi people</h1>
-  //   <Markdown>{html}</Markdown>
-  //   <Testimonials testimonials={testimonials} />
-  //   {/*<Tilt gyroscope={true} tiltMaxAngleX={5} tiltMaxAngleY={5}>*/}
-  //   {/*  <Img fluid={bianca.childImageSharp?.fluid} />*/}
-  //   {/*</Tilt>*/}
+  //   <Jumbotron jumbotron={jumbotron}/>
+  //   <Testimonials title={title} subtitle={subtitle} testimonials={testimonials}/>
   // </Layout>;
 };
 
 export const homeData = graphql`
-  query getHomeData {
-    home: allMarkdownRemark(filter: {frontmatter: {key: {eq: "home"}}}) {
-      edges {
-        node {
-          html
-          frontmatter {
-            title
-#            jumbotron{
-#              welcomeImage {
-#                childImageSharp {
-#                  fixed(width: 400, height: 400) {
-#                    ...GatsbyImageSharpFixed_withWebp
-#                  }
-#                }
-#              }
-#            }
-            testimonials {
-              author
-              quote
-              description
-              image {
-                childImageSharp {
-                  fluid {
-                    ...GatsbyImageSharpFluid
-                  }
+    query getHomeData {
+        home: allMarkdownRemark(filter: {frontmatter: {key: {eq: "home"}}}) {
+            edges {
+                node {
+                    html
+                    frontmatter {
+                        title
+                        jumbotron{
+                            welcomeDescription
+                            welcomeTextLeft
+                            welcomeTextRight
+                            welcomeImage {
+                                childImageSharp {
+                                    fixed(width: 400, height: 400) {
+                                        ...GatsbyImageSharpFixed_withWebp
+                                    }
+                                }
+                            }
+                        }
+                        testimonials {
+                            title
+                            subtitle
+                            testimonials {
+                                author
+                                quote
+                                description
+                                image {
+                                    childImageSharp {
+                                        fluid {
+                                            ...GatsbyImageSharpFluid
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
-              }
             }
-          }
         }
-      }
     }
-  }
 `;
 
 Index.propTypes = {
