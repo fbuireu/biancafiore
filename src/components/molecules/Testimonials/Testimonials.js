3495bc81fc61065ede7f60326012759ffe7f6d23
Ferran Buireu <fbuireu@gmail.com>
2021-03-01 18:33:02 +0100

    Testimonials component (slider) added

--- src/components/molecules/Testimonials/Testimonials.js
+++ src/components/molecules/Testimonials/Testimonials.js
@@ -7,13 +7,34 @@ import Testimonial from '../../atoms/Testimonial/Testimonial';
 import './Testimonials.scss';
 
 const Testimonials = ({ title, subtitle, testimonials }) => {
+  const SLIDER_PARAMETERS = {
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: true
+    },
+    centeredSlides: true,
+    keyboard: {
+      enabled: true
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 1.85,
+        spaceBetween: 150
+      },
+      320: {
+        slidesPerView: 1
+      }
+    }
+  };
+
   return <section className={`testimonials__wrapper`}>
-    <h2>{title}</h2>
-    <Markdown>{subtitle}</Markdown>
-    <Swiper>
+    <h2 className={`testimonials__title`}>{title}</h2>
+    <Markdown className={`testimonials__subtitle`}>{subtitle}</Markdown>
+    <Swiper {...SLIDER_PARAMETERS} className={`wrapper`}>
       {testimonials.map(testimonial => {
         return <SwiperSlide key={testimonial.name}>
-          <Testimonial {...testimonial} />
+          {({ isActive }) => <Testimonial {...testimonial} isActive={isActive} />}
         </SwiperSlide>;
       })}
     </Swiper>
