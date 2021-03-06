48bc2250b65b44f52d8494ea0fb808974c65a60a
Ferran Buireu <fbuireu@gmail.com>
2021-03-06 18:10:52 +0100

    Custom hook to get latest articles from CMS to isolate the page query component

---
+++ src/utils/hooks/useLatestArticles.js
@@ -0,0 +1,46 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+export const useLatestArticles = () => {
+  const { latestArticles } = useStaticQuery(graphql`
+      query getLatestArticles {
+          latestArticles: allMarkdownRemark(
+              filter: {
+                  frontmatter: {
+                      key: { eq: "article" },
+                      isDraft: { eq: false }
+                  }
+              },
+              sort: {
+                  fields: frontmatter___content___publishDate,
+                  order: DESC
+              }) {
+              edges {
+                  node {
+                      fields {
+                          slug
+                      }
+                      frontmatter {
+                          author
+                          content {
+                              title
+                              publishDate
+                              featuredImage {
+                                  childImageSharp {
+                                      fluid {
+                                          ...GatsbyImageSharpFluid
+                                      }
+                                  }
+                              }
+                          }
+                          content {
+                              tags
+                          }
+                      }
+                  }
+              }
+          }
+      }
+  `);
+
+  return latestArticles.edges;
+};
