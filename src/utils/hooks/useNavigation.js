dfd014196c6f8bc8404a0b1e9176cd0dfafffc77
Ferran Buireu <fbuireu@gmail.com>
2021-03-07 18:14:05 +0100

    Format fixed

--- src/utils/hooks/useNavigation.js
+++ src/utils/hooks/useNavigation.js
@@ -1,21 +1,21 @@
 import { graphql, useStaticQuery } from 'gatsby';
 
 export const useNavigation = () => {
-    const { navigation } = useStaticQuery(graphql`
-        query getAllNavigationData {
-            navigation: allMarkdownRemark(
-                filter: {frontmatter: {key: {eq: "navigation" }}}) {
-                edges {
-                    node {
-                        html
-                        frontmatter {
-                            menuItems
-                        }
-                    }
-                }
-            }
-        }
+  const { navigation } = useStaticQuery(graphql`
+      query getAllNavigationData {
+          navigation: allMarkdownRemark(
+              filter: { frontmatter: { key: {eq: "navigation" }}}) {
+              edges {
+                  node {
+                      html
+                      frontmatter {
+                          menuItems
+                      }
+                  }
+              }
+          }
+      }
     `);
 
-    return navigation.edges;
+  return navigation.edges;
 };
