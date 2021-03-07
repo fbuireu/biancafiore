d3f5ad1218c013de25f63f3a48d59b990db10681
Ferran Buireu <fbuireu@gmail.com>
2021-03-07 17:56:15 +0100

    Implicit return fixed

--- src/components/atoms/Tag/Tag.js
+++ src/components/atoms/Tag/Tag.js
@@ -10,15 +10,15 @@ const Tag = ({ tags }) => {
   const { locale: currentLanguage } = useIntl();
   
   return <ul className={`article__tags__list`}>
-    {tags.map(tag =>
+    {tags.map(tag => (
       <li className={`article__tag__item`} key={tag}>
         <Link to={`/${currentLanguage}/tag/${slugify(tag)}`}
               className={`article__tag__item__link`}>
           <Label className={`article__tag__item__label`}/>
           {tag}
         </Link>
-      </li>)
-    }
+      </li>
+    ))}
   </ul>;
 };
 
