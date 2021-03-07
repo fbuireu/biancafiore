ee3fa5adafde7a18e9c975e7d3f8bd3a0cc69332
Ferran Buireu <fbuireu@gmail.com>
2021-03-07 17:57:23 +0100

    Moment locale global set

--- src/components/molecules/LanguageSwitcher/LanguageSwitcher.js
+++ src/components/molecules/LanguageSwitcher/LanguageSwitcher.js
@@ -1,28 +1,34 @@
 import { changeLocale, FormattedMessage, useIntl } from 'gatsby-plugin-intl';
+import moment from 'moment';
 import React, { useState } from 'react';
 import ArrowDown from '../../../assets/svg/arrow-down.svg';
 import { useLanguages } from '../../../utils/hooks/useLanguages';
 import './LanguageSwitcher.scss';
 
 const LanguageSwitcher = () => {
-  const [isMenuSelectorOpen, setIsMenuSelectorOpen] = useState(false),
-    languages = useLanguages();
+  const [isMenuSelectorOpen, setIsMenuSelectorOpen] = useState(false);
+  const languages = useLanguages();
   const { locale: currentLanguage } = useIntl();
 
-  const handleMenuSelector = () => setIsMenuSelectorOpen(!isMenuSelectorOpen);
+  const handleMenuSelector = () => {
+    setIsMenuSelectorOpen(!isMenuSelectorOpen);
+  };
 
-  const handleLanguage = language => changeLocale(language);
+  const handleLanguage = language => {
+    changeLocale(language);
+    moment.locale(language);
+  };
 
   return <li className={`language-switcher__wrapper ${isMenuSelectorOpen ? `--is-open` : ``}`}>
     <span onClick={handleMenuSelector} className={`language-switcher__icon`}>
-      <FormattedMessage id={`global.changeLanguage`} />
-      <ArrowDown className={`arrow-down`} />
+      <FormattedMessage id={`global.changeLanguage`}/>
+      <ArrowDown className={`arrow-down`}/>
     </span>
     <ul className={`language-switcher__list`}>
       {languages.map(language => isMenuSelectorOpen &&
         <li className={`language-switcher__item ${currentLanguage === language.isoCode ? `--is-current-language` : ``}`}
             onClick={() => handleLanguage(language.isoCode)}>
-          <FormattedMessage id={`global.${language.isoCode}`} />
+          <FormattedMessage id={`global.${language.isoCode}`}/>
         </li>
       )}
     </ul>
