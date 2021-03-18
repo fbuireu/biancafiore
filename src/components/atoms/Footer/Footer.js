f975e6c48be5a1a95d0be8712abdece6e5245643
Ferran Buireu <fbuireu@gmail.com>
2021-03-18 16:30:15 +0100

    Footer component with initial layout added

---
+++ src/components/atoms/Footer/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { IS_VALID_EMAIL } from '../../../utils/form/validateField';
+import { useFooterItems } from '../../../utils/hooks/useFooterItems';
+import './Footer.scss';
+
+const Footer = () => {
+  const footerItems = useFooterItems();
+
+  return <section className={`footer-wrapper`}>
+    <ul className={`footer__list wrapper`}>
+      {footerItems.map(({ node: footerItem }) => (
+        <li key={footerItem.frontmatter.name} className={`footer__item`}>
+          {footerItem.frontmatter.url.match(IS_VALID_EMAIL)
+            ? <a href={`mailto:${footerItem.frontmatter.url}`} className={`footer__link`}>{footerItem.frontmatter.name}</a>
+            : <a href={footerItem.frontmatter.url}
+                 target={`_blank`}
+                 className={`footer__link`}
+                 rel={`noopener noreferrer`}>{footerItem.frontmatter.name}</a>
+          }
+        </li>
+      ))}
+    </ul>
+  </section>;
+};
+
+Footer.propTypes = {};
+
+Footer.defaultProps = {};
+
+export default Footer;
