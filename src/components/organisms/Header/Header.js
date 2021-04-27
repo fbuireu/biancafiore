1f24746f8cb0eb9ec27262cfbc4ae7bff86e4169
Ferran Buireu <fbuireu@gmail.com>
2021-04-27 17:13:05 +0200

    Fade animation when scrolling added

--- src/components/organisms/Header/Header.js
+++ src/components/organisms/Header/Header.js
@@ -1,18 +1,26 @@
 import React, { useState } from 'react';
+import { useScrollPosition } from '../../../utils/hooks/useScrollPosition';
 import HamburgerMenu from '../../atoms/HamburgerMenu/HamburgerMenu';
 import Navigation from '../Navigation/Navigation';
 import './Header.scss';
 
 const Header = () => {
   const [isMenuActive, setIsMenuActive] = useState(false);
+  const [isScrolling, setIsScrolling] = useState(false);
 
   const toggleMenuVisibility = () => setIsMenuActive(!isMenuActive);
 
-  return <header>
+  useScrollPosition(function setScrollPosition ({ currentPosition }) {
+    let { y: currentVerticalYPosition } = currentPosition;
+
+    setIsScrolling(Math.abs(currentVerticalYPosition) > 0);
+  });
+
+  return <header className={`${isScrolling ? `--is-scrolling` : ``}`}>
     <section className={`wrapper`}>
-      Logo
-      <HamburgerMenu toggleMenuVisibility={toggleMenuVisibility} isMenuActive={isMenuActive}/>
-      <Navigation isMenuActive={isMenuActive} toggleMenuVisibility={toggleMenuVisibility}/>
+      <span>Logo</span>
+      <HamburgerMenu toggleMenuVisibility={toggleMenuVisibility} isMenuActive={isMenuActive} />
+      <Navigation toggleMenuVisibility={toggleMenuVisibility} isMenuActive={isMenuActive} />
     </section>
   </header>;
 };
