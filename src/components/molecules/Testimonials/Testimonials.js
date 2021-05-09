949f1c8a717a6c5863ee29c69d0815b5f71c684d
Ferran Buireu <fbuireu@gmail.com>
2021-05-09 10:59:24 +0200

    Swiper core for navigation installed

--- src/components/molecules/Testimonials/Testimonials.js
+++ src/components/molecules/Testimonials/Testimonials.js
@@ -1,14 +1,18 @@
 import Markdown from 'markdown-to-jsx';
 import PropTypes from 'prop-types';
 import React from 'react';
+import SwiperCore, { Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper.scss';
 import Testimonial from '../../atoms/Testimonial/Testimonial';
 import './Testimonials.scss';
 
+SwiperCore.use([Navigation]);
+
 const Testimonials = ({ title, subtitle, testimonials }) => {
   const SLIDER_PARAMETERS = {
     loop: true,
+    navigation: true,
     autoplay: {
       delay: 5000,
       disableOnInteraction: true
@@ -31,7 +35,7 @@ const Testimonials = ({ title, subtitle, testimonials }) => {
   return <section className={`testimonials__wrapper`}>
     <h2 className={`testimonials__title`}>{title}</h2>
     <Markdown className={`testimonials__subtitle`}>{subtitle}</Markdown>
-    <Swiper {...SLIDER_PARAMETERS} className={`wrapper`}>
+    <Swiper {...SLIDER_PARAMETERS} className={`testimonials__slider wrapper`}>
       {testimonials.map(testimonial => {
         return <SwiperSlide key={testimonial.name}>
           {({ isActive }) => <Testimonial {...testimonial} isActive={isActive} />}
