931ef36c2639b02e20c9957e0a7e1f4f58ba1464
Ferran Buireu <fbuireu@gmail.com>
2021-05-09 10:55:11 +0200

    Folder structure for SVG components refactored

--- src/components/atoms/ReadingTime/ReadingTime.js
+++ src/components/atoms/ReadingTime/ReadingTime.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import Clock from '../../../assets/svg/clock.svg';
+import Clock from '../../../assets/svg-components/clock.svg';
 import './ReadingTime.scss';
 
 const ReadingTime = ({ readingTime,classNames }) => <div className={classNames}><Clock /><span>{readingTime} min</span></div>;
