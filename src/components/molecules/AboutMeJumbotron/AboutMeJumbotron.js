e64c63630b83af2f702fa5d4ad5b113e1ccf720c
Ferran Buireu <fbuireu@gmail.com>
2021-05-11 20:23:24 +0200

    Jumbotron components added

--- src/components/molecules/AboutMeJumbotron/AboutMeJumbotron.js
+++ src/components/molecules/AboutMeJumbotron/AboutMeJumbotron.js
@@ -1,5 +1,7 @@
 import { Link } from 'gatsby';
 import Img from 'gatsby-image';
+import { useIntl } from 'gatsby-plugin-intl';
+import Markdown from 'markdown-to-jsx';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useSocialNetworkItems } from '../../../utils/hooks/useSocialNetworkItems';
@@ -20,29 +22,34 @@ const AboutMeJumbotron = ({
   }
 }) => {
   const allSocialNetworkItems = useSocialNetworkItems();
+  const { locale: currentLanguage } = useIntl();
+
   let availableSocialNetworks = { names: [], urls: [] };
 
   allSocialNetworkItems.map(({ node: socialNetworkItem }) => {
-    if (socialNetworks.includes(socialNetworkItem.frontmatter.name)) {
-      availableSocialNetworks.names.push(socialNetworkItem.frontmatter.name);
-      availableSocialNetworks.urls.push(socialNetworkItem.frontmatter.url);
+    let { frontmatter: { name, url } } = socialNetworkItem;
+
+    if (socialNetworks.includes(name)) {
+      availableSocialNetworks.names.push(name);
+      availableSocialNetworks.urls.push(url);
     }
 
     return availableSocialNetworks;
   });
 
-  return <section className={`wrapper`}>
-    {title}
-    {welcomeText}
-    {welcomeDescription}
-    {cta}
-
-    <Img fixed={bianca?.childImageSharp?.fixed} />
-    <ul>
+  return <section className={`about-me__jumbotron__wrapper wrapper`}>
+    <h1 className={`about-me__jumbotron__title`}>{title}</h1>
+    <Markdown className={`about-me__jumbotron__header`}>{welcomeText}</Markdown>
+    <Markdown className={`about-me__jumbotron__description`}>{welcomeDescription}</Markdown>
+    <Link className={`about-me__jumbotron__cta`} to={`/${currentLanguage}/blog`}>{cta}</Link>
+    <Img className={`about-me__jumbotron__image`} fixed={bianca?.childImageSharp?.fixed} />
+    <ul className={`about-me__jumbotron__social-networks__list`}>
       {availableSocialNetworks.urls.map((url, index) => {
         let name = availableSocialNetworks.names[index];
 
-        return <li key={name}><Link to={url}>{name}</Link></li>;
+        return <li key={name} className={`about-me__jumbotron__social-network__item`}>
+          <a className={`about-me__jumbotron__social-network__link`} href={url} target={`_blank`} rel={`noopener noreferer`}>{name}</a>
+        </li>;
       })}
     </ul>
   </section>;
