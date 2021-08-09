20a69bdddb91f7f483cb723d29d5e6e7836efc85
Ferran Buireu <fbuireu@gmail.com>
2021-08-09 17:44:51 +0200

    Homepage restored

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,5 @@
 import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
-import SEO from '../components/atoms/SEO/SEO';
-import HomeJumbotron from '../components/molecules/HomeJumbotron/HomeJumbotron';
-import HomeLatestArticles from '../components/molecules/HomeLatestArticles/HomeLatestArticles';
-import MyWork from '../components/molecules/MyWork/MyWork';
-import Testimonials from '../components/molecules/Testimonials/Testimonials';
-import Layout from '../components/templates/Layout/Layout';
 
 const Index = ({
   data: {
@@ -25,19 +18,19 @@ const Index = ({
   }
 }) => {
 
-  // return <h1 style={{
-  //   textAlign: `center`,
-  //   marginTop: `50vh`,
-  //   fontSize: `52px`
-  // }}>Site under construction<br />We&apos;ll be back soon</h1>;
+  return <h1 style={{
+    textAlign: `center`,
+    marginTop: `50vh`,
+    fontSize: `52px`
+  }}>Site under construction<br />We&apos;ll be back soon</h1>;
 
-  return <Layout>
-    <SEO title="Home" />
-    <HomeJumbotron jumbotron={jumbotron} />
-    <Testimonials title={testimonialsTitle} subtitle={subtitle} testimonials={testimonials} />
-    <MyWork title={myWorkTitle} works={works} />
-    <HomeLatestArticles title={latestArticlesTitle} />
-  </Layout>;
+  // return <Layout>
+  //   <SEO title="Home" />
+  //   <HomeJumbotron jumbotron={jumbotron} />
+  //   <Testimonials title={testimonialsTitle} subtitle={subtitle} testimonials={testimonials} />
+  //   <MyWork title={myWorkTitle} works={works} />
+  //   <HomeLatestArticles title={latestArticlesTitle} />
+  // </Layout>;
 };
 
 export const homeData = graphql`
