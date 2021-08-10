5134c1f9303bbcae7e366bd82bcc5f93103c62c4
Ferran Buireu <fbuireu@gmail.com>
2021-08-10 17:39:03 +0200

    CSS minimal Masonry layout added

--- src/components/organisms/ArticleHitCard/ArticleHitCard.js
+++ src/components/organisms/ArticleHitCard/ArticleHitCard.js
@@ -1,15 +1,27 @@
 import PropTypes from 'prop-types';
 import { connectHits } from 'react-instantsearch-dom';
+import Masonry from 'react-masonry-css';
 import FeaturedImageArticleCard from '../FeaturedImageArticleCard/FeaturedImageArticleCard';
 import SimpleArticleCard from '../SimpleArticleCard/SimpleArticleCard';
 import './ArticleHitCard.scss';
 
 const CustomHit = ({ hits: articles }) => {
+  const BREAKPOINT_COLUMNS = {
+    default: 3,
+    760: 2,
+    500: 1
+  };
+
   return <ul className={`articles-card__list`}>
-    {articles.map(article => article.content.featuredImage
-      ? <FeaturedImageArticleCard key={article.content.title} {...article} />
-      : <SimpleArticleCard key={article.content.title} {...article} />
-    )}
+    <Masonry breakpointCols={BREAKPOINT_COLUMNS}
+             className={`articles-card__list__masonry`}
+             columnClassName={`articles-card__list__item`}
+    >
+      {articles.map(article => article.content.featuredImage
+        ? <FeaturedImageArticleCard key={article.content.title} {...article} />
+        : <SimpleArticleCard key={article.content.title} {...article} />
+      )}
+    </Masonry>
   </ul>;
 };
 
