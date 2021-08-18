df2f5ad2017f6a974d122d426e18bd8540e07a27
Ferran Buireu <fbuireu@gmail.com>
2021-08-18 18:04:07 +0200

    Initial simple card layout added

--- src/components/organisms/SimpleArticleCard/SimpleArticleCard.js
+++ src/components/organisms/SimpleArticleCard/SimpleArticleCard.js
@@ -1,22 +1,49 @@
 import { Link } from 'gatsby';
 import { useIntl } from 'gatsby-plugin-intl';
+import moment from 'moment/moment';
 import PropTypes from 'prop-types';
+import { Highlight } from 'react-instantsearch-dom';
+import slugify from '../../../utils/slugify/slugify';
 import HitTitle from '../../atoms/HitTitle/HitTitle';
 import Summary from '../../atoms/Summary/Summary';
-// import HitTags from '../../molecules/HitTags/HitTags';
+import './SimpleArticleCard.scss';
 
 const SimpleArticleCard = article => {
   const { locale: currentLanguage } = useIntl();
 
-  return <article className={`article-card__item ${article.content.isFeaturedArticle ? `--is-featured` : ``}`}>
+  return <article className={`simple-hit-card__item ${article?.content?.isFeaturedArticle ? `--is-featured` : ``}`}>
+    <div className={`hit-card__information`}>
+      <span className={`hit-card__information__reading-time`}>{article.content.readingTime} min.</span>
+      <ul className={`hit-card__information__tags__list`}>
+        {article.content.tags.map((tag, index) => (
+          <li className={`hit-card__information__tag__item`} key={tag}>
+            <Link to={`/${currentLanguage}/tag/${slugify(tag)}`}
+                  className={`hit-card__information__tag__item__link`}>
+              #<Highlight attribute={`content.tags[${index}]`} hit={article} tagName={`mark`} />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
     <Link to={`/${currentLanguage}/blog${article.fields.slug}`}
-          className={`article-card__link`}>
-      <header>
-        <HitTitle hit={article} attribute={`content.title`} />
-        {/*<HitTags hit={article} attribute={`content.tags`} />*/}
-        <Summary summary={article.content.summary ?? article.excerpt} classNames={`hit-card__summary`} />
-      </header>
+          className={`hit-card__link`}>
+      <HitTitle hit={article} attribute={`content.title`} />
     </Link>
+    <div className={`hit-card-subtitle`}>
+      <Link to={`/${currentLanguage}/tag/${slugify(article.author)}`}
+            className={`hit-card-subtitle__author`}>
+        <Highlight attribute={`author`} hit={article} tagName={`mark`} />
+      </Link>
+      <time dateTime={article.content.publishDate} className={`hit-card-subtitle__publish-date`}>
+        {moment(new Date(article.content.publishDate))
+          .locale(currentLanguage)
+          .format(`LL`)
+        }
+      </time>
+    </div>
+    <Summary summary={article.content.summary ?? article.excerpt} classNames={`hit-card__summary`} />
+    <Link to={`/${currentLanguage}/blog${article.fields.slug}`}
+          className={`hit-card__cta`}>Read more</Link>
   </article>;
 };
 
