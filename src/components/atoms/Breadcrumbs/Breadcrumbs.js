2c355e31afefd4b4aad15b968b6a757096c14521
Ferran Buireu <fbuireu@gmail.com>
2021-08-19 18:03:40 +0200

    Custom breadcrumb component with custom default label added

--- src/components/atoms/Breadcrumbs/Breadcrumbs.js
+++ src/components/atoms/Breadcrumbs/Breadcrumbs.js
@@ -2,49 +2,50 @@ import { Link } from 'gatsby';
 import { useIntl } from 'gatsby-plugin-intl';
 import path from 'path';
 import PropTypes from 'prop-types';
+import { capitalizy } from '../../../utils/capitalizy/capitalizy';
+import './Breadcrumbs.scss';
 
-const homePagePath = [`/`];
+const HOMEPAGE_PATH = [`/`];
 
-const Breadcrumbs = ({ location }) => {
+const Breadcrumbs = ({ location, breadcrumbLabel = null }) => {
   const { locale: currentLanguage } = useIntl();
-  let linkPath = ``;
 
-  const breadcrumbs = homePagePath.concat(location.pathname.split(`/`).filter(routeName => routeName !== `` && routeName !== currentLanguage));
+  let linkPath = ``;
+  const breadcrumbs = HOMEPAGE_PATH.concat(location.pathname.split(`/`).filter(routeName => routeName !== `` && routeName !== currentLanguage));
 
   const lastPage = breadcrumbs.length > 0 ? breadcrumbs.length - 1 : 0;
 
-  function capitalizeSlug(slug) {
-    return slug
-      .split(`-`)
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(` `);
-  }
-
   return (
-    <nav aria-label={`Breadcrumb`}>
-      {breadcrumbs.map((page, index) => {
-        linkPath = path.join(index === 0 ? `/${currentLanguage}/${linkPath}` : `/${linkPath}`, `${page}/`);
-        page = page === `/` ? `Home` : capitalizeSlug(page);
+    <nav aria-label={`Breadcrumb`} className={`breadcrumbs`}>
+      <ol className={`breadcrumbs__list`}>
+        {breadcrumbs.map((breadcrumb, index) => {
+          linkPath = path.join(index === 0 ? `/${currentLanguage}/${linkPath}` : `/${linkPath}`, `${breadcrumb}/`);
+          let isSameName = capitalizy(breadcrumb).toUpperCase() === breadcrumbLabel?.toUpperCase();
+          let isHome = breadcrumb === `/`;
 
-        return (
-          <ol key={page}>
+          if (isHome) breadcrumb = `Home`;
+          else if (isSameName) breadcrumb = breadcrumbLabel;
+          else breadcrumb = capitalizy(breadcrumb);
+
+          return <>
             {index !== lastPage ? (
-              <li>
-                <Link to={linkPath}>{page}</Link>
-                <span aria-hidden={true}>&gt;</span>
+              <li key={breadcrumb} className={`breadcrumb__item`}>
+                <Link to={linkPath} className={`breadcrumb__item__link`}>{breadcrumb}</Link>
+                <span aria-hidden={true} className={`breadcrumb__item__separator`}>/</span>
               </li>
             ) : (
-              <li aria-current={`page`}>{page}</li>
+              <li key={breadcrumb} className={`breadcrumb__item--is-current`} aria-current={`page`}><strong>{breadcrumb}</strong></li>
             )}
-          </ol>
-        );
-      })}
+          </>;
+        })}
+      </ol>
     </nav>
   );
 };
 
 Breadcrumbs.propTypes = {
-  location: PropTypes.string.isRequired
+  location: PropTypes.string.isRequired,
+  breadcrumbLabel: PropTypes.string
 };
 
 Breadcrumbs.defaultProps = {};
