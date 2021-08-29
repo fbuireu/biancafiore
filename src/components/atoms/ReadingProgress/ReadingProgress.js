b7f27739500324f796398d07df484db102bc5fad
Ferran Buireu <fbuireu@gmail.com>
2021-08-29 12:33:01 +0200

    refactor!(render-time): scope function removed

--- src/components/atoms/ReadingProgress/ReadingProgress.js
+++ src/components/atoms/ReadingProgress/ReadingProgress.js
@@ -4,6 +4,13 @@ import ArrowUp from '../../../assets/svg-components/arrow-up.svg';
 import { useWindowSize } from '../../../utils/hooks/useWindowSize';
 import './ReadingProgress.scss';
 
+const scrollToTop = () => {
+  window.scroll({
+    top: 0,
+    behavior: `smooth`
+  });
+};
+
 const ReadingProgress = ({
   scroll,
   articleProperties: { offsetTop: articleOffsetTop, offsetHeight: articleHeight }
@@ -13,13 +20,6 @@ const ReadingProgress = ({
   const isArticleVisible = currentScroll >= articleOffsetTop;
   const currentProgress = isArticleVisible ? Math.round(
     (currentScroll - articleOffsetTop) / (articleHeight - windowHeight) * 100) : 0;
-
-  const scrollToTop = () => {
-    window.scroll({
-      top: 0,
-      behavior: `smooth`
-    });
-  };
 
   return <div className={`reading-progress ${isArticleVisible ? `--is-visible` : ``}`} onClick={scrollToTop}>
     <CircularProgressbarWithChildren className={`reading-progress__svg__wrapper`} value={currentProgress}>
