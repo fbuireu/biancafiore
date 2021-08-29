d7fb6ae0e3ffcf79407c21de9a13db8cc663a178
Ferran Buireu <fbuireu@gmail.com>
2021-08-29 12:47:09 +0200

    feat: required input attribute lifted from props added

--- src/components/atoms/TextInput/TextInput.js
+++ src/components/atoms/TextInput/TextInput.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import './TextInput.scss';
 
-const TextInput = ({ name, type, label, value, isValid, errorMessage, onChange, onBlur }) => {
+const TextInput = ({ name, type, label, value, isValid, errorMessage, isRequired, onChange, onBlur }) => {
   return <div className={`text-input__wrapper ${type === `hidden` ? `--is-hidden` : ``}`}>
     <label className={`text-input__label`} htmlFor={name}>
       <p className={`text-input__label__text`}>{label && `${label}:`}</p>
@@ -12,6 +12,7 @@ const TextInput = ({ name, type, label, value, isValid, errorMessage, onChange, 
              spellCheck={true}
              onChange={onChange}
              onBlur={onBlur}
+             required={isRequired}
       />
     </label>
     {!isValid && <small className={`text-input__error-message`}>{errorMessage}</small>}
@@ -25,6 +26,7 @@ TextInput.propTypes = {
   value: PropTypes.string.isRequired,
   isValid: PropTypes.bool.isRequired,
   errorMessage: PropTypes.string.isRequired,
+  isRequired: PropTypes.bool.isRequired,
   onChange: PropTypes.func.isRequired,
   onBlur: PropTypes.func.isRequired,
 };
--- src/components/atoms/TextareaInput/TextareaInput.js
+++ src/components/atoms/TextareaInput/TextareaInput.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import './TextareaInput.scss';
 
-const TextareaInput = ({ name, label, value, isValid, errorMessage, onChange, onBlur }) => {
+const TextareaInput = ({ name, label, value, isValid, errorMessage, isRequired, onChange, onBlur }) => {
   return <div className={`textarea-input__wrapper`}>
     <label className={`textarea-input__label`} htmlFor={name}>
       <p className={`textarea-input__label__text`}>{label && `${label}:`}</p>
@@ -11,6 +11,7 @@ const TextareaInput = ({ name, label, value, isValid, errorMessage, onChange, on
                 spellCheck={true}
                 onChange={onChange}
                 onBlur={onBlur}
+                required={isRequired}
       />
     </label>
     {!isValid && <small className={`textarea-input__error-message`}>{errorMessage}</small>}
@@ -23,8 +24,9 @@ TextareaInput.propTypes = {
   value: PropTypes.string.isRequired,
   isValid: PropTypes.bool.isRequired,
   errorMessage: PropTypes.string.isRequired,
+  isRequired: PropTypes.bool.isRequired,
   onChange: PropTypes.func.isRequired,
-  onBlur: PropTypes.func.isRequired,
+  onBlur: PropTypes.func.isRequired
 };
 
 TextareaInput.defaultProps = {};
