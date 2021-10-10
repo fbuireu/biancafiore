dc60c5a0c7f58ee7355e5abcfe939c1866992e1b
Ferran Buireu <fbuireu@gmail.com>
2021-10-10 12:19:59 +0200

    fix(runtime): fix static variables outside scope

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import SEO from '../components/atoms/SEO/SEO';
 import AlgoliaWrapper from '../components/organisms/AlgoliaWrapper/AlgoliaWrapper';
 import ProjectHitCards from '../components/organisms/ProjectHitCards/ProjectHitCards';
@@ -6,12 +5,12 @@ import Layout from '../components/templates/Layout/Layout';
 import { PROJECTS_SEARCH_PARAMETERS } from '../utils/algolia/config/projectsSearchParameters';
 import { PROJECTS_SORT_BY } from '../utils/algolia/config/projectsSortBy';
 
-const Projects = ({ location }) => {
-  const FILTER_PARAMETERS = {
-    SEARCH_PARAMETERS: PROJECTS_SEARCH_PARAMETERS,
-    SORT_BY: PROJECTS_SORT_BY
-  };
+const FILTER_PARAMETERS = {
+  SEARCH_PARAMETERS: PROJECTS_SEARCH_PARAMETERS,
+  SORT_BY: PROJECTS_SORT_BY
+};
 
+const Projects = () => {
   return <Layout>
     <SEO title="Projects" />
     <section className={`wrapper`}>
@@ -23,9 +22,7 @@ const Projects = ({ location }) => {
   </Layout>;
 };
 
-Projects.propTypes = {
-  location: PropTypes.objectOf(PropTypes.object).isRequired
-};
+Projects.propTypes = {};
 
 Projects.defaultProps = {};
 
