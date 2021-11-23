db4aa3daaef53095df52cad3caad4bb8c29eee92
Ferran Buireu <fbuireu@gmail.com>
2021-11-23 16:45:01 +0100

    refactor: simplified component structure

--- src/components/organisms/ProjectHitCard/ProjectHitCard.js
+++ src/components/organisms/ProjectHitCard/ProjectHitCard.js
@@ -1,20 +1,16 @@
 import PropTypes from 'prop-types';
 import { connectHits } from 'react-instantsearch-dom';
 import FeaturedImageProjectCard from '../FeaturedImageProjectCard/FeaturedImageProjectCard';
-import SimpleProjectCard from '../SimpleProjectCard/SimpleProjectCard';
 import './ProjectHitCard.scss';
 
 const CustomHit = ({ hits: projects }) => {
   return <ul className={`projects-card__list`}>
-    {projects.map(project => project.content.featuredImage
-      ? <FeaturedImageProjectCard key={project.content.title} {...project} />
-      : <SimpleProjectCard key={project.content.title} {...project} />,
-    )}
+    {projects.map(project => <FeaturedImageProjectCard key={project.content.title} {...project} />)}
   </ul>;
 };
 
 CustomHit.propTypes = {
-  hits: PropTypes.objectOf(PropTypes.object),
+  hits: PropTypes.objectOf(PropTypes.object)
 };
 
 CustomHit.defaultProps = {};
