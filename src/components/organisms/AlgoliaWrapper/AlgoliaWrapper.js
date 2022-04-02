7eb39b282f821778beb77f4a69fdd2fde96ffeaf
Ferran Buireu <fbuireu@gmail.com>
2022-04-02 11:15:10 +0200

    refactor: correct props drilling

--- src/components/organisms/AlgoliaWrapper/AlgoliaWrapper.js
+++ src/components/organisms/AlgoliaWrapper/AlgoliaWrapper.js
@@ -6,10 +6,10 @@ import './AlgoliaWrapper.scss';
 
 const SEARCH_CLIENT = algoliasearch(process.env.GATSBY_ALGOLIA_APP_ID, process.env.GATSBY_ALGOLIA_API_KEY);
 
-const AlgoliaWrapper = ({ hitsComponent: HitsComponent, indexName, filterParameters, hasRange }) => {
+const AlgoliaWrapper = ({ hitsComponent: HitsComponent, indexName, ...props }) => {
   return <section className={`wrapper algolia__wrapper`}>
     <InstantSearch searchClient={SEARCH_CLIENT} indexName={indexName}>
-      <Filter filterParameters={filterParameters} defaultRefinement={indexName} hasRange={hasRange} />
+      <Filter defaultRefinement={indexName} {...props} />
       <HitsComponent />
     </InstantSearch>
   </section>;
@@ -18,8 +18,6 @@ const AlgoliaWrapper = ({ hitsComponent: HitsComponent, indexName, filterParamet
 AlgoliaWrapper.propTypes = {
   hitsComponent: PropTypes.string.isRequired,
   indexName: PropTypes.string.isRequired,
-  filterParameters: PropTypes.array.isRequired,
-  hasRange: PropTypes.bool.isRequired
 };
 
 AlgoliaWrapper.defaultProps = {};
