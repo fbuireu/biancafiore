ae2b349a01829bb5cf242ab30220573e05924efe
Ferran Buireu <fbuireu@gmail.com>
2022-04-02 10:53:56 +0200

    chore: remove deprecated dependencies

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,2 @@
-import 'moment/locale/ca';
-import 'moment/locale/en-gb';
-import 'moment/locale/es';
-import 'moment/locale/it';
 import 'swiper/components/navigation/navigation.min.css';
 import './src/styles/global/global.scss';
