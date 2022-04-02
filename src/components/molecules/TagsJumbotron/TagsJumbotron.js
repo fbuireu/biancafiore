68b1c1aa0ad9791c05da27b9fd0831ee4ba4e68b
Ferran Buireu <fbuireu@gmail.com>
2022-04-02 11:16:38 +0200

    feat: add tags jumbotron

---
+++ src/components/molecules/TagsJumbotron/TagsJumbotron.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import Breadcrumbs from '../../atoms/Breadcrumbs/Breadcrumbs';
+import './TagsJumbotron.scss';
+
+const TagsJumbotron = ({ location }) => {
+  return <>
+    <h1 className={`tags__title`}>Tags Glossary</h1>
+    <Breadcrumbs location={location} classNames={`tags__jumbotron`} />
+  </>;
+};
+
+TagsJumbotron.propTypes = {
+  location: PropTypes.objectOf(PropTypes.object).isRequired
+};
+
+TagsJumbotron.defaultProps = {};
+
+export default TagsJumbotron;
