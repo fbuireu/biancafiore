67a6398566391d42d709c0ddff85bc36e98b038c
Ferran Buireu <fbuireu@gmail.com>
2022-04-02 11:08:18 +0200

    style: correct import order

--- src/components/molecules/ContactJumbotron/ContactJumbotron.js
+++ src/components/molecules/ContactJumbotron/ContactJumbotron.js
@@ -4,15 +4,14 @@ import PropTypes from 'prop-types';
 import Breadcrumbs from '../../atoms/Breadcrumbs/Breadcrumbs';
 import './ContactJumbotron.scss';
 
-const ContactJumbotron = ({ jumbotron:{welcomeText, welcomeDescription, image}, location }) => {
-  console.log("location", location);
+const ContactJumbotron = ({ jumbotron: { welcomeText, welcomeDescription, image }, location }) => {
   return <section className={`contact__jumbotron__wrapper wrapper`}>
     <Breadcrumbs classNames={`contact__jumbotron`} location={location} />
     <h1 className={`contact__jumbotron__title`}>{welcomeText}</h1>
     <Markdown className={`contact__jumbotron__description`}>{welcomeDescription}</Markdown>
     <Img className={`contact__jumbotron__image`} fixed={image?.childImageSharp?.fixed} />
   </section>
-}; 
+};
 
 ContactJumbotron.propTypes = {
   jumbotron: PropTypes.arrayOf(String).isRequired,
