16cd6391866efcf64f54dbaa1d8a65e91e2febf7
Ferran Buireu <fbuireu@gmail.com>
2022-04-02 11:19:25 +0200

    fix: correct target searching

--- src/components/organisms/Search/Search.js
+++ src/components/organisms/Search/Search.js
@@ -11,7 +11,7 @@ const CustomSearch = ({ currentRefinement, refine }) => {
     refine(``);
   };
 
-  const searchQuery = ({ event: { currentTarget: { value } } }) => refine(value);
+  const searchQuery = ({ currentTarget: { value } }) => refine(value);
 
   return <div className={`filter__search`}>
     <form noValidate action={``} role={`search`}>
