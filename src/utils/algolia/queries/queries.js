3498d6d305fde0f95f6e7f8b409770326b72968b
Ferran Buireu <fbuireu@gmail.com>
2022-04-02 11:12:22 +0200

    refactor: add built-in array flattening

--- src/utils/algolia/queries/queries.js
+++ src/utils/algolia/queries/queries.js
@@ -5,30 +5,31 @@ const { GATSBY_ALGOLIA_ARTICLES_INDEX_NAME, GATSBY_ALGOLIA_PROJECTS_INDEX_NAME }
 
 const SETTINGS = { attributesToSnippet: [`excerpt: 200`] };
 
-const flatten = data => data.map(({ node: { frontmatter, ...rest } }) => ({ ...frontmatter, ...rest }));
-
 const ALGOLIA_QUERIES = [
   {
     query: ARTICLES_QUERY,
-    transformer: ({ data }) => flatten(data.articles.edges),
+    transformer: ({ data }) => data.articles.edges.flatMap(({
+      node: {
+        frontmatter,
+        ...rest
+      }
+    }) => ({ ...frontmatter, ...rest })),
     indexName: GATSBY_ALGOLIA_ARTICLES_INDEX_NAME,
     SETTINGS,
     matchFields: [`fields.slug`, `content.title`, `content.lastUpdated`]
   },
   {
     query: PROJECTS_QUERY,
-    transformer: ({ data }) => flatten(data.projects.edges),
+    transformer: ({ data }) => data.projects.edges.flatMap(({
+      node: {
+        frontmatter,
+        ...rest
+      }
+    }) => ({ ...frontmatter, ...rest })),
     indexName: GATSBY_ALGOLIA_PROJECTS_INDEX_NAME,
     SETTINGS,
     matchFields: [`content.name`, `content.publishDate`]
-  },
-  // {
-  //   query: PROJECTS_AND_ARTICLES_QUERY,
-  //   transformer: ({ data }) => flatten(data.projctsAndArticles.edges),
-  //   indexName: process.env.GATSBY_ALGOLIA_PROJECTS_INDEX_NAME,
-  //   SETTINGS,
-  //   matchFields: [`fields.slug`, `content.title`,`content.lastUpdated`, `name`]
-  // },
+  }
 ];
 
 module.exports = ALGOLIA_QUERIES;
