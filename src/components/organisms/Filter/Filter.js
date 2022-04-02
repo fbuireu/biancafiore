2c63fe795842d57359048e14959d65e67208d7be
Ferran Buireu <fbuireu@gmail.com>
2022-04-02 11:17:36 +0200

    refactor: correct props drilling

--- src/components/organisms/Filter/Filter.js
+++ src/components/organisms/Filter/Filter.js
@@ -5,20 +5,30 @@ import { SortHitsBy } from '../../molecules/SortHitsBy/SortHitsBy';
 import { Search } from '../Search/Search';
 import './Filter.scss';
 
-const Filter = ({ filterParameters, defaultRefinement, hasRange }) => {
+const Filter = ({ filterParameters, defaultRefinement, hasRange, queryStringCriteria }) => {
   return <aside className={`filters__wrapper`}>
     <Search />
-    <SortHitsBy defaultRefinement={defaultRefinement}
-                items={filterParameters.SORT_BY} />
-    {filterParameters.SEARCH_PARAMETERS.map(searchParameter => <RefinementsList key={searchParameter.label} {...searchParameter} />)}
-    {hasRange && <RangeSlider attribute="content.readingTime"/>}
+    <SortHitsBy defaultRefinement={defaultRefinement} items={filterParameters.SORT_BY} />
+    {filterParameters.SEARCH_PARAMETERS.map(searchParameter => {
+      let matchCriteria;
+      if (queryStringCriteria) {
+        matchCriteria = queryStringCriteria[searchParameter.queryString];
+      }
+
+      return <RefinementsList key={searchParameter.label}
+                              queryStringCriteria={matchCriteria ? queryStringCriteria : null}
+                              {...searchParameter}
+      />;
+    })}
+    {hasRange && <RangeSlider attribute="content.readingTime" />}
   </aside>;
 };
 
 Filter.propTypes = {
   filterParameters: PropTypes.array.isRequired,
   defaultRefinement: PropTypes.string.isRequired,
-  hasRange: PropTypes.bool.isRequired
+  hasRange: PropTypes.bool.isRequired,
+  queryStringCriteria: PropTypes.object
 };
 
 Filter.defaultProps = {};
