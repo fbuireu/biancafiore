d865142a437902455f3b98e2a83e360311929083
Ferran Buireu <fbuireu@gmail.com>
2022-04-02 11:09:49 +0200

    feat: add latest projects staticQuery fetch

--- src/components/molecules/MyWork/MyWork.js
+++ src/components/molecules/MyWork/MyWork.js
@@ -1,19 +1,31 @@
+import { Link } from 'gatsby';
 import Img from 'gatsby-image';
-import { Link } from 'gatsby-plugin-intl';
+import { useIntl } from 'gatsby-plugin-intl';
 import PropTypes from 'prop-types';
 import 'swiper/swiper.scss';
+import { useLatestProjects } from '../../../utils/hooks/useLatestProjects';
 import slugify from '../../../utils/slugify/slugify';
 import './MyWork.scss';
 
-const MyWork = ({ title, works }) => {
+const MyWork = ({ title }) => {
+  const latestProjects = useLatestProjects();
+  const { locale } = useIntl();
 
   return <section className={`my-work__wrapper wrapper`}>
     <h2 className={`my-work__title`}>{title}</h2>
     <ul className={`my-work__list`}>
-      {works.sort(() => Math.random() - .5).map(({ tags, image }) => {
-        return <Link className={`my-work__item`} key={tags[0]} to={`/tag/${slugify(tags[0])}`}>
-          <Img className={`my-work__item__image`} fluid={image.childImageSharp.fluid} alt={tags[0]}/>
-          <h3 className={`my-work__item__name`}>{tags[0]}</h3>
+      {latestProjects.map(({ node: latestProject }) => {
+        return <Link
+          className={`my-work__item`}
+          key={latestProject.frontmatter.content.name}
+          to={`/${locale}/projects/${slugify(latestProject.frontmatter.content.name)}`}
+        >
+          <Img
+            className={`my-work__item__image`}
+            fluid={latestProject.frontmatter.content.featuredImage.childImageSharp.fluid}
+            alt={latestProject.frontmatter.content.name}
+          />
+          <h3 className={`my-work__item__name`}>{latestProject.frontmatter.content.name}</h3>
         </Link>;
       })}
     </ul>
