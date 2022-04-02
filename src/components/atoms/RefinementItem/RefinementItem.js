a88b692c78cf8a054c8438a29d9b1bdc17d948cd
Ferran Buireu <fbuireu@gmail.com>
2022-04-02 11:04:22 +0200

    feat: add query string search/filter feature

--- src/components/atoms/RefinementItem/RefinementItem.js
+++ src/components/atoms/RefinementItem/RefinementItem.js
@@ -1,15 +1,21 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { connectRefinementList } from 'react-instantsearch-dom';
 import './RefinementItem.scss';
 
-const CustomRefinementItem = ({ items, refine, selectRefinement }) => {
+const CustomRefinementItem = ({ items, refine, selectRefinement, queryStringCriteria, attribute, queryString }) => {
   const [isActiveRefinement, setIsActiveRefinements] = useState([]);
   let isRefinementSelected = false;
 
+  useEffect(() => {
+    if (queryStringCriteria) {
+      refine(queryStringCriteria[queryString]);
+      selectRefinement(true);
+    }
+  }, [refine, queryStringCriteria, selectRefinement]);
+
   const handleClick = ({ event, value: { value }, index }) => {
     event.preventDefault();
-
     let isSelectingMoreRefinements = !isActiveRefinement.includes(index);
 
     if (isSelectingMoreRefinements) {
@@ -26,16 +32,18 @@ const CustomRefinementItem = ({ items, refine, selectRefinement }) => {
 
   return <div className={`refinement-list__wrapper`}>
     <ul className={`refinement-list__list`}>
-      {items.map((item, index) => (
-        <li key={item.label}
-            className={`refinement-list__item ${isActiveRefinement.includes(index) ? `--is-selected` : ``}`}
-            onClick={event => handleClick({ event: event, value: item, index: index })}>
+      {items.map((item, index) => {
+        return <li
+          key={item.label}
+          className={`refinement-list__item ${isActiveRefinement.includes(index) ? `--is-selected` : ``} ${item.label === (queryStringCriteria && queryStringCriteria[queryString]) ? `--is-locked` : ``}`}
+          onClick={event => handleClick({ event: event, value: item, index: index })}
+        >
           <label className={`refinement-list__item__label`}>
             <input className={`refinement-list__item__checkbox`} type={`checkbox`} />
             <span className={`refinement-list__item__label-text`}>{item.label} <sub>({item.count})</sub></span>
           </label>
-        </li>
-      ))}
+        </li>;
+      })}
     </ul>
   </div>;
 };
@@ -44,6 +52,9 @@ CustomRefinementItem.propTypes = {
   items: PropTypes.string.isRequired,
   refine: PropTypes.string.isRequired,
   selectRefinement: PropTypes.string.isRequired,
+  attribute: PropTypes.string,
+  queryStringCriteria: PropTypes.string,
+  queryString: PropTypes.string
 };
 
 CustomRefinementItem.defaultProps = {};
