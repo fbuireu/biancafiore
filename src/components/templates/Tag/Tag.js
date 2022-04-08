e45030827f619a736fb316d56864d83b0e712c01
Ferran Buireu <fbuireu@gmail.com>
2022-04-08 16:17:48 +0200

    feat: add switch of wrapper depending on type

--- src/components/templates/Tag/Tag.js
+++ src/components/templates/Tag/Tag.js
@@ -1,23 +1,27 @@
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
+import { PROJECTS_SEARCH_PARAMETERS } from '../../../utils/algolia/config/projectsSearchParameters';
+import { PROJECTS_SORT_BY } from '../../../utils/algolia/config/projectsSortBy';
 import { TAGS_SEARCH_PARAMETERS } from '../../../utils/algolia/config/tagsSearchParameters';
 import { TAGS_SORT_BY } from '../../../utils/algolia/config/tagsSortBy';
 import Breadcrumbs from '../../atoms/Breadcrumbs/Breadcrumbs';
 import SEO from '../../atoms/SEO/SEO';
 import AlgoliaWrapper from '../../organisms/AlgoliaWrapper/AlgoliaWrapper';
 import ArticleHitCards from '../../organisms/ArticleHitCards/ArticleHitCards';
+import ProjectHitCards from '../../organisms/ProjectHitCards/ProjectHitCards';
 import Layout from '../Layout/Layout';
 import './Tag.scss';
 
 const CRITERIA = `criteria`;
 
-const FILTER_PARAMETERS = {
-  SEARCH_PARAMETERS: TAGS_SEARCH_PARAMETERS,
-  SORT_BY: TAGS_SORT_BY
-};
-
 const Tag = ({ location, pageContext: { tag } }) => {
   const [queryStringCriteria, setQueryStringCriteria] = useState(null);
+  const isProject = tag.key.toLowerCase().includes(`project`);
+
+  const FILTER_PARAMETERS = {
+    SEARCH_PARAMETERS: isProject ? PROJECTS_SEARCH_PARAMETERS : TAGS_SEARCH_PARAMETERS,
+    SORT_BY: isProject ? PROJECTS_SORT_BY : TAGS_SORT_BY
+  };
 
   useEffect(() => {
     if (location?.search) {
@@ -37,16 +41,17 @@ const Tag = ({ location, pageContext: { tag } }) => {
     }
   }, []);
 
+  console.log(`queryStringCriteria`, queryStringCriteria);
   return <Layout>
     <SEO title="Tag" />
     <section className={`wrapper`}>
       <h1 className={`tag__title`}>Tag: <span className={`tag__name`}>{tag.name}</span></h1>
       <Breadcrumbs location={location} classNames={`blog__jumbotron`} />
       <AlgoliaWrapper
-        hitsComponent={ArticleHitCards}
-        indexName={process.env.GATSBY_ALGOLIA_ARTICLES_INDEX_NAME}
+        hitsComponent={isProject ? ProjectHitCards : ArticleHitCards}
+        indexName={isProject ? process.env.GATSBY_ALGOLIA_PROJECTS_INDEX_NAME : process.env.GATSBY_ALGOLIA_ARTICLES_INDEX_NAME}
         filterParameters={FILTER_PARAMETERS}
-        hasRange={true}
+        hasRange={isProject}
         queryStringCriteria={queryStringCriteria}
       />
     </section>
