325e8576caa2beda66ca00ce42bf4c656a841b24
Ferran Buireu <fbuireu@gmail.com>
2022-04-08 16:18:28 +0200

    feat: add initial project layout

---
+++ src/components/templates/Project/Project.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import Breadcrumbs from '../../atoms/Breadcrumbs/Breadcrumbs';
+import SEO from '../../atoms/SEO/SEO';
+import Layout from '../Layout/Layout';
+
+const Project = ({ location }) => {
+
+  return <Layout>
+    <SEO title="Tag" />
+    <section className={`wrapper`}>
+      <h1 className={`tag__title`}><span className={`tag__name`}>HEHE</span></h1>
+      <Breadcrumbs location={location} classNames={`blog__jumbotron`} />
+
+    </section>
+  </Layout>;
+};
+
+Project.propTypes = {
+  location: PropTypes.object.isRequired,
+  pageContext: PropTypes.object.isRequired
+};
+
+Project.defaultProps = {};
+
+export default Project;
