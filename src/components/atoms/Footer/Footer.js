39e3b071a960692b60baaca0eb80999303132ca5
Ferran Buireu <fbuireu@gmail.com>
2022-04-28 16:07:44 +0200

    style: fix indent

--- src/components/atoms/Footer/Footer.js
+++ src/components/atoms/Footer/Footer.js
@@ -5,21 +5,24 @@ import './Footer.scss';
 const Footer = () => {
   const footerItems = useSocialNetworkItems();
 
-  return <section className={`footer-wrapper`}>
-    <ul className={`footer__list wrapper`}>
-      {footerItems.map(({ node: footerItem }) => (
-        <li key={footerItem.frontmatter.name} className={`footer__item`}>
-          {footerItem.frontmatter.url.match(VALID_EMAIL)
-            ? <a href={`mailto:${footerItem.frontmatter.url}`} className={`footer__link`}>{footerItem.frontmatter.name}</a>
-            : <a href={footerItem.frontmatter.url}
-                 target={`_blank`}
-                 className={`footer__link`}
-                 rel={`noopener noreferrer`}>{footerItem.frontmatter.name}</a>
-          }
-        </li>
-      ))}
-    </ul>
-  </section>;
+  return <footer>
+    <section className={`footer-wrapper`}>
+      <ul className={`footer__list wrapper`}>
+        {footerItems.map(({ node: footerItem }) => (
+          <li key={footerItem.frontmatter.name} className={`footer__item`}>
+            {footerItem.frontmatter.url.match(VALID_EMAIL)
+              ? <a href={`mailto:${footerItem.frontmatter.url}`}
+                   className={`footer__link`}>{footerItem.frontmatter.name}</a>
+              : <a href={footerItem.frontmatter.url}
+                   target={`_blank`}
+                   className={`footer__link`}
+                   rel={`noopener noreferrer`}>{footerItem.frontmatter.name}</a>
+            }
+          </li>
+        ))}
+      </ul>
+    </section>
+  </footer>;
 };
 
 Footer.propTypes = {};
