e7f0e1ff5146d9a5f17cd4f7eb6e2fc92ea265d1
Ferran Buireu <fbuireu@gmail.com>
2022-05-03 18:52:20 +0200

    feat(hooks): add custom hooks to reuse states in one/two-ways

---
+++ src/utils/hooks/useBoolean.js
@@ -0,0 +1,20 @@
+import { useMemo } from 'react';
+
+import useToggle from './useToggle';
+
+function useBoolean(initialValue = false) {
+  const [value, toggle] = useToggle(initialValue);
+
+  const handlers = useMemo(
+    () => ({
+      toggle,
+      on: () => toggle(true),
+      off: () => toggle(false)
+    }),
+    [toggle]
+  );
+
+  return [value, handlers];
+}
+
+export default useBoolean;
