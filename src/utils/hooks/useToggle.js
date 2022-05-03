4050f421d3604f1369bb6521b93496f2aa363aef
Ferran Buireu <fbuireu@gmail.com>
2022-05-03 18:57:54 +0200

    style: rename private function

--- src/utils/hooks/useToggle.js
+++ src/utils/hooks/useToggle.js
@@ -1,11 +1,11 @@
 import { useReducer } from 'react';
 
-function _toggler(currentValue, newValue) {
+function _toggle(currentValue, newValue) {
   return typeof newValue === `boolean` ? newValue : !currentValue;
 }
 
 function useToggle(initialValue = false) {
-  return useReducer(_toggler, initialValue);
+  return useReducer(_toggle, initialValue);
 }
 
 export default useToggle;
