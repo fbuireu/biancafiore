3a47d715c18107ff3144dd6f85516a2a8daf493d
Ferran Buireu <fbuireu@gmail.com>
2022-05-25 22:20:38 +0200

    feat: apply custom hook

--- src/components/molecules/LanguageSwitcher/LanguageSwitcher.js
+++ src/components/molecules/LanguageSwitcher/LanguageSwitcher.js
@@ -1,31 +1,28 @@
 import { changeLocale, FormattedMessage, useIntl } from 'gatsby-plugin-intl';
 import moment from 'moment/moment';
-import { useState } from 'react';
 import ArrowDown from '../../../assets/svg-components/arrow-down.svg';
+import useBoolean from '../../../utils/hooks/useBoolean';
 import { useLanguages } from '../../../utils/hooks/useLanguages';
 import './LanguageSwitcher.scss';
 
 const LanguageSwitcher = () => {
-  const [isMenuSelectorOpen, setIsMenuSelectorOpen] = useState(false);
+  const [isLanguageSelectorActive, { toggle }] = useBoolean();
+
   const languages = useLanguages();
   const { locale: currentLanguage } = useIntl();
-
-  const handleMenuSelector = () => {
-    setIsMenuSelectorOpen(!isMenuSelectorOpen);
-  };
 
   const handleLanguage = language => {
     changeLocale(language);
     moment.locale(language);
   };
 
-  return <li className={`language-switcher__wrapper ${isMenuSelectorOpen ? `--is-open` : ``}`}>
-    <span onClick={handleMenuSelector} className={`language-switcher__icon`}>
+  return <li className={`language-switcher__wrapper ${isLanguageSelectorActive ? `--is-open` : ``}`}>
+    <span onClick={toggle} className={`language-switcher__icon`}>
       <FormattedMessage id={`global.changeLanguage`} />
       <ArrowDown className={`arrow-down`} />
     </span>
     <ul className={`language-switcher__list`}>
-      {languages.map(language => isMenuSelectorOpen &&
+      {languages.map(language => isLanguageSelectorActive &&
         <li className={`language-switcher__item ${currentLanguage === language.isoCode ? `--is-current-language` : ``}`}
             onClick={() => handleLanguage(language.isoCode)}>
           <FormattedMessage id={`global.${language.isoCode}`} />
