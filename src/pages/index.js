7448afbbcf9175f389a97457f3d5c08afcabc6ac
Ferran Buireu <fbuireu@gmail.com>
2022-05-25 22:21:06 +0200

    feat: add components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,23 @@
 import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
+import SEO from '../components/atoms/SEO/SEO';
+import HomeJumbotron from '../components/molecules/HomeJumbotron/HomeJumbotron';
+import HomeLatestArticles from '../components/molecules/HomeLatestArticles/HomeLatestArticles';
+import MyWork from '../components/molecules/MyWork/MyWork';
+import Testimonials from '../components/molecules/Testimonials/Testimonials';
+import Layout from '../components/templates/Layout/Layout';
 
 const Index = ({
+  location,
   data: {
     home: {
       edges: [{
         node: {
           frontmatter: {
             jumbotron,
-            testimonials: { title: testimonialsTitle, subtitle, testimonials },
-            myWork: { title: myWorkTitle, works },
-            latestArticles: { title: latestArticlesTitle }
+            testimonials,
+            myWork,
+            latestArticles
           }
         }
       }]
@@ -18,19 +25,19 @@ const Index = ({
   }
 }) => {
 
-  return <h1 style={{
-    textAlign: `center`,
-    marginTop: `50vh`,
-    fontSize: `52px`
-  }}>Site under construction<br />We&apos;ll be back soon</h1>;
+  // return <h1 style={{
+  //   textAlign: `center`,
+  //   marginTop: `50vh`,
+  //   fontSize: `52px`
+  // }}>Site under construction<br />We&apos;ll be back soon</h1>;
 
-  // return <Layout>
-  //   <SEO title="Home" />
-  //   <HomeJumbotron jumbotron={jumbotron} />
-  //   <Testimonials title={testimonialsTitle} subtitle={subtitle} testimonials={testimonials} />
-  //   <MyWork title={myWorkTitle} works={works} />
-  //   <HomeLatestArticles title={latestArticlesTitle} />
-  // </Layout>;
+  return <Layout>
+    <SEO title="Home" />
+    <HomeJumbotron {...jumbotron} />
+    <Testimonials {...testimonials} />
+    <MyWork {...myWork} />
+    <HomeLatestArticles {...latestArticles} />
+  </Layout>;
 };
 
 export const homeData = graphql`
@@ -71,16 +78,6 @@ export const homeData = graphql`
                         }
                         myWork {
                             title
-                            works {
-                                tags
-                                image {
-                                    childImageSharp {
-                                        fluid {
-                                            ...GatsbyImageSharpFluid
-                                        }
-                                    }
-                                }
-                            }
                         }
                         latestArticles {
                             title
@@ -93,7 +90,8 @@ export const homeData = graphql`
 `;
 
 Index.propTypes = {
-  data: PropTypes.objectOf(PropTypes.object).isRequired
+  data: PropTypes.objectOf(PropTypes.object).isRequired,
+  location: PropTypes.objectOf(PropTypes.object).isRequired
 };
 
 Index.defaultProps = {};
