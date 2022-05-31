409b87c2a50446e94ef799807387a88521e1ed2f
Ferran Buireu <fbuireu@gmail.com>
2022-05-31 15:32:40 +0200

    feat: add placeholder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,5 @@
 import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
-import SEO from '../components/atoms/SEO/SEO';
-import HomeJumbotron from '../components/molecules/HomeJumbotron/HomeJumbotron';
-import HomeLatestArticles from '../components/molecules/HomeLatestArticles/HomeLatestArticles';
-import MyWork from '../components/molecules/MyWork/MyWork';
-import Testimonials from '../components/molecules/Testimonials/Testimonials';
-import Layout from '../components/templates/Layout/Layout';
 
 const Index = ({
   location,
@@ -25,19 +19,19 @@ const Index = ({
   }
 }) => {
 
-  // return <h1 style={{
-  //   textAlign: `center`,
-  //   marginTop: `50vh`,
-  //   fontSize: `52px`
-  // }}>Site under construction<br />We&apos;ll be back soon</h1>;
+  return <h1 style={{
+    textAlign: `center`,
+    marginTop: `50vh`,
+    fontSize: `52px`
+  }}>Site under construction<br />We&apos;ll be back soon</h1>;
 
-  return <Layout>
-    <SEO title="Home" />
-    <HomeJumbotron {...jumbotron} />
-    <Testimonials {...testimonials} />
-    <MyWork {...myWork} />
-    <HomeLatestArticles {...latestArticles} />
-  </Layout>;
+  // return <Layout>
+  //   <SEO title="Home" />
+  //   <HomeJumbotron {...jumbotron} />
+  //   <Testimonials {...testimonials} />
+  //   <MyWork {...myWork} />
+  //   <HomeLatestArticles {...latestArticles} />
+  // </Layout>;
 };
 
 export const homeData = graphql`
