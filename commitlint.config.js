131ee850cfd0a8ef136fb8261d9fc4dfcefc81e3
Ferran Buireu <fbuireu@gmail.com>
2023-12-20 14:31:42 +0100

    fix: correct scope-enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'scope-case': [2, 'always', ['lower-case', 'pascal-case', 'camel-case']],
-        'scope-enum': [2, 'always', ['only-ui', 'deps', 'other']],
+        'scope-enum': [2, 'always', ['deps', 'other']],
         'header-max-length': [2, 'always', 130],
     },
 };
