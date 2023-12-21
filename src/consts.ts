a13ef43e83faa57b499ddacdcdc21f9db93db843
Ferran Buireu <fbuireu@gmail.com>
2023-12-21 15:04:31 +0100

    feat: add footer

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,8 @@
 export const SITE_TITLE = 'Bianca Fiore';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 
+export const ENCODED_BIANCA_EMAIL = btoa('biancamaria.fiore@gmail.com');
+
 export const DEFAULT_DATE_OPTIONS: Intl.DateTimeFormatOptions = {
     weekday: 'long',
     year: 'numeric',
