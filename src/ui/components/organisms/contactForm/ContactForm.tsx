c5ef739a3ab3943544837b9ebe58876b09961403
Ferran Buireu <fbuireu@gmail.com>
2024-02-29 09:51:56 +0100

    feat: add submit animation

--- src/ui/components/organisms/contactForm/ContactForm.tsx
+++ src/ui/components/organisms/contactForm/ContactForm.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
@@ -7,6 +7,7 @@ import './contact-form.css';
 import { autosize } from '@components/organisms/contactForm/utils/autosize';
 import { CONTACT_FORM_REQUEST_PARAMETERS } from 'src/consts.ts';
 import { encode } from '@components/organisms/contactForm/utils/encode';
+import { flyPlane } from '@components/organisms/contactForm/utils/flyPlane';
 
 const schema = z.object({
     name: z.string().trim().min(1, 'Please insert your name'),
@@ -40,6 +41,7 @@ export const ContactForm = () => {
     });
     const [formStatus, setFormStatus] = useState<FormStatus>(FormStatus.INITIAL);
     const { executeRecaptcha } = useGoogleReCaptcha();
+    const submitRef = useRef<HTMLButtonElement>(null);
 
     const verifyRecaptcha = useCallback(
         async (data: FormData, event: React.FormEvent<HTMLFormElement>) => {
@@ -60,21 +62,28 @@ export const ContactForm = () => {
 
     const submitForm = useCallback(async (data: FormData, event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        try {
+            setFormStatus(FormStatus.LOADING);
+            const requestParams: RequestInit = {
+                ...CONTACT_FORM_REQUEST_PARAMETERS,
+                body: encode({ ...data }),
+            };
 
-        const requestParams: RequestInit = {
-            ...CONTACT_FORM_REQUEST_PARAMETERS,
-            body: encode({ ...data }),
-        };
-        setFormStatus(FormStatus.LOADING);
-        fetch(`/`, requestParams)
-            .then(() => {
-                reset();
-                setFormStatus(FormStatus.SUCCESS);
-            })
-            .catch((error) => {
-                console.log(error);
-                setFormStatus(FormStatus.ERROR);
-            });
+            const response = await fetch(`/`, requestParams);
+
+            if (response.ok) {
+                flyPlane(submitRef.current!);
+                setTimeout(() => {
+                    setFormStatus(FormStatus.SUCCESS);
+                    reset();
+                }, 2000);
+            } else {
+                throw new Error('Failed to submit form');
+            }
+        } catch (error) {
+            console.error(error);
+            setFormStatus(FormStatus.ERROR);
+        }
     }, []);
 
     return (
@@ -142,10 +151,15 @@ export const ContactForm = () => {
                             <p className="contact-form__recaptcha__error-message">{errors.recaptcha.message}</p>
                         )}
                     </div>
-                    <button type="submit">Enviar</button>
+                    <button ref={submitRef} className="contact-form__submit plane clickable" type="submit">
+                        <span>Send email</span>
+                        <div className="plane__left-wing" />
+                        <div className="plane__right-wing" />
+                        <span></span>
+                    </button>
                 </form>
             ) : (
-                <h4>Form sent correctly! Will be in touch soon</h4>
+                <h4 className="contact-form__success-message">Form sent correctly! Will be in touch soon</h4>
             )}
         </>
     );
