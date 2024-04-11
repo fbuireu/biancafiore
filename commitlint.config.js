c6c69c7b3be101f2c23c5d87c6a4a068a84ec481
Ferran Buireu <fbuireu@users.noreply.github.com>
2024-04-11 09:14:57 +0200

    foo: this will fail

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    "scope-case": [2, "always", ["lower-case", "pascal-case", "camel-case"]],
+    "scope-enum": [2, "always", ["deps", "other"]],
+    "header-max-length": [2, "always", 130],
+  },
+};
