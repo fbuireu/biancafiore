8a0d0b58afc7c03d51de65fd386b1f82efb0cd12
Ferran <fbuireu@gmail.com>
2024-05-13 08:51:59 +0200

    feat: add /articles responsive

--- src/ui/hooks/useTabVisibility/index.ts
+++ src/ui/hooks/useTabVisibility/index.ts
@@ -1,2 +1,1 @@
-
-export * from './useTabVisibility.ts'
+export * from "./useTabVisibility.ts";
--- src/ui/hooks/useTabVisibility/useTabVisibility.ts
+++ src/ui/hooks/useTabVisibility/useTabVisibility.ts
@@ -1,25 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export enum TabVisibility {
-  VISIBLE = 'visible',
-  HIDDEN = 'hidden',
-  UNDEFINED = 'undefined'
+	VISIBLE = "visible",
+	HIDDEN = "hidden",
+	UNDEFINED = "undefined",
 }
 
 const useTabVisibility = (): TabVisibility => {
-  const [tabVisibility, setTabVisibility] = useState<TabVisibility>(document.visibilityState as TabVisibility);
+	const [tabVisibility, setTabVisibility] = useState<TabVisibility>(document.visibilityState as TabVisibility);
 
-  useEffect(() => {
-    const handleVisibilityChange = () => {
-      setTabVisibility(document.visibilityState as TabVisibility);
-    };
+	useEffect(() => {
+		const handleVisibilityChange = () => {
+			setTabVisibility(document.visibilityState as TabVisibility);
+		};
 
-    document.addEventListener('visibilitychange', handleVisibilityChange);
+		document.addEventListener("visibilitychange", handleVisibilityChange);
 
-    return () => document.removeEventListener('visibilitychange', handleVisibilityChange);
-  }, []);
+		return () => document.removeEventListener("visibilitychange", handleVisibilityChange);
+	}, []);
 
-  return tabVisibility;
+	return tabVisibility;
 };
 
 export default useTabVisibility;
