2640d01d5d277444eeac777185889e3a2a4e385b
Ferran Buireu <fbuireu@gmail.com>
2024-06-24 16:43:34 +0200

    chore: fix build method

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,12 +1,11 @@
-import { getCollection } from "astro:content";
-import rss from "@astrojs/rss";
-import { DEFAULT_SEO_PARAMS } from "@const/index.js";
+import { getCollection } from 'astro:content';
+import rss from '@astrojs/rss';
 
 export async function GET(context) {
 	const posts = await getCollection("articles");
 	return rss({
-		title: DEFAULT_SEO_PARAMS.TITLE,
-		description: DEFAULT_SEO_PARAMS.DESCRIPTION,
+		title: "DEFAULT_SEO_PARAMS.TITLE",
+		description: "DEFAULT_SEO_PARAMS.DESCRIPTION",
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
