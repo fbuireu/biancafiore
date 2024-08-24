04c2dd09be1a767ace77a7555ca0077f8158ec13
Ferran Buireu <fbuireu@gmail.com>
2024-08-24 09:38:41 +0200

    feat: migrate to commitlint TS

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,11 +1,12 @@
+/** @type {import('stylelint').Config} */
 module.exports = {
-	extends: "stylelint-config-recommended",
-	plugins: ["stylelint-order"],
-	allowEmptyInput: true,
-	rules: {
-		"order/properties-alphabetical-order": true,
-		"selector-class-pattern": null,
-		"value-keyword-case": null,
-		"custom-property-pattern": null,
-	},
+  extends: "stylelint-config-recommended",
+  plugins: ["stylelint-order"],
+  allowEmptyInput: true,
+  rules: {
+    "order/properties-alphabetical-order": true,
+    "selector-class-pattern": null,
+    "value-keyword-case": null,
+    "custom-property-pattern": null,
+  },
 };
