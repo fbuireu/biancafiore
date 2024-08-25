1f6ece0030032f40aa7bc25d4856e70e8f5ac26a
Ferran Buireu <fbuireu@gmail.com>
2024-08-25 09:50:52 +0200

    feat: add TS stylelint support

---
+++ .stylelintrc.ts
@@ -0,0 +1,12 @@
+import type { Config } from "stylelint";
+
+const config: Config = {
+    extends: "stylelint-config-recommended",
+    plugins: ["stylelint-order"],
+    allowEmptyInput: true,
+    rules: {
+        "order/properties-alphabetical-order": true,
+    },
+};
+
+export default config;
