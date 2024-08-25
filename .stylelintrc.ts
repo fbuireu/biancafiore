c2ef4c39af0fcd13316d0af8587194b4f1c1fd09
Ferran Buireu <fbuireu@gmail.com>
2024-08-25 10:43:10 +0200

    perf: improved lint-staged

--- .stylelintrc.ts
+++ .stylelintrc.ts
@@ -4,7 +4,6 @@ const config: Config = {
     extends: "stylelint-config-recommended",
     plugins: ["stylelint-order"],
     allowEmptyInput: true,
-    maxWarnings: 0,
     rules: {
         "order/properties-alphabetical-order": true,
     },
