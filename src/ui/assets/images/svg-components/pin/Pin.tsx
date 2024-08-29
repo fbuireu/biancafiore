0d4834c1c74318b0db5cc8af892e5c4210a5b3db
Ferran <fbuireu@gmail.com>
2024-08-29 09:12:40 +0200

    feat: add map animations

--- src/ui/assets/images/svg-components/pin/Pin.tsx
+++ src/ui/assets/images/svg-components/pin/Pin.tsx
@@ -4,12 +4,13 @@ import type { SVGProps } from "react";
 interface PinProps extends SVGProps<SVGSVGElement> {
 	classNames?: string;
 	fill?: string;
+	title?: string;
 }
 
-export const Pin = ({ fill, classNames, ...props }: PinProps) => {
+export const Pin = ({ fill, title = "Pin", classNames, ...props }: PinProps) => {
 	return (
 		<svg viewBox="-4 0 36 36" className={clsx(classNames)} {...props}>
-			<title>Pin</title>
+			<title>{title}</title>
 			<path
 				fill={fill}
 				d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"
--- src/ui/modules/about/components/cityCard/CityCard.tsx
+++ src/ui/modules/about/components/cityCard/CityCard.tsx
@@ -3,7 +3,7 @@ import type { CityDescriptionProps } from "@modules/about/components/cityCard/at
 import { CityImage } from "@modules/about/components/cityCard/atoms/cityImage";
 import type { CityImageProps } from "@modules/about/components/cityCard/atoms/cityImage";
 import { CityName } from "@modules/about/components/cityCard/atoms/cityName";
-import type { CityTitleProps } from "@modules/about/components/cityCard/atoms/cityName";
+import type { CityNameProps } from "@modules/about/components/cityCard/atoms/cityName";
 import { CityPeriod } from "@modules/about/components/cityCard/atoms/cityPeriod";
 import type { CityPeriodProps } from "@modules/about/components/cityCard/atoms/cityPeriod";
 import type { CSSProperties, ReactNode } from "react";
@@ -13,15 +13,16 @@ interface CityCardProps {
 	children: ReactNode;
 	index: number;
 	numCards: number;
+	name: string;
 }
 
-export const CityCard = ({ children, index, numCards }: CityCardProps) => {
+export const CityCard = ({ children, index, numCards, name }: CityCardProps) => {
 	const style: CSSProperties & { [key: string]: string } = {
 		"--inline-index": String(index),
 		"--inline-num-cards": String(numCards),
 	};
 	return (
-		<li className={"city-card__item"} style={style}>
+		<li className={"city-card__item"} style={style} id={name}>
 			<article className={"city-card__content"}>{children}</article>
 		</li>
 	);
@@ -29,10 +30,10 @@ export const CityCard = ({ children, index, numCards }: CityCardProps) => {
 
 const Image = (props: CityImageProps) => <CityImage {...props} />;
 const Period = ({ children }: CityPeriodProps) => <CityPeriod>{children}</CityPeriod>;
-const Title = ({ children, city }: CityTitleProps) => <CityName city={city}>{children}</CityName>;
+const Name = ({ children }: CityNameProps) => <CityName>{children}</CityName>;
 const Description = ({ children }: CityDescriptionProps) => <CityDescription>{children}</CityDescription>;
 
 CityCard.Period = Period;
-CityCard.Title = Title;
+CityCard.Name = Name;
 CityCard.Description = Description;
 CityCard.Image = Image;
