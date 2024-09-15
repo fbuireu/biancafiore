a18165af1e859b5470a7eb631264c937fbb8eb51
Ferran Buireu <fbuireu@gmail.com>
2024-09-15 08:05:57 +0200

    fix: a11y

--- src/infrastructure/cms/client.ts
+++ src/infrastructure/cms/client.ts
@@ -2,9 +2,9 @@ import { getSecret } from "astro:env/server";
 import contentful from "contentful";
 
 export const client = contentful.createClient({
-	space: getSecret("CONTENTFUL_SPACE_ID") as string,
-	accessToken: import.meta.env.DEV
-		? <string>(getSecret("CONTENTFUL_PREVIEW_TOKEN"))
-		: <string>(getSecret("CONTENTFUL_DELIVERY_TOKEN")),
-	host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+    space: getSecret("CONTENTFUL_SPACE_ID") as string,
+    accessToken: import.meta.env.DEV
+        ? (getSecret("CONTENTFUL_PREVIEW_TOKEN") as string)
+        : (getSecret("CONTENTFUL_DELIVERY_TOKEN") as string),
+    host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
 });
