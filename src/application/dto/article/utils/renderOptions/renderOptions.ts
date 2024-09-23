efad40e12f846156cfdb75e2f81d3642390205bd
Ferran <fbuireu@gmail.com>
2024-09-23 15:23:45 +0200

    feat: add link follow

--- src/application/dto/article/utils/renderOptions/renderOptions.ts
+++ src/application/dto/article/utils/renderOptions/renderOptions.ts
@@ -1,8 +1,9 @@
 import type { RawArticle } from "@application/dto/article/types";
-import type { Block, Inline } from "@contentful/rich-text-types";
+import { parseHeadings } from "@application/dto/article/utils/parseHeadings";
+import type { Block, Inline, Text } from "@contentful/rich-text-types";
 import { BLOCKS, INLINES } from "@contentful/rich-text-types";
 
-type Node = Block | Inline;
+type Node = Block | Inline | Text;
 
 export function renderOptions(rawArticle: RawArticle) {
 	return {
@@ -12,11 +13,7 @@ export function renderOptions(rawArticle: RawArticle) {
 				const { slug, title } = node.data.target.fields;
 
 				if (contentTypeId === "article" && slug && title) {
-					return `
-                    <a href="/articles/${slug}">
-                        ${title}
-                    </a>
-                `;
+					return `<a href="/articles/${slug}">${title}</a>`;
 				}
 				return "";
 			},
@@ -25,23 +22,11 @@ export function renderOptions(rawArticle: RawArticle) {
 				const { code, embedUrl, title } = node.data.target.fields;
 
 				if (contentTypeId === "codeBlock" && code) {
-					return `
-                    <pre>
-                        <code>${code}</code>
-                    </pre>
-                `;
+					return `<pre><code>${code}</code></pre>`;
 				}
 
 				if (contentTypeId === "videoEmbed" && embedUrl && title) {
-					return `
-                    <iframe
-                        src="${embedUrl}"
-                        height="100%"
-                        width="100%"
-                        title="${title}"
-                        allowfullscreen
-                    ></iframe>
-                `;
+					return `<iframe src="${embedUrl}" height="100%" width="100%" title="${title}" allowfullscreen></iframe>`;
 				}
 				return "";
 			},
@@ -53,21 +38,22 @@ export function renderOptions(rawArticle: RawArticle) {
 
 				if (url) {
 					return `
-                    <figure class="full-bleed">
-                        <img
-                            src="https:${url}"
-                            height="${height || ""}"
-                            width="${width || ""}"
-                            alt="${description || rawArticle.fields.title}"
-                            loading="lazy"
-                            decoding="async"
-                        />
-                        ${description ? `<figcaption>${description}</figcaption>` : ""}
-                    </figure>
-                `;
+            <figure class="full-bleed">
+              <img
+                src="https:${url}"
+                height="${height ?? ""}"
+                width="${width ?? ""}"
+                alt="${description ?? rawArticle.fields.title}"
+                loading="lazy"
+                decoding="async"
+              />
+              ${description ? `<figcaption>${description}</figcaption>` : ""}
+            </figure>
+          `;
 				}
 				return "";
 			},
+			...parseHeadings(),
 		},
 	};
 }
