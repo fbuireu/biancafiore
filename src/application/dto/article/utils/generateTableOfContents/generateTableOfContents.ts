cd9eb79ab5907e20127f4b9a72a76e31a19b866a
Ferran <fbuireu@gmail.com>
2024-09-27 11:04:44 +0200

    feat: add scroll-driven-animation to toc

--- src/application/dto/article/utils/generateTableOfContents/generateTableOfContents.ts
+++ src/application/dto/article/utils/generateTableOfContents/generateTableOfContents.ts
@@ -3,14 +3,15 @@ import { slugify } from "@modules/core/utils/slugify";
 
 type TableOfContentsReturnType = CollectionEntry<"articles">["data"]["tableOfContents"];
 
-const HEADINGS_REGEX = /<h([1-6])>(.*?)<\/h\1>/g;
+const HEADINGS_REGEX = /<h([2-6])>(.*?)<\/h\1>/g;
+const HEADING_LEVEL_OFFSET = 1;
 
 export function generateTableOfContents(html: string): TableOfContentsReturnType {
 	const items: TableOfContentsReturnType = [];
 	const headings = html.matchAll(HEADINGS_REGEX);
 
 	for (const heading of headings) {
-		const level = Number(heading[1]);
+		const level = Number(heading[1]) - HEADING_LEVEL_OFFSET;
 		const text = heading[2];
 		const id = slugify(text);
 
