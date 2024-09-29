0c7d7a9800e0177eaa64cd1b75ba38320ee85857
Ferran Buireu <fbuireu@gmail.com>
2024-09-29 09:15:03 +0200

    feat: add table of contents

--- src/application/dto/article/utils/parseHeadings/parseHeadings.tsx
+++ src/application/dto/article/utils/parseHeadings/parseHeadings.tsx
@@ -1,26 +1,58 @@
+import type { Block, Text, TopLevelBlock } from "@contentful/rich-text-types";
 import { BLOCKS } from "@contentful/rich-text-types";
-import type { Block, Text } from "@contentful/rich-text-types";
 import { slugify } from "@modules/core/utils/slugify";
 
 const HEADING_LEVELS = [1, 2, 3, 4, 5, 6];
 
 type HeadingBlock = Block & { content: Text[] };
 
+interface CreateSectionParams {
+	level: number;
+	index: number;
+	id: string;
+	text: string;
+	content: string;
+}
+
+interface ExtractContentFromNextNodesParams {
+	nextNodes: TopLevelBlock[];
+	level: number;
+}
+
+const extractContentFromNextNodes = ({ nextNodes, level }: ExtractContentFromNextNodesParams) => {
+	if (!Array.isArray(nextNodes)) return "";
+
+	return nextNodes
+		.map((nextNode) => {
+			if (nextNode.nodeType !== BLOCKS[`HEADING_${level}` as keyof typeof BLOCKS]) {
+				return nextNode.content.map((child) => ("value" in child ? child.value : "")).join("");
+			}
+			return "";
+		})
+		.join("");
+};
+
+const createSection = ({ level, id, text, content, index }: CreateSectionParams) => {
+	return `
+    <section style="--is: --section-${index}">
+      <h${level} id="${id}" class="article__heading flex flex-start align-baseline">
+        <a href="#${id}">${text}</a>
+      </h${level}>
+      <p>${content}</p>
+    </section>
+  `;
+};
+
 export function parseHeadings() {
 	return Object.fromEntries(
-		HEADING_LEVELS.map((level) => [
+		HEADING_LEVELS.map((level, index) => [
 			BLOCKS[`HEADING_${level}` as keyof typeof BLOCKS],
-			(node: HeadingBlock) => {
+			(node: HeadingBlock, nextNodes: TopLevelBlock[]) => {
 				const text = node.content.map((child: Text) => child.value).join("");
 				const id = slugify(text);
+				const content = extractContentFromNextNodes({ nextNodes, level });
 
-				return `
-					<h${level} id="${id}" class="article__heading flex flex-start align-baseline">
-						<a href="#${id}">
-							${text}
-						</a>
-					</h${level}>
-				`;
+				return createSection({ level, index, id, text, content });
 			},
 		]),
 	);
