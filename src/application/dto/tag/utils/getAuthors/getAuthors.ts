32f47d8abb84cd7aad62046bff7fe8d88c9acbac
Ferran Buireu <fbuireu@gmail.com>
2024-09-29 13:13:20 +0200

    refactor: refactor methods

---
+++ src/application/dto/tag/utils/getAuthors/getAuthors.ts
@@ -0,0 +1,12 @@
+import { getCollection } from "astro:content";
+import { TagType } from "@application/dto/tag/types";
+
+export async function getAuthors() {
+	return (await getCollection("authors")).map((author) => ({
+		name: author.data.name,
+		slug: author.data.slug,
+		type: TagType.AUTHOR,
+		count: author.data.articles.length,
+		articles: author.data.articles,
+	}));
+}
