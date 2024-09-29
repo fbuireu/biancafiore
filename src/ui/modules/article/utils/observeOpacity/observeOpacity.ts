b0a99b280cceeb19c2da3ed6e471b42187baa852
Ferran Buireu <fbuireu@gmail.com>
2024-09-29 09:15:03 +0200

    feat: add table of contents

---
+++ src/ui/modules/article/utils/observeOpacity/observeOpacity.ts
@@ -0,0 +1,20 @@
+const SELECTORS = {
+	TABLE_OF_CONTENTS: ".article__table-of-contents__nav",
+};
+
+const ANIMATION_NAME = "fade-in";
+
+export function observeOpacity() {
+	const TABLE_OF_CONTENTS = document.querySelector(SELECTORS.TABLE_OF_CONTENTS);
+
+	if (!TABLE_OF_CONTENTS) return;
+
+	TABLE_OF_CONTENTS.addEventListener("animationend", (event) => {
+		const animationEvent = event as AnimationEvent;
+
+		if (animationEvent.animationName === ANIMATION_NAME) {
+			const isVisible = window.getComputedStyle(TABLE_OF_CONTENTS).opacity === "1";
+			TABLE_OF_CONTENTS.classList.toggle("--is-visible", isVisible);
+		}
+	});
+}
