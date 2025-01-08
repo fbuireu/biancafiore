15eeae6f9a41ab38201c4af92059c57040b19983
Ferran <fbuireu@gmail.com>
2025-01-08 07:51:20 +0100

    perf: improve parallel promise

--- src/application/dto/tag/tagDTO.ts
+++ src/application/dto/tag/tagDTO.ts
@@ -1,15 +1,18 @@
-import { getCollection } from "astro:content";
-import type { RawTag, TagDTO } from "@application/dto/tag/types";
-import { getAuthors } from "@application/dto/tag/utils/getAuthors";
-import { getTags } from "@application/dto/tag/utils/getTags";
-import type { BaseDTO } from "@shared/application/dto/baseDTO";
+import { getCollection } from 'astro:content';
+import type { RawTag, TagDTO } from '@application/dto/tag/types';
+import { getAuthors } from '@application/dto/tag/utils/getAuthors';
+import { getTags } from '@application/dto/tag/utils/getTags';
+import type { BaseDTO } from '@shared/application/dto/baseDTO';
 
 export const tagDTO: BaseDTO<RawTag[], Promise<TagDTO>> = {
-	create: async (raw) => {
-		const articles = await getCollection("articles");
-		const tags = await getTags({ raw, articles });
-		const authors = await getAuthors();
+  create: async (raw) => {
+    const articles = await getCollection('articles');
 
-		return Object.groupBy([...tags, ...authors], ({ name }) => name.charAt(0).toUpperCase()) as TagDTO;
-	},
+    const [tags, authors] = await Promise.all([
+      getTags({ raw, articles }),
+      getAuthors(),
+    ]);
+
+    return Object.groupBy([...tags, ...authors], ({ name }) => name.charAt(0).toUpperCase()) as TagDTO;
+  },
 };
