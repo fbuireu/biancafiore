c51d19db5f13acb220b838fed35bda0480318918
Ferran <fbuireu@gmail.com>
2025-01-16 16:39:52 +0100

    fix: fix catch to return default values

--- src/application/dto/author/utils/getArticlesByAuthor.ts
+++ src/application/dto/author/utils/getArticlesByAuthor.ts
@@ -1,14 +1,14 @@
-import { getCollection } from "astro:content";
-import type { RawAuthor } from "@application/dto/author/types";
-import type { Reference } from "@shared/application/types";
+import { getCollection } from 'astro:content';
+import type { RawAuthor } from '@application/dto/author/types';
+import type { Reference } from '@shared/application/types';
 
-export async function getArticlesByAuthor(rawAuthor: RawAuthor): Promise<Reference<"articles">[]> {
-	const articles = await getCollection("articles");
+export async function getArticlesByAuthor(rawAuthor: RawAuthor): Promise<Reference<'articles'>[]> {
+  const articles = await getCollection('articles').catch(() => []);
 
-	return articles
-		.filter((article) => article.data.author.name === String(rawAuthor.fields.name))
-		.map((article) => ({
-			id: article.data.slug,
-			collection: "articles",
-		}));
+  return articles
+      .filter((article) => article.data.author.name === String(rawAuthor.fields.name))
+      .map((article) => ({
+        id: article.data.slug,
+        collection: 'articles',
+      }));
 }
