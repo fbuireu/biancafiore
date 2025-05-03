6321b0dad2aa43cace43fff4544a82e7648af9df
Ferran Buireu <fbuireu@gmail.com>
2025-05-03 08:40:10 +0200

    style: minor tweaks

--- src/ui/modules/about/utils/renderPin/renderPin.ts
+++ src/ui/modules/about/utils/renderPin/renderPin.ts
@@ -1,50 +1,64 @@
-import type { CollectionEntry } from "astro:content";
 import { Pin as pin } from "@assets/images/svg-components/pin";
 import type { ReactGlobePoint } from "@modules/about/components/worldGlobe";
 import { slugify } from "@modules/core/utils/slugify";
+import type { CollectionEntry } from "astro:content";
 import { createRoot } from "react-dom/client";
 
 interface RenderPinParams {
-	markerData: ReactGlobePoint;
-	cities: CollectionEntry<"cities">[];
+    markerData: ReactGlobePoint;
+    cities: CollectionEntry<"cities">[];
 }
 
-export function renderPin({ markerData, cities }: RenderPinParams): HTMLElement {
-	const citiesInitialPosition = cities.map(
-		({ data }) => (document.querySelector(`#${slugify(data.name)}`) as HTMLElement)?.offsetTop,
-	);
+export function renderPin({
+    markerData,
+    cities,
+}: RenderPinParams): HTMLElement {
+    const citiesInitialPosition = cities.map(
+        ({ data }) =>
+            (document.querySelector(`#${slugify(data.name)}`) as HTMLElement)
+                ?.offsetTop
+    );
 
-	const markerWrapper = document.createElement("button");
-	markerWrapper.classList.add("marker__wrapper", `--is-${slugify(markerData.label)}`);
+    const markerWrapper = document.createElement("button");
+    markerWrapper.classList.add(
+        "marker__wrapper",
+        `--is-${slugify(markerData.label)}`
+    );
 
-	const marker = document.createElement("div");
-	const root = createRoot(marker);
-	root.render(pin({ fill: "currentColor", title: markerData.label }));
-	markerWrapper.append(marker);
+    const marker = document.createElement("div");
+    const root = createRoot(marker);
+    root.render(pin({ fill: "currentColor", title: markerData.label }));
+    markerWrapper.append(marker);
 
-	const label = document.createElement("div");
-	label.classList.add("marker__label", "font-sans-serif");
-	label.textContent = markerData.label;
-	markerWrapper.append(label);
+    const label = document.createElement("div");
+    label.classList.add("marker__label", "font-sans-serif");
+    label.textContent = markerData.label;
+    markerWrapper.append(label);
 
-	markerWrapper.onclick = () => {
-		const city = document.querySelector(`#${slugify(markerData.label)}`);
-		if (!city) {
-			return;
-		}
+    markerWrapper.onclick = () => {
+        const city = document.querySelector(`#${slugify(markerData.label)}`);
+        if (!city) {
+            return;
+        }
 
-		const cityIndex = Number(getComputedStyle(city).getPropertyValue("--inline-index"));
-		const cityPosition = citiesInitialPosition[cityIndex - 1];
+        const cityIndex = Number(
+            getComputedStyle(city).getPropertyValue("--inline-index")
+        );
+        const cityPosition = citiesInitialPosition[cityIndex - 1];
 
-		if (cityIndex < 1 || cityIndex > citiesInitialPosition.length || !cityPosition) {
-			return;
-		}
+        if (
+            cityIndex < 1 ||
+            cityIndex > citiesInitialPosition.length ||
+            !cityPosition
+        ) {
+            return;
+        }
 
-		window.scrollTo({
-			top: cityPosition - city.clientHeight / 4,
-			behavior: "smooth",
-		});
-	};
+        window.scrollTo({
+            top: cityPosition - city.clientHeight / 4,
+            behavior: "smooth",
+        });
+    };
 
-	return markerWrapper;
+    return markerWrapper;
 }
--- src/ui/modules/core/components/articleCard/atoms/articleCardTagItem/ArticleCardTagItem.tsx
+++ src/ui/modules/core/components/articleCard/atoms/articleCardTagItem/ArticleCardTagItem.tsx
@@ -10,12 +10,12 @@ export interface ArticleCardTagItemProps {
 }
 
 export const ArticleCardTagItem = ({ children, tag, style }: ArticleCardTagItemProps) => (
-	<a
-		key={tag.name}
-		className="article-card__tag__item --underline-on-hover --is-clickable"
-		href={`${PAGES_ROUTES.TAGS}/${tag.slug}`}
-		style={style}
-	>
+	<li key={tag.name} className="article-card__tag__item" style={style}>
+		<a
+			href={`${PAGES_ROUTES.TAGS}/${tag.slug}`}
+			className="--underline-on-hover --is-clickable"
+		>
 		#{children}
 	</a>
+	</li>
 );
