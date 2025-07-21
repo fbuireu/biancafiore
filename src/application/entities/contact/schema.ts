092fa7661161260bdef1e7c431a96acbb5214082
Ferran Buireu <fbuireu@gmail.com>
2025-07-21 22:15:01 +0200

    fix: zod schema

--- src/application/entities/contact/schema.ts
+++ src/application/entities/contact/schema.ts
@@ -1,13 +1,13 @@
-import { z } from "astro:schema";
+import { z } from 'astro:schema'
 
 export const contactFormSchema = z.object({
-	id: z.string(),
-	name: z.string().trim().min(1, "Please insert your name"),
-	email: z
-		.string()
-		.email()
-		.trim()
-		.min(1, "Please insert a valid email")
-		.email({ message: "Still not a valid email fella" }),
-	message: z.string().trim().min(1, "Please insert a valid message"),
-});
+    id: z.string(),
+    name: z.string().trim().min(1, 'Please insert your name'),
+    email: z
+        .string()
+        .email()
+        .trim()
+        .min(1, 'Please insert a valid email')
+        .email({ message: 'Still not a valid email fella' }),
+    message: z.string().trim().min(1, 'Please insert a valid message'),
+})
