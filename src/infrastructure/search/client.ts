ceabfc85410320b6292eb20dac375725b29308cd
Ferran Buireu <fbuireu@gmail.com>
2026-05-22 21:22:05 +0200

    ci: fix env

--- src/infrastructure/search/client.ts
+++ src/infrastructure/search/client.ts
@@ -1,6 +1,1 @@
-import { algoliasearch } from "algoliasearch";
-
-export async function createSearchClient() {
-	const { getSecret } = await import("astro:env/server");
-	return algoliasearch(getSecret("ALGOLIA_APP_ID") as string, getSecret("ALGOLIA_API_KEY") as string);
-}
+export {};
