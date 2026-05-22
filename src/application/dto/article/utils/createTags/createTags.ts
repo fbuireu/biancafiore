dc1b3a8312710dbda5f3bc5c8a5abbf54daacc17
Ferran Buireu <fbuireu@gmail.com>
2026-05-22 22:17:41 +0200

    fix: warnings

--- src/application/dto/article/utils/createTags/createTags.ts
+++ src/application/dto/article/utils/createTags/createTags.ts
@@ -1,8 +1,8 @@
 import type { BaseTagDTO } from "@application/dto/tag/types";
 import type { Entry, EntrySkeletonType } from "contentful";
 
-export function createTags(tags: Array<Entry<EntrySkeletonType<BaseTagDTO>>>): BaseTagDTO[] {
-	return tags.map((tag) => ({
+export function createTags(tags: Array<Entry<EntrySkeletonType<BaseTagDTO>>> | undefined): BaseTagDTO[] {
+	return (tags ?? []).map((tag) => ({
 		name: tag.fields.name as unknown as string,
 		slug: tag.fields.slug as unknown as string,
 	}));
