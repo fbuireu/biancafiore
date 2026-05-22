90abacea2a7421959c395d5b62f932c0ce444c45
Ferran Buireu <fbuireu@gmail.com>
2026-05-22 22:17:41 +0200

    fix: warnings

--- src/application/dto/article/utils/createRelatedArticles/createRelatedArticles.ts
+++ src/application/dto/article/utils/createRelatedArticles/createRelatedArticles.ts
@@ -1,8 +1,8 @@
 import type { Reference } from "@shared/application/types";
 import type { Entry, EntrySkeletonType } from "contentful";
 
-export function createRelatedArticles(relatedArticles: Array<Entry<EntrySkeletonType>>): Reference<"articles">[] {
-	return relatedArticles.map((relatedArticle) => ({
+export function createRelatedArticles(relatedArticles: Array<Entry<EntrySkeletonType>> | undefined): Reference<"articles">[] {
+	return (relatedArticles ?? []).map((relatedArticle) => ({
 		id: relatedArticle.fields.slug as unknown as string,
 		collection: "articles",
 	}));
