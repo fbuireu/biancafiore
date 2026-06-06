74937d5a4e6ecca931844a2665c7d75e13a11faa
Ferran Buireu <fbuireu@gmail.com>
2026-06-06 20:03:43 +0200

    fix: ci codecov upload

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		include: ["src/**/*.test.{ts,tsx}", "src/**/*.spec.{ts,tsx}"],
+		coverage: {
+			provider: "v8",
+			reporter: ["text", "lcov"],
+			reportsDirectory: "./coverage",
+		},
 	},
 });
