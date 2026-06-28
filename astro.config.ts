4aebb3d8c5dbe2f958af0899eb3854fc7ebefdc6
Ferran Buireu <fbuireu@gmail.com>
2026-06-28 21:04:51 +0200

    fix: revert custom image service that returned 404 for transformed images

--- astro.config.ts
+++ astro.config.ts
@@ -86,7 +86,7 @@ export default defineConfig({
 			customPages: ["https://biancafiore.me/tags"],
 		}),
 	],
-	adapter: cloudflare({ imageService: "custom" }),
+	adapter: cloudflare(),
 	env: {
 		schema: {
 			SITE_URL: envField.string({
