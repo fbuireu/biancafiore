6683bb9ffaca5eda6bd87a6e3857b9aca950f5f8
Ferran Buireu <fbuireu@gmail.com>
2026-06-28 20:55:47 +0200

    fix: minor css issues

--- src/ui/modules/core/components/emailButton/utils/mailTo/mailTo.ts
+++ src/ui/modules/core/components/emailButton/utils/mailTo/mailTo.ts
@@ -1,16 +1,20 @@
 import { CONTACT_DETAILS } from "@const/index";
 
+let isListening = false;
+
 export function mailTo(): void {
-	const SELECTOR = document.querySelector(".mailTo__button");
-	if (!SELECTOR) {
+	if (isListening) {
 		return;
 	}
+	isListening = true;
 
-	SELECTOR.addEventListener("click", (event) => {
-		event.preventDefault();
-		if (!event.isTrusted) {
+	document.addEventListener("click", (event) => {
+		const button = (event.target as HTMLElement).closest(".mailTo__button");
+		if (!button || !event.isTrusted) {
 			return;
 		}
+
+		event.preventDefault();
 
 		const mailto = encodeURIComponent(`"${CONTACT_DETAILS.NAME}"<${atob(CONTACT_DETAILS.ENCODED_EMAIL_BIANCA)}>`);
 
