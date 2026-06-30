6f291046dad30c474af13937318ff209a7c39375
Ferran Buireu <fbuireu@gmail.com>
2026-06-30 18:45:10 +0200

    fix: type import vite

--- src/const/const.ts
+++ src/const/const.ts
@@ -31,9 +31,5 @@ export const DEFAULT_DATE_FORMAT: Intl.DateTimeFormatOptions = {
 
 export const DEFAULT_LOCALE_STRING: Intl.LocalesArgument = "en-GB" as const;
 
-export const IMAGE_CDN = {
-	CLOUDFLARE: "cloudflare",
-	CONTENTFUL: "contentful",
-} as const;
-
-export type ImageCdn = (typeof IMAGE_CDN)[keyof typeof IMAGE_CDN];
+export { IMAGE_CDN } from "./imageCdn";
+export type { ImageCdn } from "./imageCdn";
